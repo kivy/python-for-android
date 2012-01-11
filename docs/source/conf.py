3ef81e50235a282d08c711ede79ae2180877c8e4
Mathieu Virbel <mat@kivy.org>
2012-01-11 02:57:52 +0100

    try to fix theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,11 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if on_rtd:
-    html_theme = 'default'
-else:
-    html_theme = 'nature'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
