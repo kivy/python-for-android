24e9711ca7b4f5da6acb6e3eda5e077558a0d434
Mathieu Virbel <mat@kivy.org>
2012-01-11 02:45:44 +0100

    change sphinx theme for rtds

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,7 +91,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
