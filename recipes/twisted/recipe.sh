8ab6ebbac9a1abf93b49dbc230f70b8635629a45
Ryan Northey <ryan@3ca.org.uk>
2012-03-14 15:45:00 +0000

    adjusting hostpython path for recipes that use setuptools where it is available

--- recipes/twisted/recipe.sh
+++ recipes/twisted/recipe.sh
@@ -23,12 +23,15 @@ function build_twisted() {
 	export LDFLAGS="$LDFLAGS -L$LIBS_PATH"
 	export LDSHARED="$LIBLINK"
 
+	export PYTHONPATH=$BUILD_PATH/hostpython/Python-2.7.2/Lib/site-packages
+
 	# fake try to be able to cythonize generated files
 	$BUILD_PATH/python-install/bin/python.host setup.py build_ext
 	try find . -iname '*.pyx' -exec cython {} \;
 	try $BUILD_PATH/python-install/bin/python.host setup.py build_ext -v
 	try find build/lib.* -name "*.o" -exec $STRIP {} \;
-	try $BUILD_PATH/python-install/bin/python.host setup.py install -O2
+
+        try $BUILD_PATH/hostpython/Python-2.7.2/hostpython setup.py install -O2 --root=$BUILD_PATH/python-install --install-lib=lib/python2.7/site-packages
 
 	try rm -rf $BUILD_PATH/python-install/lib/python*/site-packages/twisted/test
 	try rm -rf $BUILD_PATH/python-install/lib/python*/site-packages/twisted/*/test
