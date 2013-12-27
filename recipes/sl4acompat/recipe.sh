38dd4f2481e3467637ab4e40dbbcae1bb44c1140
Alan Justino <alan.justino@yahoo.com.br>
2013-12-26 23:09:10 -0200

    Minimal jar-copying recipe.sh

---
+++ recipes/sl4acompat/recipe.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+URL_sl4acompat=https://github.com/damonkohler/sl4a/raw/dcfa832f80/android/script_for_android_template.zip
+DEPS_sl4acompat=()
+MD5_sl4acompat=eed6a34bdf63e57a94b3c61528b6a577
+BUILD_sl4acompat=$BUILD_PATH/sl4acompat/$(get_directory $URL_sl4acompat)
+RECIPE_sl4acompat=$RECIPES_PATH/sl4acompat
+
+function prebuild_sl4acompat() {
+	true
+}
+
+function shouldbuild_sl4acompat() {
+	true
+}
+
+function build_sl4acompat() {
+	cd $BUILD_sl4acompat
+        cp -a libs/* $BUILD_PATH/libs/
+}
+
+function postbuild_sl4acompat() {
+	true
+}
