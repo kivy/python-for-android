88a692d98f87b3f14e0e09005e16f2f20afa76d2
Alan Justino <alan.justino@yahoo.com.br>
2013-12-27 00:08:28 -0200

    Hack to copy pre-built jars via recipe

--- recipes/sl4acompat/recipe.sh
+++ recipes/sl4acompat/recipe.sh
@@ -3,7 +3,7 @@
 URL_sl4acompat=https://github.com/damonkohler/sl4a/raw/dcfa832f80/android/script_for_android_template.zip
 DEPS_sl4acompat=()
 MD5_sl4acompat=eed6a34bdf63e57a94b3c61528b6a577
-BUILD_sl4acompat=$BUILD_PATH/sl4acompat/$(get_directory $URL_sl4acompat)
+BUILD_sl4acompat=$BUILD_PATH/sl4acompat/
 RECIPE_sl4acompat=$RECIPES_PATH/sl4acompat
 
 function prebuild_sl4acompat() {
@@ -15,10 +15,13 @@ function shouldbuild_sl4acompat() {
 }
 
 function build_sl4acompat() {
-	cd $BUILD_sl4acompat
-        cp -a libs/* $BUILD_PATH/libs/
+        true
 }
 
 function postbuild_sl4acompat() {
+        cd $BUILD_sl4acompat
+        mkdir "$DIST_PATH"/libs
+        cp -av libs/* "$DIST_PATH"/libs/
+        cd -
 	true
 }
