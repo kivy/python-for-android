34ef8383a31122cfca46d9eca6e190b3a1f1429d
Alan Justino <alan.justino@yahoo.com.br>
2013-12-27 04:14:42 -0200

    Try once again

--- src/src/org/alanjds/sl4acompat/MinimalServiceStarter.java
+++ src/src/org/alanjds/sl4acompat/MinimalServiceStarter.java
@@ -1,14 +1,19 @@
 package org.alanjds.sl4acompat;
 
+import com.googlecode.android_scripting.Log;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
 
 public class MinimalServiceStarter extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent service = new Intent(context, MinimalService.class);
-        context.startService(service);
+        Log.v('Received intent!')
+        Intent serviceIntent = new Intent(context, MinimalService.class);
+        context.startService(serviceIntent);
+        Log.v('Service started?')
     }
 } 
