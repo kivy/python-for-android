3f4733b15d6b531429b322e2d7c659970ea850a0
Michael R. Hines <michael@hinespot.com>
2014-06-09 00:01:13 +0800

    1. cjklib ('cjklib' on pypi) is a pure-python swiss-army knife translation library for character-oriented languages like Chinese. 2. WebOb ('webob' on pypi) is a popular python WSGI middleware library for building reusable python web applications.
    I've tested both of these in my buildozer-created APK and they work great. No patches required as they do not have C-extensions.
    
    One warning: cjklib uses sqlite and creates a pretty "hefty" 45 MB database upon it's 'setup.py' installation for the purposes
    of translating different languages. I've submitted a request on their mailing list for options for pruning the size of this
    database. Hope to get a response from them soon.

---
+++ recipes/webob/recipe.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+VERSION_webob=${VERSION_webob:-1.4}
+DEPS_webob=(setuptools android)
+URL_webob=http://pypi.python.org/packages/source/W/WebOb/WebOb-${VERSION_webob}.tar.gz
+MD5_webob=8437607c0cc00c35f658f972516ffb55
+BUILD_webob=$BUILD_PATH/webob/$(get_directory $URL_webob)
+RECIPE_webob=$RECIPES_PATH/webob
+
+# function called for preparing source code if needed
+# (you can apply patch etc here.)
+function prebuild_webob() {
+	true
+}
+
+function shouldbuild_webob() {
+    if [ -d "$SITEPACKAGES_PATH/webob" ]; then
+		DO_BUILD=0
+	fi
+}
+
+function build_webob() {
+	cd $BUILD_webob
+	push_arm
+	export EXTRA_CFLAGS="--host linux-armv"
+	try $HOSTPYTHON setup.py install -O2
+
+	pop_arm
+}
+
+# function called after all the compile have been done
+function postbuild_webob() {
+	true
+}
+
