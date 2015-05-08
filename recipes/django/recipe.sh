4a724bd2800962460cafa781e31e2213237f83b1
Francisco Demartino <demartino.francisco@gmail.com>
2015-05-08 00:03:55 -0300

    recipes: update django to 1.8.1

--- recipes/django/recipe.sh
+++ recipes/django/recipe.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VERSION_django=${VERSION_django:-1.6.1}
+VERSION_django=${VERSION_django:-1.8.1}
 DEPS_django=(sqlite3 python)
 URL_django=https://pypi.python.org/packages/source/D/Django/Django-$VERSION_django.tar.gz
-MD5_django=3ea7a00ea9e7a014e8a4067dd6466a1b
+MD5_django=0f0a677a2cd56b9ab7ccb1c562d70f53
 BUILD_django=$BUILD_PATH/django/$(get_directory $URL_django)
 RECIPE_django=$RECIPES_PATH/django
 
