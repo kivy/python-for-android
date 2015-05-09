5f6e0b023f154a183ac51e4f2083d7f63fb13962
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-05-09 22:12:44 +0100

    Fixed patching and added pygame prebuild

--- recipes/pygame/__init__.py
+++ recipes/pygame/__init__.py
@@ -1,11 +1,22 @@
 
 from toolchain import PythonRecipe, shprint
+from os.path import exists, join
+import sh
 
 class PygameRecipe(PythonRecipe):
     name = 'pygame'
     version = '1.9.1'
     url = 'http://pygame.org/ftp/pygame-{version}release.tar.gz'
     depends = ['python2', 'sdl']
+
+    def prebuild_armeabi(self):
+        if exists(join(self.get_build_dir('armeabi'), '.patched')):
+            print('Pygame already patched, skipping.')
+            return
+        self.apply_patch(join('patches', 'fix-surface-access.patch'))
+        self.apply_patch(join('patches', 'fix-array-surface.patch'))
+        shprint(sh.touch, join(self.get_build_dir('armeabi'), '.patched'))
+        
 
 
 recipe = PygameRecipe()
