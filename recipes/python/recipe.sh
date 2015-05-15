4fcaad654e300082dc2729a708dbbce4175c1941
Ryan Pessa <ryan@essential-elements.net>
2015-05-15 10:02:47 -0500

    add wildcard for system

--- recipes/python/recipe.sh
+++ recipes/python/recipe.sh
@@ -131,7 +131,7 @@ function build_python() {
 	fi
 	try cp $BUILD_hostpython/hostpython $HOSTPYTHON
 	try cp libpython2.7.so $LIBS_PATH/
-	try cp -a build/lib.${system}-${machine}-2.7/_ctypes*.so $LIBS_PATH
+	try cp -a build/lib.*-${machine}-2.7/_ctypes*.so $LIBS_PATH
 
 	# reduce python
 	rm -rf "$BUILD_PATH/python-install/lib/python2.7/test"
