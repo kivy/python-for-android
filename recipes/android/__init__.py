200fa2378048ccbd6a8494a9c7198cba74a4bcb9
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-05-18 01:13:34 +0100

    Made android recipe work

--- recipes/android/__init__.py
+++ recipes/android/__init__.py
@@ -1,6 +1,7 @@
 
-from toolchain import PythonRecipe, shprint, ensure_dir
+from toolchain import PythonRecipe, shprint, ensure_dir, current_directory, ArchAndroid
 import sh
+from os.path import exists, join
 
 
 class AndroidRecipe(PythonRecipe):
@@ -12,6 +13,26 @@ class AndroidRecipe(PythonRecipe):
     def prebuild_armeabi(self):
         shprint(sh.cp, '-a', self.get_recipe_dir() + '/src', self.get_build_dir('armeabi'))
         
+    def build_armeabi(self):
+
+        with current_directory(join(self.get_build_dir('armeabi'), 'src')):
+            if exists('.done'):
+                print('android module already compiled, exiting')
+                return
+
+            env = ArchAndroid(self.ctx).get_env()
+
+            env['LDFLAGS'] = env['LDFLAGS'] + ' -L{}'.format(self.ctx.libs_dir)
+            env['LDSHARED'] = env['LIBLINK']
+
+            shprint(sh.find, '.', '-iname', '*.pyx', '-exec', self.ctx.cython, '{}', ';')
+
+            hostpython = sh.Command(self.ctx.hostpython)
+            shprint(hostpython, 'setup.py', 'build_ext', '-v', _env=env)
+            shprint(hostpython, 'setup.py', 'install', '-O2', _env=env)
+
+            shprint(sh.touch, '.done')
+            
 
 
 recipe = AndroidRecipe()
