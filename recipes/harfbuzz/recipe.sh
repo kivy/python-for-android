42f279674f25c8a96c686351dbd139effa6fc611
Oliver Marks <oly@digitaloctave.com>
2015-05-18 07:26:28 +0100

    rebuild harfbuzz if freetype exists, add -nostdinc to stop the build using system headers

--- recipes/harfbuzz/recipe.sh
+++ recipes/harfbuzz/recipe.sh
@@ -22,32 +22,21 @@ function build_harfbuzz() {
     cd $BUILD_harfbuzz
     push_arm
     try ./configure --without-icu --host=arm-linux-androideabi --prefix="$BUILD_PATH/python-install" --without-freetype --without-glib
-    try make -j5
+    try make -nostdinc -j5
     pop_arm
+    try cp -L $BUILD_harfbuzz/src/.libs/libharfbuzz.so $LIBS_PATH
 }
 
 # function called after all the compile have been done
 function postbuild_harfbuzz() {
-    ln -s $BUILD_freetype "$BUILD_PATH/freetype2"
     if [ -f "$BUILD_freetype/objs/.libs/libfreetype.so" ]; then
         echo "freetype found rebuilding harfbuzz with freetype support";
-	#true
-	echo $BUILD_PATH;
-    #build again now that freetype has finished
-    cd $BUILD_harfbuzz
-    echo $BUILD_freetype;
-    ls $BUILD_freetype;
-    
-    export LDFLAGS="-L$BUILD_freetype/objs/.libs/ $LDFLAGS"
-    export CFLAGS="-I$BUILD_freetype/include/ -I$BUILD_freetype/ -I$BUILD_PATH/python-install/include/python2.7 $CFLAGS"
-    export CPPFLAGS="-I$BUILD_freetype/include/ -I$BUILD_PATH/python-install/include/python2.7 $CPPFLAGS"
-    export CC="-I$BUILD_freetype/include/ $CC "
-    export LDSHARED="$LIBLINK"
-    push_arm
-    try ./configure --without-icu --host=arm-linux-androideabi --cross-compile --prefix="$BUILD_PATH/python-install" --without-glib --libdir="$BUILD_freetype/objs/.libs/" --includedir="$BUILD_freetype/include/"
-    try make  --include-dir="$BUILD_freetype/include/" #-j5
-    pop_arm
-    try cp -L $BUILD_harfbuzz/src/.libs/libharfbuzz.so $LIBS_PATH
+        cd $BUILD_harfbuzz
+        push_arm
+        try ./configure --without-icu --host=arm-linux-androideabi --prefix="$BUILD_PATH/python-install" --without-glib
+        try make -nostdinc -j5
+        pop_arm
+        try cp -L $BUILD_harfbuzz/src/.libs/libharfbuzz.so $LIBS_PATH
     fi
 }
 
