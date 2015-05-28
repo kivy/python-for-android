c9b374b00dcee0571438bdf19f5a6f2bf65dd81d
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-05-29 00:34:09 +0100

    Removed commented out code from {P,C}ythonRecipes

--- recipes/android/__init__.py
+++ recipes/android/__init__.py
@@ -11,29 +11,5 @@ class AndroidRecipe(IncludedFilesBehaviour, CythonRecipe):
     depends = ['pygame']
     src_filename = 'src'
 
-    # def prebuild_armeabi(self):
-    #     shprint(sh.cp, '-a', self.get_recipe_dir() + '/src', self.get_build_dir('armeabi'))
-        
-    # def build_armeabi(self):
-
-    #     with current_directory(self.get_build_dir('armeabi')):
-    #         if exists('.done'):
-    #             print('android module already compiled, exiting')
-    #             return
-
-    #         env = ArchAndroid(self.ctx).get_env()
-
-    #         env['LDFLAGS'] = env['LDFLAGS'] + ' -L{}'.format(self.ctx.libs_dir)
-    #         env['LDSHARED'] = join(self.ctx.root_dir, 'tools', 'liblink')
-
-    #         shprint(sh.find, '.', '-iname', '*.pyx', '-exec', self.ctx.cython, '{}', ';')
-
-    #         hostpython = sh.Command(self.ctx.hostpython)
-    #         shprint(hostpython, 'setup.py', 'build_ext', '-v', _env=env)
-    #         shprint(hostpython, 'setup.py', 'install', '-O2', _env=env)
-
-    #         shprint(sh.touch, '.done')
-            
-
 
 recipe = AndroidRecipe()
