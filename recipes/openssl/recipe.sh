0492e14e88f6b501186cd8bbda427ff48178270d
Ben Hagen <c.ben.hagen@gmail.com>
2015-06-14 21:57:54 +0200

    update openssl version to 1.0.2c

--- recipes/openssl/recipe.sh
+++ recipes/openssl/recipe.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VERSION_openssl=${VERSION_openssl:-1.0.2b}
+VERSION_openssl=${VERSION_openssl:-1.0.2c}
 URL_openssl=https://www.openssl.org/source/openssl-$VERSION_openssl.tar.gz
 DEPS_openssl=()
-MD5_openssl=7729b259e2dea7d60b32fc3934d6984b
+MD5_openssl=8c8d81a9ae7005276e486702edbcd4b6
 BUILD_openssl=$BUILD_PATH/openssl/$(get_directory $URL_openssl)
 RECIPE_openssl=$RECIPES_PATH/openssl
 
