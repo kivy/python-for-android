1e1097282ba82f3c2caeed661c56d2e9782d3b8f
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-06-16 23:19:20 +0100

    Removed now-unused parts of sdl2 recipe

--- recipes/sdl2/__init__.py
+++ recipes/sdl2/__init__.py
@@ -11,22 +11,7 @@ class LibSDL2Recipe(NDKRecipe):
     depends = ['python2']
     dir_name = 'SDL'
 
-    def prebuild_arch(self, arch):
-        with current_directory(self.get_build_container_dir(arch)):
-            if exists('SDL2-{}'.format(self.version)):
-                if not exists('SDL'):
-                    shprint(sh.mv, 'SDL2-{}'.format(self.version), 'SDL')
-                else:
-                    shprint(sh.rm, '-rf', 'SDL2-{}'.format(self.version))
-
     def build_arch(self, arch):
-        # shprint(sh.xcodebuild,
-        #         "ONLY_ACTIVE_ARCH=NO",
-        #         "ARCHS={}".format(arch.arch),
-        #         "-sdk", arch.sdk,
-        #         "-project", "Xcode-iOS/SDL/SDL.xcodeproj",
-        #         "-target", "libSDL",
-        #         "-configuration", "Release")
         env = self.get_recipe_env(arch)
 
         with current_directory(self.get_jni_dir()):
