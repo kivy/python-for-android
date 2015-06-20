ecfd5fa7e9e292cf18a067f06366f9888f16ff78
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-06-20 23:37:26 +0100

    Added initial setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(name='python-for-android',
+      version='0.1',
+      description='Android APK packager for Python scripts and apps',
+      author='Alexander Taylor',
+      author_email='kivy-dev@googlegroups.com',
+      url='https://github.com/inclement/p4a-experiment/', 
+      license='MIT', 
+      packages=['pythonforandroid'],
+      install_requires=['appdirs', 'colorama', 'sh'],
+      )
+
