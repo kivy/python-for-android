584e54fc668071196dd98282b322dd95329beac0
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-06-28 23:15:34 +0100

    Added preliminary apk command to cl options

--- pythonforandroid/bootstraps/pygame/build/build.py
+++ pythonforandroid/bootstraps/pygame/build/build.py
@@ -466,7 +466,8 @@ tools directory of the Android SDK.
                     help='Custom file to add at the end of the manifest')
 
     if args is None:
-        args = ap.parse_args()
+        args = sys.argv
+    args = ap.parse_args(args)
 
     if not args.dir and not args.private and not args.launcher:
         ap.error('One of --dir, --private, or --launcher must be supplied.')
