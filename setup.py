25aa9a9aa5cb879dc7c84dbff92fbafe8599fee1
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-07-01 22:34:09 +0100

    Fixed setup.py to use with MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+# NOTE: All package data is set in MANIFEST.in
 
 setup(name='python-for-android',
       version='0.1',
       description='Android APK packager for Python scripts and apps',
       author='Alexander Taylor',
       author_email='kivy-dev@googlegroups.com',
-      url='https://github.com/inclement/p4a-experiment/', 
+      url='https://github.com/inclement/python-for-android-revamp', 
       license='MIT', 
-      packages=['pythonforandroid'],
       install_requires=['appdirs', 'colorama', 'sh'],
       entry_points={
           'console_scripts': [
-              'python-for-android = pythonforandroid.toolchain:ToolchainCL'
+              'python-for-android = pythonforandroid.toolchain:ToolchainCL',
               ],
-          }
+          'distutils.commands': [
+              'apktest = pythonforandroid.toolchain:test_setuptools_entry',
+              ],
+          },
       )
-
