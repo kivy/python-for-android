42c71319cc6791b805057f74befa2b260ef2d40f
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-07-04 19:34:45 +0100

    Added argparse to setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 # NOTE: All package data is set in MANIFEST.in
 
 setup(name='python-for-android',
-      version='0.1',
+      version='0.2',
       description='Android APK packager for Python scripts and apps',
       author='Alexander Taylor',
       author_email='kivy-dev@googlegroups.com',
       url='https://github.com/inclement/python-for-android-revamp', 
       license='MIT', 
-      install_requires=['appdirs', 'colorama', 'sh', 'jinja2'],
+      install_requires=['appdirs', 'colorama', 'sh', 'jinja2', 'argparse'],
       entry_points={
           'console_scripts': [
               'python-for-android = pythonforandroid.toolchain:ToolchainCL',
