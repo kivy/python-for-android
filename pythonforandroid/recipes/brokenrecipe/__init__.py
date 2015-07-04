4c75b99d04af85ee16e88d517212acec38c8f17e
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-07-04 20:21:21 +0100

    Removed all p4a contents ready to merge revamp

---
+++ pythonforandroid/recipes/brokenrecipe/__init__.py
@@ -0,0 +1,8 @@
+from pythonforandroid.toolchain import Recipe
+
+class BrokenRecipe(Recipe):
+    def __init__(self):
+        print('This is a broken recipe, not a real one!')
+
+
+recipe = BrokenRecipe()
--- src/jni/sqlite3/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := sqlite3.c
-
-LOCAL_MODULE := sqlite3
-
-include $(BUILD_SHARED_LIBRARY)
