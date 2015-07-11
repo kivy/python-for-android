06ff8f8526e7e82090aaffefe1ca0f2dba9d5984
Mathieu Virbel <mat@kivy.org>
2015-07-12 00:33:54 +0200

    update openssl recipes to prevent not found issue

--- recipes/openssl/recipe.sh
+++ recipes/openssl/recipe.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VERSION_openssl=${VERSION_openssl:-1.0.2c}
+VERSION_openssl=${VERSION_openssl:-1.0.2d}
 URL_openssl=https://www.openssl.org/source/openssl-$VERSION_openssl.tar.gz
 DEPS_openssl=()
-MD5_openssl=8c8d81a9ae7005276e486702edbcd4b6
+MD5_openssl=38dd619b2e77cbac69b99f52a053d25a
 BUILD_openssl=$BUILD_PATH/openssl/$(get_directory $URL_openssl)
 RECIPE_openssl=$RECIPES_PATH/openssl
 
