f7f70ee65b556977c87685ebafc879e89d40d119
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-07-15 00:03:21 +0100

    Changed vispy to 0.4 and added patches

--- pythonforandroid/recipes/vispy/__init__.py
+++ pythonforandroid/recipes/vispy/__init__.py
@@ -6,9 +6,20 @@ import glob
 
 
 class VispyRecipe(PythonRecipe):
-    version = 'master'
-    url = 'https://github.com/vispy/vispy/archive/{version}.zip'
+    version = '0.4.0'
+    url = 'https://github.com/vispy/vispy/archive/v{version}.tar.gz'
 
     depends = ['python2', 'numpy']
+
+    def prebuild_arch(self, arch):
+        super(VispyRecipe, self).prebuild_arch(arch)
+        build_dir = self.get_build_dir(arch.arch)
+        if exists(join(build_dir, '.patched')):
+            print('vispy already patched, skipping')
+            return
+        self.apply_patch('disable_freetype.patch')
+        self.apply_patch('disable_font_triage.patch')
+        self.apply_patch('use_es2.patch')
+        shprint(sh.touch, join(build_dir, '.patched'))
 
 recipe = VispyRecipe()
