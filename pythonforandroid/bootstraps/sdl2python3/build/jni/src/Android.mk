358c6bb18f4586a2676ea653f85345536690d1b7
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-07-19 15:16:25 +0100

    Added bootstrap and recipes for python3 (unfinished!)

---
+++ pythonforandroid/bootstraps/sdl2python3/build/jni/src/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := main
+
+SDL_PATH := ../SDL
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+
+# Add your application source files here...
+LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
+	start.c
+
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/../../../../python-install/include/python3.4m
+
+LOCAL_SHARED_LIBRARIES := SDL2
+
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog -lpython3.4m
+
+LOCAL_LDFLAGS += -L$(LOCAL_PATH)/../../../../python-install/lib $(APPLICATION_ADDITIONAL_LDFLAGS)
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ pythonforandroid/recipes/python3/__init__.py
@@ -0,0 +1,162 @@
+
+from pythonforandroid.toolchain import Recipe, shprint, get_directory, current_directory, ArchAndroid
+from os.path import exists, join
+from os import uname
+import glob
+import sh
+
+class Python3Recipe(Recipe):
+    version = '3.4.2'
+    url = 'http://python.org/ftp/python/{version}/Python-{version}.tgz'
+    name = 'python3'
+
+    depends = ['hostpython3']  
+
+    def prebuild_armeabi(self):
+        build_dir = self.get_build_container_dir('armeabi')
+        if exists(join(build_dir, '.patched')):
+            print('Python3 already patched, skipping.')
+            return
+
+        self.apply_patch(join('patches', 'python-{version}-android-libmpdec.patch'.format(version=self.version)))
+        self.apply_patch(join('patches', 'python-{version}-android-locale.patch'.format(version=self.version)))
+        self.apply_patch(join('patches', 'python-{version}-android-misc.patch'.format(version=self.version)))
+        # self.apply_patch(join('patches', 'python-{version}-android-missing-getdents64-definition.patch'.format(version=self.version)))
+        self.apply_patch(join('patches', 'python-{version}-cross-compile.patch'.format(version=self.version)))
+        self.apply_patch(join('patches', 'python-{version}-python-misc.patch'.format(version=self.version)))
+
+        shprint(sh.touch, join(build_dir, '.patched'))
+
+    def build_armeabi(self):
+        if 'sqlite' in self.ctx.recipe_build_order or 'openssl' in self.ctx.recipe_build_order:
+            print('sqlite or openssl support not yet enabled in python recipe')
+            exit(1)
+
+        hostpython_recipe = Recipe.get_recipe('hostpython3', self.ctx)
+        shprint(sh.cp, self.ctx.hostpython, self.get_build_dir('armeabi'))
+        shprint(sh.cp, self.ctx.hostpgen, self.get_build_dir('armeabi'))
+        hostpython = join(self.get_build_dir('armeabi'), 'hostpython')
+        hostpgen = join(self.get_build_dir('armeabi'), 'hostpython')
+
+        if exists(join(self.get_build_dir('armeabi'), 'libpython3.4m.so')):
+            print('libpython3.4m.so already exists, skipping python build.')
+            self.ctx.hostpython = join(self.ctx.build_dir, 'python-install',
+                                       'bin', 'python.host')
+
+            return
+
+        with current_directory(self.get_build_dir('armeabi')):
+
+
+            hostpython_recipe = Recipe.get_recipe('hostpython3', self.ctx)
+            # shprint(sh.cp, join(hostpython_recipe.get_recipe_dir(), 'Setup'), 'Modules')
+
+            env = ArchAndroid(self.ctx).get_env()
+
+            # AND: Should probably move these to get_recipe_env for
+            # neatness, but the whole recipe needs tidying along these
+            # lines
+            env['HOSTARCH'] = 'arm-eabi'
+            env['BUILDARCH'] = shprint(sh.gcc, '-dumpmachine').stdout
+            # env['CFLAGS'] = ' '.join([env['CFLAGS'], '-DNO_MALLINFO'])
+
+            env['HOSTARCH'] = 'arm-linux-androideabi'
+            env['BUILDARCH'] = 'x86_64-pc-linux-gnu'
+
+            configure = sh.Command('./configure')
+
+            # AND: OFLAG isn't actually set, should it be?
+            # shprint(configure,
+            #         '--host={}'.format(env['HOSTARCH']),
+            #         '--build={}'.format(env['BUILDARCH']),
+            #         # 'OPT={}'.format(env['OFLAG']),
+            #         '--prefix={}'.format(join(self.ctx.build_dir, 'python-install')),
+            #         '--enable-shared',
+            #         '--disable-toolbox-glue',
+            #         '--disable-framefork',
+            #         _env=env)
+
+            with open('config.site', 'w') as fileh:
+                fileh.write('''
+    ac_cv_file__dev_ptmx=no
+    ac_cv_file__dev_ptc=no
+                ''')
+
+            shprint(configure,
+                    'CROSS_COMPILE_TARGET=yes',
+                    'HOSTPYTHON={}'.format(hostpython),
+                    'CONFIG_SITE=config.site',
+                    '--prefix={}'.format(join(self.ctx.build_dir, 'python-install')),
+                    '--host={}'.format(env['HOSTARCH']),
+                    '--build={}'.format(env['BUILDARCH']),
+                    '--disable-ipv6',
+                    '--enable-shared',
+                    '--without-ensurepip',
+                    _env=env)
+
+            # The python2 build always fails the first time, but python3 seems to work.
+
+            make = sh.Command(env['MAKE'].split(' ')[0])
+            # print('First install (expected to fail...')
+            # try:
+            #     shprint(make, '-j5', 'install', 'HOSTPYTHON={}'.format(hostpython),
+            #             'HOSTPGEN={}'.format(hostpgen),
+            #             'CROSS_COMPILE_TARGET=yes',
+            #             'INSTSONAME=libpython3.7.so',
+            #             _env=env)
+            # except sh.ErrorReturnCode_2:
+            #     print('First python3 make failed. This is expected, trying again.')
+                
+
+            # print('Second install (expected to work)')
+            shprint(sh.touch, 'python.exe', 'python')
+            # shprint(make, '-j5', 'install', 'HOSTPYTHON={}'.format(hostpython),
+            #         'HOSTPGEN={}'.format(hostpgen),
+            #         'CROSS_COMPILE_TARGET=yes',
+            #         'INSTSONAME=libpython3.7.so',
+            #         _env=env)
+
+            shprint(make, '-j5',
+                    'CROSS_COMPILE_TARGET=yes',
+                    'HOSTPYTHON={}'.format(hostpython),
+                    'HOSTPGEN={}'.format(hostpgen),
+                    _env=env)
+
+            shprint(make, '-j5', 'install',
+                    'CROSS_COMPILE_TARGET=yes',
+                    'HOSTPYTHON={}'.format(hostpython),
+                    'HOSTPGEN={}'.format(hostpgen),
+                    _env=env)
+
+            # if uname()[0] == 'Darwin':
+            #     shprint(sh.cp, join(self.get_recipe_dir(), 'patches', '_scproxy.py'),
+            #             join(self.get_build_dir(), 'Lib'))
+            #     shprint(sh.cp, join(self.get_recipe_dir(), 'patches', '_scproxy.py'),
+            #             join(self.ctx.build_dir, 'python-install', 'lib', 'python3.7'))
+
+            print('Ready to copy .so for python arm')
+            shprint(sh.cp, 'libpython3.4m.so', self.ctx.libs_dir)
+            shprint(sh.cp, 'libpython3.so', self.ctx.libs_dir)
+
+            print('Copying hostpython binary to targetpython folder')
+            shprint(sh.cp, self.ctx.hostpython,
+                    join(self.ctx.build_dir, 'python-install', 'bin',
+                         'python.host'))
+            self.ctx.hostpython = join(self.ctx.build_dir, 'python-install',
+                                       'bin', 'python.host')
+
+
+            # reduce python?
+            for dir_name in ('test', join('json', 'tests'), 'lib-tk',
+                             join('sqlite3', 'test'), join('unittest, test'),
+                             join('lib2to3', 'tests'), join('bsddb', 'tests'),
+                             join('distutils', 'tests'), join('email', 'test'),
+                             'curses'):
+                shprint(sh.rm, '-rf', join(self.ctx.build_dir, 'python-install',
+                                           'lib', 'python3.7', dir_name))
+
+        # print('python3 build done, exiting for debug')
+        # exit(1)
+
+
+recipe = Python3Recipe()
