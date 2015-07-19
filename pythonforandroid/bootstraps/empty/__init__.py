20c6ee23bd39bf9a8a60c8bec1dd6fcebb500c53
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-07-19 15:16:52 +0100

    Added empty bootstrap for testing recipes

---
+++ pythonforandroid/bootstraps/empty/__init__.py
@@ -0,0 +1,16 @@
+from pythonforandroid.toolchain import Bootstrap, shprint, current_directory, info, warning, ArchAndroid, logger, info_main, which
+from os.path import join, exists
+from os import walk
+import glob
+import sh
+
+class EmptyBootstrap(Bootstrap):
+    name = 'empty'
+
+    recipe_depends = []
+
+    def run_distribute(self):
+        print('empty bootstrap has no distribute')
+        exit(1)
+
+bootstrap = EmptyBootstrap()
