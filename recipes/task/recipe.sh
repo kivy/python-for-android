b50f9b1c0e52e38eb22bc9c4a7f23cdde6ea9424
gabriel pettier <dev@tangibledisplay.com>
2015-08-15 01:04:42 +0200

    task recipe switch to -fPIC not PIE

--- recipes/task/recipe.sh
+++ recipes/task/recipe.sh
@@ -43,7 +43,7 @@ function build_task() {
 	cmake -DCMAKE_TOOLCHAIN_FILE=android-cmake/android.toolchain.cmake \
 	      -DUUID_INCLUDE_DIR=$BUILD_libuuid/build/include \
 	      -DUUID_LIBRARY=$BUILD_libuuid/build/lib/libuuid.a \
-	      -DCMAKE_CXX_FLAGS="-fPIE" \
+	      -DCMAKE_CXX_FLAGS="-fPIC" \
 	      -DCMAKE_EXE_LINKER_FLAGS="-pie" \
 	      -DANDROID_NDK=$ANDROIDNDK \
 	      -DCMAKE_BUILD_TYPE=Release \
