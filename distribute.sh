190f3850b24edccfb41556418e5dbe90dbd8d499
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-09-22 21:49:50 +0100

    Wrapped text in distribute.sh error

--- distribute.sh
+++ distribute.sh
@@ -10,14 +10,23 @@
 
 cat <<EOF
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 ### ERROR ###
-You are trying to run an old version of python-for-android via distribute.sh. However, python-for-android has been rewritten and no longer supports the distribute.sh interface.
+You are trying to run an old version of python-for-android via
+distribute.sh. However, python-for-android has been rewritten and no
+longer supports the distribute.sh interface.
 
 If you are using buildozer, you should:
 - upgrade buildozer to the latest version (at least 0.30)
 - delete the .buildozer folder in your app directory (the same directory that has your buildozer.spec)
 - run buildozer again as normal
 
-If you are not using buildozer, see https://github.com/kivy/python-for-android/blob/master/README.md for instructions on using the new python-for-android toolchain. Alternatively, you can get the old toolchain from the 'old_toolchain' branch at https://github.com/kivy/python-for-android/tree/old_toolchain .
+If you are not using buildozer, see
+https://github.com/kivy/python-for-android/blob/master/README.md for
+instructions on using the new python-for-android
+toolchain. Alternatively, you can get the old toolchain from the
+'old_toolchain' branch at
+https://github.com/kivy/python-for-android/tree/old_toolchain .
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 EOF
