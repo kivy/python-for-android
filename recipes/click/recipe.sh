f8c89a1acdd1de9a7d595377158e473b0c3db492
Liu Chao <lc-soft@live.cn>
2015-09-25 14:00:34 +0800

    add flask and flask depend packages.
    the `setup.py` file can not be used directly in these packages,
     In `from setuptools import  setup`, there will be error when package building ,
    this code need change into `from distutils.core import setup` can solve errors..

---
+++ recipes/click/recipe.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VERSION_click=${VERSION_click:-master}
+DEPS_click=(python)
+URL_click=https://github.com/mitsuhiko/click/archive/$VERSION_click.zip
+MD5_click=
+BUILD_click=$BUILD_PATH/click/$(get_directory $URL_click)
+RECIPE_click=$RECIPES_PATH/click
+
+function prebuild_click() {
+	true
+}
+
+function shouldbuild_click() {
+	if [ -d "$SITEPACKAGES_PATH/click" ]; then
+		DO_BUILD=0
+	fi
+}
+
+function build_click() {
+	cd $BUILD_click
+
+	push_arm
+	try $HOSTPYTHON setup.py install
+	pop_arm
+}
+
+function postbuild_click() {
+	true
+}
+
---
+++ recipes/markupsafe/recipe.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VERSION_markupsafe=${VERSION_markupsafe:-master}
+DEPS_markupsafe=(python)
+URL_markupsafe=https://github.com/mitsuhiko/markupsafe/archive/$VERSION_markupsafe.zip
+MD5_markupsafe=
+BUILD_markupsafe=$BUILD_PATH/markupsafe/$(get_directory $URL_markupsafe)
+RECIPE_markupsafe=$RECIPES_PATH/markupsafe
+
+function prebuild_markupsafe() {
+	true
+}
+
+function shouldbuild_markupsafe() {
+	if [ -d "$SITEPACKAGES_PATH/markupsafe" ]; then
+		DO_BUILD=0
+	fi
+}
+
+function build_markupsafe() {
+	cd $BUILD_markupsafe
+
+	push_arm
+	try $HOSTPYTHON setup.py install
+	pop_arm
+}
+
+function postbuild_markupsafe() {
+	true
+}
+
