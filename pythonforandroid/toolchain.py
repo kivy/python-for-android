c755662870abff537fde90ed078dcbb97ba043e9
Denys Duchier <denys.duchier@univ-orleans.fr>
2015-11-19 18:41:52 +0100

    suppress colorization if stdout is not a tty

--- pythonforandroid/toolchain.py
+++ pythonforandroid/toolchain.py
@@ -41,7 +41,16 @@ except ImportError:
 import argparse
 from appdirs import user_data_dir
 import sh
-from colorama import Style, Fore
+if sys.stdout.isatty():
+    from colorama import Style, Fore
+else:
+    from collections import defaultdict
+    class colorama_shim(object):
+        def __init__(self):
+            self._dict = defaultdict(str)
+        def __getattr__(self, key):
+            return self._dict[key]
+    Style = Fore = colorama_shim()
 
 user_dir = dirname(realpath(os.path.curdir))
 toolchain_dir = dirname(__file__)
