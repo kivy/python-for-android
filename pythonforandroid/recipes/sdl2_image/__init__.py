53e063422107af17e5187dcfcd574b26d0bbd8d3
Alexander Taylor <alexanderjohntaylor@gmail.com>
2015-11-30 23:04:05 +0000

    Made jpg disable only occur with x86 build

--- pythonforandroid/recipes/sdl2_image/__init__.py
+++ pythonforandroid/recipes/sdl2_image/__init__.py
@@ -14,7 +14,8 @@ class LibSDL2Image(NDKRecipe):
             info('SDL2_image already patched, skipping')
             return
         self.apply_patch('disable_webp.patch', arch.arch)
-        self.apply_patch('disable_jpg.patch', arch.arch)
+        if arch.arch == 'x86':
+            self.apply_patch('disable_jpg.patch', arch.arch)
         shprint(sh.touch, join(build_dir, '.patched'))
 
 recipe = LibSDL2Image()
