8f4b996c17d49202c71bcf46013466615ad47fbd
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-01-09 23:21:00 +0000

    Removed unnecessary python3 bootstraps

--- pythonforandroid/bootstraps/sdl2python3crystax/build/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-
-# Uncomment this if you're using STL in your project
-# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
-# APP_STL := stlport_static 
-
-# APP_ABI := armeabi armeabi-v7a x86
-APP_ABI := $(ARCH)
