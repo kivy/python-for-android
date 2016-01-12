23ae7d73c82b4027a2fda61d611ba83aa771b17f
Ryan Pessa <ryan@essential-elements.net>
2016-01-12 17:15:09 -0600

    add babel and pytz recipes

---
+++ pythonforandroid/recipes/pytz/__init__.py
@@ -0,0 +1,15 @@
+from pythonforandroid.recipe import PythonRecipe
+
+
+class PytzRecipe(PythonRecipe):
+	name = 'pytz'
+	version = '2015.7'
+	url = 'https://pypi.python.org/packages/source/p/pytz/pytz-{version}.tar.bz2'
+
+	depends = [('python2', 'python3')]
+
+	call_hostpython_via_targetpython = False
+	install_in_hostpython = True
+
+
+recipe = PytzRecipe()
