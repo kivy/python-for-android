792d76a6370900ea9504836dc9a74d2f25664e17
Ryan Pessa <ryan@essential-elements.net>
2016-01-12 16:47:22 -0600

    add py3 support to PythonUtil load

--- pythonforandroid/bootstraps/sdl2/build/src/org/kivy/android/PythonUtil.java
+++ pythonforandroid/bootstraps/sdl2/build/src/org/kivy/android/PythonUtil.java
@@ -14,25 +14,41 @@ public class PythonUtil {
             "SDL2_image",
             "SDL2_mixer",
             "SDL2_ttf",
-            "python2.7",
             "main"
         };
     }
 
 	public static void loadLibraries(File filesDir) {
 		for (String lib : getLibraries()) {
-			System.loadLibrary(lib);
-		}
+            System.loadLibrary(lib);
+        }
 
-		System.load(filesDir + "/lib/python2.7/lib-dynload/_io.so");
-		System.load(filesDir + "/lib/python2.7/lib-dynload/unicodedata.so");
+        try {
+            System.loadLibrary("python2.7");
+        } catch(UnsatisfiedLinkError e) {
+            Log.v(TAG, "Failed to load libpython2.7");
+        }
+        
+        try {
+            System.loadLibrary("python3.5m");
+        } catch(UnsatisfiedLinkError e) {
+            Log.v(TAG, "Failed to load libpython3.5m");
+        }
+        
+        try {
+            System.load(getFilesDir() + "/lib/python2.7/lib-dynload/_io.so");
+            System.load(getFilesDir() + "/lib/python2.7/lib-dynload/unicodedata.so");
+        } catch(UnsatisfiedLinkError e) {
+            Log.v(TAG, "Failed to load _io.so or unicodedata.so...but that's okay.");
+        }
+        
+        try {
+            // System.loadLibrary("ctypes");
+            System.load(getFilesDir() + "/lib/python2.7/lib-dynload/_ctypes.so");
+        } catch(UnsatisfiedLinkError e) {
+            Log.v(TAG, "Unsatisfied linker when loading ctypes");
+        }
 
-		try {
-			System.load(filesDir + "/lib/python2.7/lib-dynload/_ctypes.so");
-		} catch(UnsatisfiedLinkError e) {
-			Log.v(TAG, "Unsatisfied linker when loading ctypes");
-		}
-
-		Log.v(TAG, "Loaded everything!");
+        Log.v(TAG, "Loaded everything!");
 	}
 }
