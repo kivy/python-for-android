73b51ac8245aebaa89f523d06dfaa8f58207dc10
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-01-20 20:12:34 +0000

    Added six to install_requires

--- setup.py
+++ setup.py
@@ -46,7 +46,8 @@ setup(name='python-for-android',
       author_email='kivy-dev@googlegroups.com',
       url='https://github.com/kivy/python-for-android', 
       license='MIT', 
-      install_requires=['appdirs', 'colorama>0.3', 'sh', 'jinja2', 'argparse'],
+      install_requires=['appdirs', 'colorama>0.3', 'sh', 'jinja2', 'argparse',
+                        'six'],
       entry_points={
           'console_scripts': [
               'python-for-android = pythonforandroid.toolchain:main',
