2c7e384c5ac1e87c9b42daf1e65142cd0b1c610d
Paul Brussee <brussee@live.nl>
2016-03-15 23:00:40 +0100

    add recipes python includes and linker flags

--- pythonforandroid/recipes/pyjnius/__init__.py
+++ pythonforandroid/recipes/pyjnius/__init__.py
@@ -10,6 +10,7 @@ class PyjniusRecipe(CythonRecipe):
     url = 'https://github.com/kivy/pyjnius/archive/{version}.zip'
     name = 'pyjnius'
     depends = [('python2', 'python3crystax'), ('sdl2', 'sdl'), 'six']
+    call_hostpython_via_targetpython = False
     site_packages_name = 'jnius'
 
     patches = [('sdl2_jnienv_getter.patch', will_build('sdl2'))]
@@ -20,5 +21,12 @@ class PyjniusRecipe(CythonRecipe):
         with current_directory(self.get_build_dir(arch.arch)):
             shprint(sh.cp, '-a', join('jnius', 'src', 'org'), self.ctx.javaclass_dir)
 
+    def get_recipe_env(self, arch=None):
+        env = super(PyjniusRecipe, self).get_recipe_env(arch)
+        env['PYTHON_ROOT'] = self.ctx.get_python_install_dir()
+        env['CFLAGS'] += ' -I' + env['PYTHON_ROOT'] + '/include/python2.7'
+        env['LDFLAGS'] += ' -L' + env['PYTHON_ROOT'] + '/lib' + \
+                          ' -lpython2.7'
+        return env
 
 recipe = PyjniusRecipe()
--- pythonforandroid/recipes/zope/__init__.py
+++ pythonforandroid/recipes/zope/__init__.py
@@ -1,27 +1,18 @@
-
-from pythonforandroid.toolchain import PythonRecipe, shprint, current_directory
-from os.path import exists, join
-import sh
-import glob
+from pythonforandroid.toolchain import PythonRecipe
 
 class ZopeRecipe(PythonRecipe):
     name = 'zope'
     version = '4.1.2'
     url = 'http://pypi.python.org/packages/source/z/zope.interface/zope.interface-{version}.tar.gz'
-
     depends = ['python2']
-    
+    call_hostpython_via_targetpython = False
+
     def get_recipe_env(self, arch):
         env = super(ZopeRecipe, self).get_recipe_env(arch)
-
-        # These are in the old zope recipe but seem like they shouldn't actually be necessary
-        env['LDFLAGS'] = env['LDFLAGS'] + ' -L{}'.format(
-            self.ctx.get_libs_dir(arch.arch))
-        env['LDSHARED'] = join(self.ctx.root_dir, 'tools', 'liblink')
-
-    def postbuild_arch(self, arch):
-        super(ZopeRecipe, self).postbuild_arch(arch)
-
-        # Should do some deleting here
+        env['PYTHON_ROOT'] = self.ctx.get_python_install_dir()
+        env['CFLAGS'] += ' -I' + env['PYTHON_ROOT'] + '/include/python2.7'
+        env['LDFLAGS'] += ' -L' + env['PYTHON_ROOT'] + '/lib' + \
+                          ' -lpython2.7'
+        return env
 
 recipe = ZopeRecipe()
