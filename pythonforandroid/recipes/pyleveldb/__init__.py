3dc81ea6c8e10886e9aa91a18d1a4cb54c5968e6
Paul Brussee <brussee@live.nl>
2016-04-15 23:04:39 +0200

    enable snappy recipe to replace source files;

--- pythonforandroid/recipes/pyleveldb/__init__.py
+++ pythonforandroid/recipes/pyleveldb/__init__.py
@@ -5,7 +5,7 @@ import sh
 class PyLevelDBRecipe(CompiledComponentsPythonRecipe):
     version = '0.193'
     url = 'https://pypi.python.org/packages/source/l/leveldb/leveldb-{version}.tar.gz'
-    depends = ['leveldb', 'hostpython2', 'python2', 'setuptools']
+    depends = ['snappy', 'leveldb', 'hostpython2', 'python2', 'setuptools']
     patches = ['bindings-only.patch']
     call_hostpython_via_targetpython = False # Due to setuptools
     site_packages_name = 'leveldb'
