a99fde346e58b7451cd2c5294c5a6ad3dadc9055
Pol Canelles <pol@canellestudi.com>
2016-04-16 23:05:43 +0200

    Solves issues with pil

--- pythonforandroid/recipes/pil/__init__.py
+++ pythonforandroid/recipes/pil/__init__.py
@@ -21,27 +21,39 @@ class PILRecipe(CompiledComponentsPythonRecipe):
         env['LDFLAGS'] += ' -L' + env['PYTHON_ROOT'] + '/lib' + \
                           ' -lpython2.7'
 
+        # PNG FLAGS
         png = self.get_recipe('png', self.ctx)
         png_lib_dir = png.get_lib_dir(arch)
-        png_jni_dir = png.get_jni_dir(arch)
+        png_inc_dir = png.get_include_dir(arch)
+        cflags = ' -I{} -L{}'.format(png_inc_dir, png_lib_dir)
+
+        # JPEG TURBO FLAGS
         jpeg = self.get_recipe('jpeg', self.ctx)
         jpeg_lib_dir = jpeg.get_lib_dir(arch)
-        if 'pygame' in self.ctx.recipe_build_order:
-            jpeg_jni_dir = join(jpeg.get_jni_dir(arch), 'jpeg')
-        else:
-            jpeg_jni_dir = jpeg.get_jni_dir(arch)
+        jpeg_jni_dir = jpeg.get_jni_dir(arch)
+        cflags += ' -I{} -I{} -I{} -L{}'.format(
+            jpeg_jni_dir, join(jpeg_jni_dir, 'android'),
+            join(jpeg_jni_dir, 'simd'), jpeg_lib_dir)
+
+        # FREETYPE FLAGS
         free = self.get_recipe('freetype', self.ctx)
         free_lib_dir = free.get_lib_dir(arch)
         free_inc_dir = join(free.get_build_dir(arch.arch), 'include')
+        cflags = ' -I{} -L{}'.format(free_inc_dir, free_lib_dir)
+        if 'harfbuzz' in self.ctx.recipe_build_order:
+            free_install = join(free.get_build_dir(arch.arch), 'install')
+            harf = self.get_recipe('harfbuzz', self.ctx)
+            harf_lib_dir = harf.get_lib_dir(arch)
+            harf_inc_dir = harf.get_build_dir(arch.arch)
+            cflags += ' -I{} -I{} -I{} -L{}'.format(
+                harf_inc_dir, join(harf_inc_dir, 'src'),
+                join(free_install, 'include'), harf_lib_dir)
 
         env['JPEG_ROOT'] = '{}|{}'.format(jpeg_lib_dir, jpeg_jni_dir)
         env['FREETYPE_ROOT'] = '{}|{}'.format(free_lib_dir, free_inc_dir)
         info('JPEG_ROOT: {}'.format(env['JPEG_ROOT']))
         info('FREETYPE_ROOT: {}'.format(env['FREETYPE_ROOT']))
 
-        cflags = ' -I{} -L{} -I{} -L{} -I{} -L{}'.format(
-            png_jni_dir, png_lib_dir, jpeg_jni_dir,
-            jpeg_lib_dir, free_inc_dir, free_lib_dir)
         env['CFLAGS'] += cflags
         env['CXXFLAGS'] += cflags
         env['CC'] += cflags
