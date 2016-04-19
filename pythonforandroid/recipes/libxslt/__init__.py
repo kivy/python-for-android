29aaaa4189af454be3685900a7d89d12eaac8c53
Zachary Goldberg <zach@zachgoldberg.com>
2016-04-19 16:59:24 -0700

    Libxslt finally builds

--- pythonforandroid/recipes/libxslt/__init__.py
+++ pythonforandroid/recipes/libxslt/__init__.py
@@ -1,5 +1,5 @@
 from pythonforandroid.toolchain import Recipe, shprint, shutil, current_directory
-from os.path import exists, join
+from os.path import exists, join, dirname, basename
 import sh
 
 class LibxsltRecipe(Recipe):
@@ -16,18 +16,21 @@ class LibxsltRecipe(Recipe):
         super(LibxsltRecipe, self).build_arch(arch)
         env = self.get_recipe_env(arch)
         with current_directory(self.get_build_dir(arch.arch)):
-            bash = sh.Command('bash')
-            shprint(bash, 'configure',
+            # If the build is done with /bin/sh things blow up,
+            # try really hard to use bash
+            libxml = dirname(dirname(self.get_build_container_dir(arch.arch))) + "/libxml2/%s/libxml2" % arch.arch
+            shprint(sh.Command('./configure'),
                     '--build=i686-pc-linux-gnu', '--host=arm-linux-eabi',
                     '--without-plugins', '--without-debug', '--without-python', '--without-crypto',
-                    '--with-libxml-src=../../../libxml2/armeabi/libxml2/')
+                    '--with-libxml-src=%s' % libxml)
             shprint(sh.make, _env=env)
-            shutil.copyfile('src/libxslt/.libs/libxslt.a', join(self.ctx.get_libs_dir(arch.arch), 'libxslt.a'))
+            shutil.copyfile('libxslt/.libs/libxslt.a', join(self.ctx.get_libs_dir(arch.arch), 'libxslt.a'))
 
 
     def get_recipe_env(self, arch):
         env = super(LibxsltRecipe, self).get_recipe_env(arch)
-        env['CFLAGS'] += ' -Os'
+        env['CONFIG_SHELL'] = '/bin/bash'
+        env['SHELL'] = '/bin/bash'
         return env
 
 recipe = LibxsltRecipe()
