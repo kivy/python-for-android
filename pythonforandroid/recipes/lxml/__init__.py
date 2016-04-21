949b530616c98c677b22934eb4a46f8b3a106fd2
Zachary Goldberg <zach@zachgoldberg.com>
2016-04-20 23:27:11 -0700

    Copy the .so at the right time

--- pythonforandroid/recipes/lxml/__init__.py
+++ pythonforandroid/recipes/lxml/__init__.py
@@ -17,7 +17,12 @@ class LXMLRecipe(CompiledComponentsPythonRecipe):
     def should_build(self, arch):
         super(LXMLRecipe, self).should_build(arch)
         return True
-        return not exists(join(self.ctx.get_libs_dir(arch.arch), 'liblxml.so'))
+        return not exists(join(self.ctx.get_libs_dir(arch.arch), 'etree.so'))
+
+    def build_arch(self, arch):
+        super(LXMLRecipe, self).build_arch(arch)
+        shutil.copyfile('%s/build/lib.linux-x86_64-2.7/lxml/etree.so' % self.get_build_dir(arch.arch), join(self.ctx.get_libs_dir(arch.arch), 'etree.so'))
+        shutil.copyfile('%s/build/lib.linux-x86_64-2.7/lxml/objectify.so' % self.get_build_dir(arch.arch), join(self.ctx.get_libs_dir(arch.arch), 'objectify.so'))
 
     def get_recipe_env(self, arch):
         env = super(LXMLRecipe, self).get_recipe_env(arch)
@@ -26,12 +31,6 @@ class LXMLRecipe(CompiledComponentsPythonRecipe):
         targetpython = "%s/include/python2.7/" % dirname(dirname(self.ctx.hostpython))
         env['CC'] += " -I%s/include -I%s -I%s" % (bxml, bxsl, targetpython)
         env['LDSHARED'] = '%s -lpython2.7 -nostartfiles' % env['CC']
-        #env['LDSHARED'] = join(self.ctx.root_dir, 'tools', 'liblink')
-        # WTH is liblink?
-        #env['LDSHARED'] = env['LIBLINK']
-        # This linking almost works.  Going to have to muck with LDSHARED to
-        # include the libs_collection folder
-        #zgoldberg@badass:~/.local/share/python-for-android/build/other_builds/lxml/armeabi/lxml (master)$ /usr/bin/ccache arm-linux-androideabi-ld -L/home/zgoldberg/.local/share/python-for-android/build/other_builds/libxml2/armeabi/libxml2//include -L/home/zgoldberg/.local/share/python-for-android/build/other_builds/libxslt/armeabi/libxslt -L/home/zgoldberg/android-sdks/ndk-bundle/platforms/android-16/arch-arm/usr/lib/ -L/home/zgoldberg/android-sdks/ndk-bundle/platforms/android-16/arch-arm/usr/lib/ -L/home/zgoldberg/.local/share/python-for-android/build/libs_collections/peggo-python/armeabi build/temp.linux-x86_64-2.7/src/lxml/lxml.objectify.o -lxslt -lxml2 -lpython2.7 -lz -lm -o build/lib.linux-x86_64-2.7/lxml/objectify.soi
 
         return env
 
