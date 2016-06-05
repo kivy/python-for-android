e96a6d72a789b87f8405d6c7805738b4a0027889
Alexander Taylor <alexanderjohntaylor@gmail.com>
2016-06-05 23:25:35 +0100

    Added package_data to testapp_setup

--- testapps/testapp_setup/setup.py
+++ testapps/testapp_setup/setup.py
@@ -4,10 +4,18 @@ from pythonforandroid.bdist_apk import register_args
 register_args('--requirements=sdl2,pyjnius,kivy,python2',
               '--android-api=19',
               '--ndk-dir=/home/asandy/android/crystax-ndk-10.3.1',
-              '--dist-name=bdisttest')
+              '--dist-name=bdisttest',
+              '--ndk-version=10.3.1')
 
 from setuptools import setup, find_packages
 from distutils.extension import Extension
+
+package_data = {'': ['*.py',
+                     '*.png']
+                }
+
+packages = find_packages()
+print('packages are', packages)
 
 setup(
     name='testapp_setup',
@@ -15,4 +23,6 @@ setup(
     description='p4a setup.py test',
     author='Alexander Taylor',
     author_email='alexanderjohntaylor@gmail.com',
+    packages=find_packages(),
+    package_data={'testapp': ['*.py', '*.png']}
 )
