cfd7fb15310366888d995075542899d69332f1da
Paul Brussee <brussee@live.nl>
2016-06-15 17:40:48 +0200

    update recipe twisted to 15.5.0 and remove tests

--- pythonforandroid/recipes/twisted/__init__.py
+++ pythonforandroid/recipes/twisted/__init__.py
@@ -1,18 +1,10 @@
-
-import glob
-from pythonforandroid.toolchain import (
-    CythonRecipe,
-    Recipe,
-    current_directory,
-    info,
-    shprint,
-)
+from pythonforandroid.toolchain import CythonRecipe, shprint, current_directory
 from os.path import join
 import sh
 
 
 class TwistedRecipe(CythonRecipe):
-    version = '15.4.0'
+    version = '15.5.0'
     url = 'https://pypi.python.org/packages/source/T/Twisted/Twisted-{version}.tar.bz2'
 
     depends = ['setuptools', 'zope_interface']
@@ -23,7 +15,33 @@ class TwistedRecipe(CythonRecipe):
     def prebuild_arch(self, arch):
         super(TwistedRecipe, self).prebuild_arch(arch)
         # TODO Need to whitelist tty.pyo and termios.so here
-        print('Should remove twisted tests etc. here, but skipping for now')
+        with current_directory(self.get_build_dir(arch.arch)):
+            # Remove all tests
+            sh.rm('-rf',
+                  'twisted/conch/test',
+                  'twisted/cred/test',
+                  'twisted/names/test',
+                  'twisted/mail/test',
+                  'twisted/_threads/test',
+                  'twisted/internet/test',
+                  'twisted/runner/test',
+                  'twisted/trial/test',
+                  'twisted/trial/_dist/test',
+                  'twisted/logger/test',
+                  'twisted/test',
+                  'twisted/persisted/test',
+                  'twisted/words/test',
+                  'twisted/web/test',
+                  'twisted/pair/test',
+                  'twisted/application/test',
+                  'twisted/python/test',
+                  'twisted/protocols/test',
+                  'twisted/manhole/ui/test',
+                  'twisted/manhole/test',
+                  'twisted/news/test',
+                  'twisted/positioning/test',
+                  'twisted/scripts/test'
+            )
 
     def get_recipe_env(self, arch):
         env = super(TwistedRecipe, self).get_recipe_env(arch)
@@ -33,5 +51,6 @@ class TwistedRecipe(CythonRecipe):
             env['BUILDLIB_PATH'],
         ])
         return env
+
 
 recipe = TwistedRecipe()
