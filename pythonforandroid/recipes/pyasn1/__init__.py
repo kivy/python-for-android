0041ad9b0b0e4877c0c40603c99c4463d4926537
Pol Canelles <pol@canellestudi.com>
2016-06-16 13:12:37 +0200

    Removes the include and link paths for python2

--- pythonforandroid/recipes/pyasn1/__init__.py
+++ pythonforandroid/recipes/pyasn1/__init__.py
@@ -6,12 +6,4 @@ class PyASN1Recipe(PythonRecipe):
     depends = ['python2']
     call_hostpython_via_targetpython = False
 
-    def get_recipe_env(self, arch):
-        env = super(PyASN1Recipe, self).get_recipe_env(arch)
-        env['PYTHON_ROOT'] = self.ctx.get_python_install_dir()
-        env['CFLAGS'] += ' -I' + env['PYTHON_ROOT'] + '/include/python2.7'
-        env['LDFLAGS'] += ' -L' + env['PYTHON_ROOT'] + '/lib' + \
-                          ' -lpython2.7'
-        return env
-
 recipe = PyASN1Recipe()
