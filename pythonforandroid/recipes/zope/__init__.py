3c6aa92dfa8dfefab4282f3317d1672a610e9249
Pol Canelles <pol@canellestudi.com>
2016-06-16 13:00:46 +0200

    Removes the include and link paths for python2

--- pythonforandroid/recipes/zope/__init__.py
+++ pythonforandroid/recipes/zope/__init__.py
@@ -7,12 +7,4 @@ class ZopeRecipe(PythonRecipe):
     depends = ['python2']
     call_hostpython_via_targetpython = False
 
-    def get_recipe_env(self, arch):
-        env = super(ZopeRecipe, self).get_recipe_env(arch)
-        env['PYTHON_ROOT'] = self.ctx.get_python_install_dir()
-        env['CFLAGS'] += ' -I' + env['PYTHON_ROOT'] + '/include/python2.7'
-        env['LDFLAGS'] += ' -L' + env['PYTHON_ROOT'] + '/lib' + \
-                          ' -lpython2.7'
-        return env
-
 recipe = ZopeRecipe()
