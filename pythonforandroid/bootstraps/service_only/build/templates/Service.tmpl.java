842aedbbe03c756ca37a4ac845c9dcc6f864fc6a
Paul Brussee <brussee@live.nl>
2016-06-22 01:40:40 +0200

    refactor python service template

--- pythonforandroid/bootstraps/service_only/build/templates/Service.tmpl.java
+++ pythonforandroid/bootstraps/service_only/build/templates/Service.tmpl.java
@@ -2,15 +2,14 @@ package {{ args.package }};
 
 import android.content.Intent;
 import android.content.Context;
-import android.app.Notification;
-import android.app.PendingIntent;
-import android.os.Bundle;
 import org.kivy.android.PythonService;
-import org.kivy.android.PythonActivity;
 
 
 public class Service{{ name|capitalize }} extends PythonService {
     {% if sticky %}
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int startType() {
         return START_STICKY;
@@ -18,39 +17,33 @@ public class Service{{ name|capitalize }} extends PythonService {
     {% endif %}
 
     {% if not foreground %}
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean canDisplayNotification() {
         return false;
     }
     {% endif %}
 
-    @Override
-    protected void doStartForeground(Bundle extras) {
-        Context context = getApplicationContext();
-        Notification notification = new Notification(context.getApplicationInfo().icon,
-            "{{ args.name }}", System.currentTimeMillis());
-        Intent contextIntent = new Intent(context, PythonActivity.class);
-        PendingIntent pIntent = PendingIntent.getActivity(context, 0, contextIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT);
-        notification.setLatestEventInfo(context, "{{ args.name }}", "{{ name| capitalize }}", pIntent);
-        startForeground({{ service_id }}, notification);
-    }
-
-    static public void start(Context ctx, String pythonServiceArgument) {
+    public static void start(Context ctx, String pythonServiceArgument) {
+    	String argument = ctx.getFilesDir().getAbsolutePath();
         Intent intent = new Intent(ctx, Service{{ name|capitalize }}.class);
-        String argument = ctx.getFilesDir().getAbsolutePath();
         intent.putExtra("androidPrivate", argument);
         intent.putExtra("androidArgument", argument);
         intent.putExtra("serviceEntrypoint", "{{ entrypoint }}");
+        intent.putExtra("serviceTitle", "{{ name|capitalize }}");
+        intent.putExtra("serviceDescription", "");
         intent.putExtra("pythonName", "{{ name }}");
         intent.putExtra("pythonHome", argument);
         intent.putExtra("pythonPath", argument + ":" + argument + "/lib");
         intent.putExtra("pythonServiceArgument", pythonServiceArgument);
         ctx.startService(intent);
     }
-
-    static public void stop(Context ctx) {
+    
+    public static void stop(Context ctx) {
         Intent intent = new Intent(ctx, Service{{ name|capitalize }}.class);
         ctx.stopService(intent);
     }
+
 }
