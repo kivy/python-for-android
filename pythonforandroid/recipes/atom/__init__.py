80c9a6652e2dcac09392efc81bba163baac6f5e7
frmdstryr <frmdstryr@gmail.com>
2016-06-25 17:24:18 -0400

    Add Cpp recipe

--- pythonforandroid/recipes/atom/__init__.py
+++ pythonforandroid/recipes/atom/__init__.py
@@ -1,29 +1,9 @@
-from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
 
-class AtomRecipe(CompiledComponentsPythonRecipe):
+class AtomRecipe(CppCompiledComponentsPythonRecipe):
     site_packages_name = 'atom'
     version = '0.3.10'
     url = 'https://github.com/nucleic/atom/archive/master.zip'
-    depends = ['python2']
-    call_hostpython_via_targetpython = False
-
-    def get_recipe_env(self, arch):
-        env = super(AtomRecipe, self).get_recipe_env(arch)
-        keys = dict(
-            ctx=self.ctx,
-            arch=arch,
-            arch_noeabi=arch.arch.replace('eabi', ''),
-            pyroot=self.ctx.get_python_install_dir()
-        )
-        #env['LDSHARED'] = env['CC'] + ' -pthread -shared -Wl,-O1 -Wl,-Bsymbolic-functions'
-
-        env['CFLAGS'] += " -I{pyroot}/include/python2.7 " \
-                        " -I{ctx.ndk_dir}/platforms/android-{ctx.android_api}/arch-{arch_noeabi}/usr/include" \
-                        " -I{ctx.ndk_dir}/sources/cxx-stl/gnu-libstdc++/{ctx.toolchain_version}/include" \
-                        " -I{ctx.ndk_dir}/sources/cxx-stl/gnu-libstdc++/{ctx.toolchain_version}/libs/{arch.arch}/include".format(**keys)
-         
-        env['LDFLAGS'] += " -L{ctx.ndk_dir}/sources/cxx-stl/gnu-libstdc++/{ctx.toolchain_version}/libs/{arch.arch}" \
-                " -lgnustl_shared".format(**keys)
-        return env
+    depends = ['python2','setuptools']
     
 recipe = AtomRecipe()
--- pythonforandroid/recipes/enaml/__init__.py
+++ pythonforandroid/recipes/enaml/__init__.py
@@ -2,9 +2,8 @@ from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
 
 class EnamlRecipe(CppCompiledComponentsPythonRecipe):
     site_packages_name = 'enaml'
-    call_hostpython_via_targetpython = False
     version = '0.9.8'
     url = 'https://github.com/frmdstryr/enaml/archive/master.zip'
-    depends = ['python2']
+    depends = ['python2','setuptools','atom','kiwisolver']
 
 recipe = EnamlRecipe()
