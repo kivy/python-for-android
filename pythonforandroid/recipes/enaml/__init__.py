4daa287726b2864c921cf5676fad4d043edd5fbf
frmdstryr <frmdstryr@gmail.com>
2016-06-25 23:44:37 -0400

    Use main repo but apply patch to remove PyQt

--- pythonforandroid/recipes/enaml/__init__.py
+++ pythonforandroid/recipes/enaml/__init__.py
@@ -3,7 +3,8 @@ from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
 class EnamlRecipe(CppCompiledComponentsPythonRecipe):
     site_packages_name = 'enaml'
     version = '0.9.8'
-    url = 'https://github.com/frmdstryr/enaml/archive/master.zip'
+    url = 'https://github.com/nucleic/enaml/archive/master.zip'
+    patches = ['0001-Update-setup.py.patch'] # Remove PyQt dependency
     depends = ['python2','setuptools','atom','kiwisolver']
 
 recipe = EnamlRecipe()
