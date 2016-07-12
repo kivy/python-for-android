00d6d2106de3d273ffa727a4163bfb2f2ea621cc
Paul Brussee <brussee@live.nl>
2016-07-12 14:40:52 +0200

    Workaround recursive link libtorrent.so
    rename native libtorrent.so to libtorrent_rasterbar.so
    link python libtorrent.so to libtorrent_rasterbar.so

--- pythonforandroid/recipes/libtorrent/__init__.py
+++ pythonforandroid/recipes/libtorrent/__init__.py
@@ -11,10 +11,10 @@ class LibtorrentRecipe(Recipe):
     url = 'https://github.com/arvidn/libtorrent/archive/libtorrent-1_0_9.tar.gz'
     depends = ['boost', 'python2']
     opt_depends = ['openssl']
-    patches = ['disable-so-version.patch', 'use-soname-python.patch']
+    patches = ['disable-so-version.patch', 'use-soname-python.patch', 'setup-lib-name.patch']
 
     def should_build(self, arch):
-        return not ( self.has_libs(arch, 'libboost_python.so', 'libboost_system.so', 'libtorrent.so')
+        return not ( self.has_libs(arch, 'libboost_python.so', 'libboost_system.so', 'libtorrent_rasterbar.so')
                      and self.ctx.has_package('libtorrent', arch.arch) )
 
     def prebuild_arch(self, arch):
@@ -55,8 +55,8 @@ class LibtorrentRecipe(Recipe):
         if 'openssl' in recipe.ctx.recipe_build_order:
             shutil.copyfile(join(env['BOOST_BUILD_PATH'], 'bin.v2/libs/date_time/build', build_subdirs, 'libboost_date_time.so'),
                         join(self.ctx.get_libs_dir(arch.arch), 'libboost_date_time.so'))
-        shutil.copyfile(join(self.get_build_dir(arch.arch), 'bin', build_subdirs, 'libtorrent.so'),
-                        join(self.ctx.get_libs_dir(arch.arch), 'libtorrent.so'))
+        shutil.copyfile(join(self.get_build_dir(arch.arch), 'bin', build_subdirs, 'libtorrent_rasterbar.so'),
+                        join(self.ctx.get_libs_dir(arch.arch), 'libtorrent_rasterbar.so'))
         shutil.copyfile(join(self.get_build_dir(arch.arch), 'bindings/python/bin', build_subdirs, 'libtorrent.so'),
                         join(self.ctx.get_site_packages_dir(arch.arch), 'libtorrent.so'))
 
