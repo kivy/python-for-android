e3df1f3b2b03417c80b08d0740186322b4f540ac
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2016-07-22 00:22:42 +0200

    Update __init__.py

--- pythonforandroid/recipes/docutils/__init__.py
+++ pythonforandroid/recipes/docutils/__init__.py
@@ -1,9 +1,6 @@
-from pythonforandroid.toolchain import Recipe, shprint, current_directory
-from os.path import exists, join
-import sh
+from pythonforandroid.toolchain import PythonRecipe
 
-
-class DocutilsRecipe(Recipe):
+class DocutilsRecipe(PythonRecipe):
 
     url = ('http://prdownloads.sourceforge.net/docutils/'
            'docutils-{version}.tar.gz')
@@ -13,27 +10,5 @@ class DocutilsRecipe(Recipe):
 
     version = '0.12'
     conflicts = []
-
-    def should_build(self, arch):
-        name = self.site_packages_name
-        if name is None:
-            name = self.name
-        if self.ctx.has_package(name):
-            shprint(sh.echo, 'Python package already exists in site-packages')
-            return False
-        shprint(sh.echo,
-                '{} apparently isn\'t already in site-packages'.format(name))
-        return True
-
-    def build_arch(self, arch):
-        super(DocutilsRecipe, self).build_arch(arch)
-
-        with current_directory(self.get_build_dir(arch.arch)):
-            env = super(DocutilsRecipe, self).get_recipe_env(arch)
-            hostpython = sh.Command(self.ctx.hostpython)
-
-            shprint(hostpython, 'setup.py', 'build_ext')
-            shprint(hostpython, 'setup.py', 'build_ext', '-v')
-            shprint(hostpython, 'setup.py', 'install', '-O2')
 
 recipe = DocutilsRecipe()
