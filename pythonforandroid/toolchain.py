46a80455cb01d443c761c62a7a1bc58860680a47
Louis Lévêque <loulouxiv@gmail.com>
2016-09-01 15:28:06 +0200

    restore call to _read_configuration in Toolchain

--- pythonforandroid/toolchain.py
+++ pythonforandroid/toolchain.py
@@ -315,6 +315,8 @@ class ToolchainCL(object):
             default=False,
             description='Copy libraries instead of using biglink (Android 4.3+)')
 
+        self._read_configuration()
+
         subparsers = parser.add_subparsers(dest='subparser_name',
                                            help='The command to run')
 
