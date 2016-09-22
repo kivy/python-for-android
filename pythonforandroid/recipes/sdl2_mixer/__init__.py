c7b6c2a9234c0aa286891a4e732d15be2b0939a0
Andre Miras <andre.miras@oridao.com>
2016-09-22 14:25:57 +0200

    Enable SDK2 MP3 support for Android
    We had to copy the external/smpeg2-2.0.0/ folder
    to the jni/ folder to fix linker errors.
    See https://groups.google.com/forum/#!topic/kivy-users/N4isIjQ1Ja8

--- pythonforandroid/recipes/sdl2_mixer/__init__.py
+++ pythonforandroid/recipes/sdl2_mixer/__init__.py
@@ -1,3 +1,8 @@
+from os.path import join
+
+import sh
+
+from pythonforandroid.logger import shprint
 from pythonforandroid.toolchain import BootstrapNDKRecipe
 
 
@@ -6,6 +11,36 @@ class LibSDL2Mixer(BootstrapNDKRecipe):
     url = 'https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-{version}.tar.gz'
     dir_name = 'SDL2_mixer'
 
-    patches = ['toggle_modplug_mikmod_smpeg_ogg.patch']
+    patches = ['toggle_modplug_mikmod_ogg.patch']
+
+    def _external_dir(self):
+        """
+        Returns the external/ dir path.
+        """
+        return join(self.get_build_dir(None), 'external')
+
+    def _smpeg2_external_dir(self):
+        """
+        Returns the smpeg2 external dir path.
+        """
+        return join(self._external_dir(), 'smpeg2-2.0.0')
+
+    def _copy_smpeg2_to_jni(self):
+        """
+        Copies the smpeg2 external to the jni/ folder.
+        """
+        smpeg2_external_dir = self._smpeg2_external_dir()
+        jni_dir = self.get_jni_dir()
+        shprint(sh.cp, '-r', smpeg2_external_dir, jni_dir)
+
+    def prebuild_arch(self, arch):
+        """
+        Copies the smpeg2 external library to the jni/ folder.
+        This is required by the linker.
+        """
+        jni_dir = self.get_jni_dir()
+        build_dir = self.get_build_dir(arch)
+        self._copy_smpeg2_to_jni()
+        super(LibSDL2Mixer, self).prebuild_arch(arch)
 
 recipe = LibSDL2Mixer()
