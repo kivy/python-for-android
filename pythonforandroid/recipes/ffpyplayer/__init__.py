b2022a096849c2c6a001da9a96c8d86cb879140a
germn <se0es@yandex.ru>
2016-12-11 16:35:16 +0300

    Fix ffpyplayer==4.0.0 compile with SDL2

--- pythonforandroid/recipes/ffpyplayer/__init__.py
+++ pythonforandroid/recipes/ffpyplayer/__init__.py
@@ -9,13 +9,16 @@ import os
 class FFPyPlayerRecipe(CythonRecipe):
     version = 'v4.0.0'
     url = 'https://github.com/matham/ffpyplayer/archive/{version}.zip'
+    md5sum = '99f4c7103bce0ecb167510fc810db82f'
     depends = ['python2', 'sdl2', 'ffmpeg']
     opt_depends = ['openssl', 'ffpyplayer_codecs']
+    patches = ['patches/fix-ffpyplayer-setup.patch']  # need this to compile with SDL2
+
+    def prebuild_arch(self, arch):
+        self.apply_patches(arch)
 
     def get_recipe_env(self, arch, with_flags_in_cc=True):
         env = super(FFPyPlayerRecipe, self).get_recipe_env(arch)
-
-        # TODO cannot find -lsdl error
 
         env["SDL_INCLUDE_DIR"] = join(self.ctx.bootstrap.build_dir, 'jni', 'SDL', 'include')
         env["SDL_LIB_DIR"] = join(self.ctx.bootstrap.build_dir, 'libs', arch.arch)
@@ -23,6 +26,7 @@ class FFPyPlayerRecipe(CythonRecipe):
         build_dir = Recipe.get_recipe('ffmpeg', self.ctx).get_build_dir(arch.arch)
         env["FFMPEG_INCLUDE_DIR"] = join(build_dir, "include")
         env["FFMPEG_LIB_DIR"] = join(build_dir, "lib")
+
         return env
 
 recipe = FFPyPlayerRecipe()
