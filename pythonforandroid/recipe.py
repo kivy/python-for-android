c3a5020fec6b5432514e6931fc423fc7dd890b14
Alexander Taylor <alexanderjohntaylor@gmail.com>
2017-01-14 18:52:52 +0000

    Made clean-recipe-build command also clean from site-packages

--- pythonforandroid/recipe.py
+++ pythonforandroid/recipe.py
@@ -582,6 +582,10 @@ class Recipe(with_metaclass(RecipeMeta)):
                 info('Deleting {}'.format(directory))
                 shutil.rmtree(directory)
 
+        # Delete any Python distributions to ensure the recipe build
+        # doesn't persist in site-packages
+        shutil.rmtree(self.ctx.python_installs_dir)
+
     def install_libs(self, arch, *libs):
         libs_dir = self.ctx.get_libs_dir(arch.arch)
         if not libs:
