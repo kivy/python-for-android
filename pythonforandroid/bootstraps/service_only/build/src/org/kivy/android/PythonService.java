72d3ff456eadd003de18f04ec5316c545ab02fcb
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-01-17 01:27:54 +0100

    Add ANDROID_UNPACK to other bootstraps

--- pythonforandroid/bootstraps/service_only/build/src/org/kivy/android/PythonService.java
+++ pythonforandroid/bootstraps/service_only/build/src/org/kivy/android/PythonService.java
@@ -26,6 +26,7 @@ public abstract class PythonService extends Service implements Runnable {
     private String pythonName;
     private String pythonHome;
     private String pythonPath;
+    private String ANDROID_UNPACK;
     private String serviceEntrypoint;
     private String pythonServiceArgument;
 
@@ -70,6 +71,7 @@ public abstract class PythonService extends Service implements Runnable {
         pythonName = extras.getString("pythonName");
         pythonHome = extras.getString("pythonHome");
         pythonPath = extras.getString("pythonPath");
+        ANDROID_UNPACK = extras.getString("ANDROID_UNPACK");
         pythonServiceArgument = extras.getString("pythonServiceArgument");
 
         Log.v(TAG, "Starting Python thread");
@@ -127,7 +129,7 @@ public abstract class PythonService extends Service implements Runnable {
     public void run() {
         PythonUtil.loadLibraries(getFilesDir());
         nativeStart(androidPrivate, androidArgument, serviceEntrypoint, pythonName, pythonHome,
-                pythonPath, pythonServiceArgument);
+                pythonPath, pythonServiceArgument, ANDROID_UNPACK);
         stopSelf();
     }
 
@@ -143,5 +145,5 @@ public abstract class PythonService extends Service implements Runnable {
     public static native void nativeStart(String androidPrivate, String androidArgument,
                                           String serviceEntrypoint, String pythonName,
                                           String pythonHome, String pythonPath,
-                                          String pythonServiceArgument);
+                                          String pythonServiceArgument, String ANDROID_UNPACK);
 }
