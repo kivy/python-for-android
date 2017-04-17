26c6f606fc9788ef4487a0265850863ead893c04
Trevor Martin <trevor@helixmediasystems.com>
2017-04-17 14:47:00 -0400

    work-around for patch

--- pythonforandroid/recipes/websocket-client/__init__.py
+++ pythonforandroid/recipes/websocket-client/__init__.py
@@ -2,11 +2,12 @@ from pythonforandroid.toolchain import Recipe
 
 
 class WebSocketClient(Recipe):
-    url = 'https://pypi.python.org/packages/a7/2b/0039154583cb0489c8e18313aa91ccd140ada103289c5c5d31d80fd6d186/websocket_client-0.40.0.tar.gz'
+    url = 'https://github.com/debauchery1st/websocket-client/blob/master/websocket_client-0.40.0.tar.gz?raw=true'
+    # url = 'https://pypi.python.org/packages/a7/2b/0039154583cb0489c8e18313aa91ccd140ada103289c5c5d31d80fd6d186/websocket_client-0.40.0.tar.gz'
     version = '0.40.0'
-    md5sum = 'f1cf4cc7869ef97a98e5f4be25c30986'
+    # md5sum = 'f1cf4cc7869ef97a98e5f4be25c30986'
 
-    patches = ['websocket.patch']  # Paths relative to the recipe dir
+    # patches = ['websocket.patch']  # Paths relative to the recipe dir
 
     depends = ['kivy', 'python2', 'android', 'pyjnius',
                'cryptography', 'pyasn1', 'pyopenssl']
