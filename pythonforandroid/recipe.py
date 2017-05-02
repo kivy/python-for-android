903528711d972599c5eddb5b64a5d94366f6a73a
mkg20001 <mkg20001@gmail.com>
2017-05-02 16:00:12 +0200

    feat: add ci_mode to disable download progress
    Otherwise Travis CI complains about log size

--- pythonforandroid/recipe.py
+++ pythonforandroid/recipe.py
@@ -140,8 +140,9 @@ class Recipe(with_metaclass(RecipeMeta)):
                 else:
                     progression = '{0:.2f}%'.format(
                         index * blksize * 100. / float(size))
-                stdout.write('- Download {}\r'.format(progression))
-                stdout.flush()
+                if "CI" not in environ:
+                    stdout.write('- Download {}\r'.format(progression))
+                    stdout.flush()
 
             if exists(target):
                 unlink(target)
