59a5916558c1a4cf375219c6731cefa140a31bcf
Alexander Taylor <alexanderjohntaylor@gmail.com>
2017-07-02 16:10:35 +0100

    Changed Android.mk back to old version
    I think the changes in -I and -L paths were due to the gradle build
    file including the NDK build, whereas this should already be done now.

--- pythonforandroid/bootstraps/sdl2_gradle/build/jni/src/Android.mk
+++ pythonforandroid/bootstraps/sdl2_gradle/build/jni/src/Android.mk
@@ -13,14 +13,12 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
 	start.c
 
 LOCAL_CFLAGS += -I$(LOCAL_PATH)/../../../../other_builds/$(PYTHON2_NAME)/$(ARCH)/python2/python-install/include/python2.7 $(EXTRA_CFLAGS)
-LOCAL_CFLAGS += -I$(LOCAL_PATH)/../../python-install/include/python2.7 $(EXTRA_CFLAGS)
 
-LOCAL_SHARED_LIBRARIES := SDL2
+LOCAL_SHARED_LIBRARIES := SDL2 python_shared
 
-LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog -lpython2.7 $(EXTRA_LDLIBS)
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog $(EXTRA_LDLIBS)
 
 LOCAL_LDFLAGS += -L$(LOCAL_PATH)/../../../../other_builds/$(PYTHON2_NAME)/$(ARCH)/python2/python-install/lib $(APPLICATION_ADDITIONAL_LDFLAGS)
-LOCAL_LDFLAGS += -L$(LOCAL_PATH)/../../python-install/lib $(APPLICATION_ADDITIONAL_LDFLAGS)
 
 include $(BUILD_SHARED_LIBRARY)
 
