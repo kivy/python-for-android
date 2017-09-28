144bff9f54117f706d7095bb6a4e2d353e546357
Alexander Taylor <alexanderjohntaylor@gmail.com>
2017-09-28 23:07:40 +0100

    Ported SDL2_image fix on Android to SDL2_image 2.0.1
    The changes are the same as the patch at
    https://hg.libsdl.org/SDL_image/rev/7ad06019831d, but modified for
    version 2.0.1.

--- pythonforandroid/recipes/sdl2_image/__init__.py
+++ pythonforandroid/recipes/sdl2_image/__init__.py
@@ -4,12 +4,12 @@ from pythonforandroid.patching import is_arch
 
 class LibSDL2Image(BootstrapNDKRecipe):
     version = '2.0.1'
-    # url = 'https://www.libsdl.org/projects/SDL_image/release/SDL2_image-{version}.tar.gz'
-    url = 'https://hg.libsdl.org/SDL_image/archive/tip.tar.gz'
+    url = 'https://www.libsdl.org/projects/SDL_image/release/SDL2_image-{version}.tar.gz'
     dir_name = 'SDL2_image'
 
     patches = ['toggle_jpg_png_webp.patch',
                ('disable_jpg.patch', is_arch('x86')),
-               'extra_cflags.patch']
+               'extra_cflags.patch',
+               'fix_with_ndk_15_plus.patch']
 
 recipe = LibSDL2Image()
