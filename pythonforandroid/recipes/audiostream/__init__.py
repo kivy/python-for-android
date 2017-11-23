e146dd17f722e25932cad31a048153406f201c57
snuq <snuq@snuq.com>
2017-11-22 16:05:28 -0800

    Update __init__.py

--- pythonforandroid/recipes/audiostream/__init__.py
+++ pythonforandroid/recipes/audiostream/__init__.py
@@ -16,13 +16,13 @@ class AudiostreamRecipe(CythonRecipe):
             sdl_include = 'sdl'
             sdl_mixer_include = 'sdl_mixer'
         elif 'sdl2' in self.ctx.recipe_build_order:
-            sdl_include = 'SDL'
+            sdl_include = 'SDL2'
             sdl_mixer_include = 'SDL2_mixer'
             
             #note: audiostream library is not yet able to judge whether it is being used with sdl or with sdl2.
             #this causes linking to fail against SDL2 (compiling against SDL2 works)
             #need to find a way to fix this in audiostream's setup.py
-            raise RuntimeError('Audiostream library is not yet able to configure itself to link against SDL2.  Patch on audiostream library needed - any help much appreciated!')
+            #raise RuntimeError('Audiostream library is not yet able to configure itself to link against SDL2.  Patch on audiostream library needed - any help much appreciated!')
 
         env = super(AudiostreamRecipe, self).get_recipe_env(arch)
         env['CFLAGS'] += ' -I{jni_path}/{sdl_include}/include -I{jni_path}/{sdl_mixer_include}'.format(
