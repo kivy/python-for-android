03836b848b94c31efd203e3422d02fe16a4fd788
Robin Becker <robin@reportlab.com>
2017-11-25 22:15:29 +0000

    changed to use python2,python3crystax insread of hostpython

--- pythonforandroid/recipes/pyrxp/__init__.py
+++ pythonforandroid/recipes/pyrxp/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.toolchain import CompiledComponentsPythonRecipe, warning
 class PyRXPURecipe(CompiledComponentsPythonRecipe):
     version = '2a02cecc87b9'
     url = 'https://bitbucket.org/rptlab/pyrxp/get/{version}.tar.gz'
-    depends = ['hostpython']
+    depends = ['python2','python3crystax']
     patches = []
 
 recipe = PyRXPURecipe()
