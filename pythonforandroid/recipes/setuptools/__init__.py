bca9ffd8884c714d707aaed91483ed9b44b15e62
Andre Miras <andre.miras@gmail.com>
2018-03-31 19:54:30 +0000

    Setuptools for Python3 (Crystax) and import fixes
    Adds python3crystax support to setuptools recipe.
    Also removed unused imports.

--- pythonforandroid/recipes/setuptools/__init__.py
+++ pythonforandroid/recipes/setuptools/__init__.py
@@ -1,20 +1,11 @@
-
-from pythonforandroid.toolchain import (
-    PythonRecipe,
-    Recipe,
-    current_directory,
-    info,
-    shprint,
-)
-from os.path import join
-import sh
+from pythonforandroid.toolchain import PythonRecipe
 
 
 class SetuptoolsRecipe(PythonRecipe):
     version = '18.3.1'
     url = 'https://pypi.python.org/packages/source/s/setuptools/setuptools-{version}.tar.gz'
 
-    depends = ['python2']
+    depends = [('python2', 'python3crystax')]
 
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
