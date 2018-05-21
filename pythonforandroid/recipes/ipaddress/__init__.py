764c2af70b67d1e882d9c917838e90d7c6d61d08
Andre Miras <andre.miras@gmail.com>
2018-05-21 04:54:22 +0200

    Fixes linter E101 and W191 errors
      - E101 indentation contains mixed spaces and tabs
      - W191 indentation contains tabs

--- pythonforandroid/recipes/ipaddress/__init__.py
+++ pythonforandroid/recipes/ipaddress/__init__.py
@@ -2,11 +2,11 @@ from pythonforandroid.recipe import PythonRecipe
 
 
 class IpaddressRecipe(PythonRecipe):
-	name = 'ipaddress'
-	version = '1.0.16'
-	url = 'https://pypi.python.org/packages/source/i/ipaddress/ipaddress-{version}.tar.gz'
+    name = 'ipaddress'
+    version = '1.0.16'
+    url = 'https://pypi.python.org/packages/source/i/ipaddress/ipaddress-{version}.tar.gz'
 
-	depends = ['python2']
+    depends = ['python2']
 
 
 recipe = IpaddressRecipe()
