ccd5b594a291fa2cd23050847129d72556a5bcf5
Andre Miras <andre.miras@gmail.com>
2018-06-08 00:29:39 +0200

    Recipes updates with CrystaX/Python3 support
    Adds CrystaX/Python3 support to 9 recipes:
    
      - cffi: fixes build in both Python2 and Python3
      - decorator: updates version and adds python3crystax support
      - idna: updates version
      - pycryptodome: fixes version string & adds python3crystax support
      - python3crystax: adds python3crystax support
      - pyyaml: updates version & drops host python via target python
      - requests: adds python3crystax support
      - scrypt: fixes version & adds python3crystax support
      - secp256k1: adds python3crystax support
    
    These recipes were tested with CrystaX/Python3 in
    https://github.com/AndreMiras/EtherollApp
    See last `buildozer android debug` build success:
    https://travis-ci.org/AndreMiras/EtherollApp/jobs/386381704

--- pythonforandroid/recipes/scrypt/__init__.py
+++ pythonforandroid/recipes/scrypt/__init__.py
@@ -1,15 +1,13 @@
+import os
 from pythonforandroid.recipe import CythonRecipe
-from os.path import join
 
 
 class ScryptRecipe(CythonRecipe):
 
-    url = 'https://bitbucket.org/mhallin/py-scrypt/get/default.zip'
-
-    depends = ['python2', 'setuptools', 'openssl']
-
+    version = '0.8.6'
+    url = 'https://bitbucket.org/mhallin/py-scrypt/get/v{version}.zip'
+    depends = [('python2', 'python3crystax'), 'setuptools', 'openssl']
     call_hostpython_via_targetpython = False
-
     patches = ["remove_librt.patch"]
 
     def get_recipe_env(self, arch, with_flags_in_cc=True):
@@ -19,12 +17,21 @@ class ScryptRecipe(CythonRecipe):
         env = super(ScryptRecipe, self).get_recipe_env(arch, with_flags_in_cc)
         openssl_build_dir = self.get_recipe(
             'openssl', self.ctx).get_build_dir(arch.arch)
-        print("openssl_build_dir:", openssl_build_dir)
-        env['CC'] = '%s -I%s' % (env['CC'], join(openssl_build_dir, 'include'))
-        env['LDFLAGS'] = env['LDFLAGS'] + ' -L{}'.format(
+        env['CFLAGS'] += ' -I{}'.format(os.path.join(openssl_build_dir, 'include'))
+        env['LDFLAGS'] += ' -L{}'.format(
             self.ctx.get_libs_dir(arch.arch) +
             '-L{}'.format(self.ctx.libs_dir)) + ' -L{}'.format(
             openssl_build_dir)
+        # required additional library and path for Crystax
+        if self.ctx.ndk == 'crystax':
+            # only keeps major.minor (discards patch)
+            python_version = self.ctx.python_recipe.version[0:3]
+            ndk_dir_python = os.path.join(self.ctx.ndk_dir, 'sources/python/', python_version)
+            env['LDFLAGS'] += ' -L{}'.format(os.path.join(ndk_dir_python, 'libs', arch.arch))
+            env['LDFLAGS'] += ' -lpython{}m'.format(python_version)
+            # until `pythonforandroid/archs.py` gets merged upstream:
+            # https://github.com/kivy/python-for-android/pull/1250/files#diff-569e13021e33ced8b54385f55b49cbe6
+            env['CFLAGS'] += ' -I{}/include/python/'.format(ndk_dir_python)
         return env
 
 
