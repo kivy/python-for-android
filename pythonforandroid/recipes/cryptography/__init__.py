a4a1131b17fd4c8ac5abbd67be5010fd155272f1
Pol Canelles <pol@canellestudi.com>
2018-06-30 11:27:32 +0200

    Unify python's flags from recipe.py into PythonRecipe
    This changes affect almost all recipes inherited from PythonRecipe. Some recipes where affected by the changes and were modified in order to succesfully build. One of the changes forces to get python headers and linkages for all the CythonRecipe,so, all inherited recipes from CythonRecipe should be reviewed and cleaned up if necessary and all the recipes who inherited from PythonRecipe probably should be reviewed, taking into account the fact pointed by user @AndreMiras, some flags of some recipes are wrong:
    
      - CFLAGS may only be used to specify C compiler flags, for macro definitions use CPPFLAGS
      - LDFLAGS may only be used to specify linker flags, for libraries use LIBS
    
    Resolves: #668
    See also: #1307

--- pythonforandroid/recipes/cryptography/__init__.py
+++ pythonforandroid/recipes/cryptography/__init__.py
@@ -1,5 +1,4 @@
 from pythonforandroid.recipe import CompiledComponentsPythonRecipe
-from os.path import join
 
 
 class CryptographyRecipe(CompiledComponentsPythonRecipe):
@@ -13,14 +12,9 @@ class CryptographyRecipe(CompiledComponentsPythonRecipe):
         env = super(CryptographyRecipe, self).get_recipe_env(arch)
         r = self.get_recipe('openssl', self.ctx)
         openssl_dir = r.get_build_dir(arch.arch)
-        env['PYTHON_ROOT'] = self.ctx.get_python_install_dir()
-        env['CFLAGS'] += ' -I' + env['PYTHON_ROOT'] + '/include/python2.7' + \
-                         ' -I' + join(openssl_dir, 'include')
         # Set linker to use the correct gcc
         env['LDSHARED'] = env['CC'] + ' -pthread -shared -Wl,-O1 -Wl,-Bsymbolic-functions'
-        env['LDFLAGS'] += ' -L' + env['PYTHON_ROOT'] + '/lib' + \
-                          ' -L' + openssl_dir + \
-                          ' -lpython2.7' + \
+        env['LDFLAGS'] += ' -L' + openssl_dir + \
                           ' -lssl' + r.version + \
                           ' -lcrypto' + r.version
         return env
