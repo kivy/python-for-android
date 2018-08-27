a930d649c604b632f199badca77233a63210a367
Kim Rinnewitz <krinnewitz@plapadoo.de>
2018-08-27 18:08:50 +0200

    Extend numpy recipe to support python3
    This patch extends the existing numpy recipe to also support
    python3crystax. Additionally, the existing recipe code is cleaned up a
    bit and numpy is upgraded to version 1.15.1.

--- pythonforandroid/recipes/numpy/__init__.py
+++ pythonforandroid/recipes/numpy/__init__.py
@@ -1,48 +1,48 @@
 from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 from pythonforandroid.toolchain import warning
+from os.path import join
 
 
 class NumpyRecipe(CompiledComponentsPythonRecipe):
 
-    version = '1.9.2'
-    url = 'https://pypi.python.org/packages/source/n/numpy/numpy-{version}.tar.gz'
-    site_packages_name= 'numpy'
+    version = '1.15.1'
+    url = 'https://pypi.python.org/packages/source/n/numpy/numpy-{version}.zip'
+    site_packages_name = 'numpy'
 
-    depends = ['python2']
+    depends = [('python2', 'python3crystax')]
 
-    patches = ['patches/fix-numpy.patch',
-               'patches/prevent_libs_check.patch',
-               'patches/ar.patch',
-               'patches/lib.patch']
+    patches = [
+        join('patches', 'fix-numpy.patch'),
+        join('patches', 'prevent_libs_check.patch'),
+        join('patches', 'ar.patch'),
+        join('patches', 'lib.patch'),
+        join('patches', 'python2-fixes.patch')
+    ]
 
     def get_recipe_env(self, arch):
-        """ looks like numpy has no proper -L flags. Code copied and adapted from
-            https://github.com/frmdstryr/p4a-numpy/
-        """
-
         env = super(NumpyRecipe, self).get_recipe_env(arch)
-        #: Hack add path L to crystax as a CFLAG
 
-        py_ver = '3.5'
-        if {'python2crystax', 'python2'} & set(self.ctx.recipe_build_order):
-            py_ver = '2.7'
+        flags = " -L{} --sysroot={}".format(
+            join(self.ctx.ndk_platform, 'usr', 'lib'),
+            self.ctx.ndk_platform
+        )
 
-        py_so = '2.7' if py_ver == '2.7' else '3.5m'
+        if self.ctx.ndk == 'crystax':
+            py_ver = self.ctx.python_recipe.version[0:3]
+            src_dir = join(self.ctx.ndk_dir, 'sources')
+            py_inc_dir = join(src_dir, 'python', py_ver, 'include', 'python')
+            py_lib_dir = join(src_dir, 'python', py_ver, 'libs', arch.arch)
+            cry_inc_dir = join(src_dir, 'crystax', 'include')
+            cry_lib_dir = join(src_dir, 'crystax', 'libs', arch.arch)
+            flags += ' -I{}'.format(py_inc_dir)
+            flags += ' -L{} -lpython{}m'.format(py_lib_dir, py_ver)
+            flags += " -I{}".format(cry_inc_dir)
+            flags += " -L{}".format(cry_lib_dir)
 
-        api_ver = self.ctx.android_api
-
-        platform = 'arm' if 'arm' in arch.arch else arch.arch
-        #: Not sure why but we have to inject these into the CC and LD env's for it to
-        #: use the correct arguments.
-        flags = " -L{ctx.ndk_dir}/platforms/android-{api_ver}/arch-{platform}/usr/lib/" \
-                " --sysroot={ctx.ndk_dir}/platforms/android-{api_ver}/arch-{platform}" \
-            .format(ctx=self.ctx, arch=arch, platform=platform, api_ver=api_ver,
-                    py_so=py_so, py_ver=py_ver)
         if flags not in env['CC']:
             env['CC'] += flags
         if flags not in env['LD']:
             env['LD'] += flags + ' -shared'
-
         return env
 
     def prebuild_arch(self, arch):
