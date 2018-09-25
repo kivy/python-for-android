061b15358ab649edc3e1cdca23a660eb11caf395
Robert Niederreiter <office@squarewave.at>
2018-09-25 11:47:20 +0200

    Update cffi recipe version to 1.11.5
    Update cryptography version to 2.3.1. Document cryptography dependencies. Add openssl includes to CFLAGS when compiling cryptography.
    
    Update enum34 recipe version to 1.1.6
    
    Update idna recipe version to 2.7
    
    Update ipaddress recipe version to 1.0.22
    
    Update openssl version to 1.0.2p
    
    Update pyasn1 recipe version to 0.4.4
    
    Update pycparser recipe version to 2.18
    
    Update pyopenssl recipe version to 18.0.0
    
    Document dependency option for TLS in twisted recipe
    
    remove commented line
    
    Update cryptography version to 2.3.1. Document cryptography dependencies. Add openssl includes to CFLAGS when compiling cryptography.
    
    Update enum34 recipe version to 1.1.6
    
    Update idna recipe version to 2.7
    
    Update ipaddress recipe version to 1.0.22
    
    Update openssl version to 1.0.2p
    
    Update pyasn1 recipe version to 0.4.4
    
    Update pycparser recipe version to 2.18
    
    Update pyopenssl recipe version to 18.0.0
    
    Document dependency option for TLS in twisted recipe

--- pythonforandroid/recipes/enum34/__init__.py
+++ pythonforandroid/recipes/enum34/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 
 class Enum34Recipe(PythonRecipe):
-    version = '1.1.3'
+    version = '1.1.6'
     url = 'https://pypi.python.org/packages/source/e/enum34/enum34-{version}.tar.gz'
     depends = [('python2', 'python3crystax'), 'setuptools']
     site_packages_name = 'enum'
