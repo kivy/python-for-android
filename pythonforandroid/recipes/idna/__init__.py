0da6f727b89e9b483ed20893b85e3675483a6532
Robert Niederreiter <office@squarewave.at>
2018-09-25 11:49:27 +0200

    Update idna recipe version to 2.7

--- pythonforandroid/recipes/idna/__init__.py
+++ pythonforandroid/recipes/idna/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 class IdnaRecipe(PythonRecipe):
     name = 'idna'
-    version = '2.6'
+    version = '2.7'
     url = 'https://github.com/kjd/idna/archive/v{version}.tar.gz'
 
     depends = [('python2', 'python3crystax'), 'setuptools']
