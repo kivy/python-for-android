bd382656d701cbdc7d4fc495873ff10561a28627
Alexander Taylor <alexanderjohntaylor@gmail.com>
2018-10-20 00:44:01 +0100

    Moved python2 python bundle creation to python recipe

--- pythonforandroid/bootstraps/sdl2/__init__.py
+++ pythonforandroid/bootstraps/sdl2/__init__.py
@@ -22,9 +22,6 @@ class SDL2GradleBootstrap(Bootstrap):
         arch = self.ctx.archs[0]
         python_install_dir = self.ctx.get_python_install_dir()
         from_crystax = self.ctx.python_recipe.from_crystax
-        crystax_python_dir = join("crystax_python", "crystax_python")
-
-        python_bundle_dir = join('_python_bundle', '_python_bundle')
 
         if len(self.ctx.archs) > 1:
             raise ValueError("SDL2/gradle support only one arch")
@@ -43,13 +40,6 @@ class SDL2GradleBootstrap(Bootstrap):
         with current_directory(self.dist_dir):
             info("Copying Python distribution")
 
-            if 'python2' in self.ctx.recipe_build_order:
-                ensure_dir("private")
-            elif not exists("crystax_python") and from_crystax:
-                ensure_dir(crystax_python_dir)
-            elif 'python3' in self.ctx.recipe_build_order:
-                ensure_dir(python_bundle_dir)
-
             hostpython = sh.Command(self.ctx.hostpython)
             if self.ctx.python_recipe.name == 'python2':
                 try:
@@ -66,53 +56,14 @@ class SDL2GradleBootstrap(Bootstrap):
             self.distribute_javaclasses(self.ctx.javaclass_dir,
                                         dest_dir=join("src", "main", "java"))
 
-            if self.ctx.python_recipe.name == 'python2':
-                info("Filling private directory")
-                if not exists(join("private", "lib")):
-                    info("private/lib does not exist, making")
-                    shprint(sh.cp, "-a",
-                            join("python-install", "lib"), "private")
-                shprint(sh.mkdir, "-p",
-                        join("private", "include", "python2.7"))
+            python_bundle_dir = join('_python_bundle', '_python_bundle')
+            if 'python2' in self.ctx.recipe_build_order:
+                # Python 2 is a special case with its own packaging location
+                python_bundle_dir = 'private'
+            ensure_dir(python_bundle_dir)
 
-                libpymodules_fn = join("libs", arch.arch, "libpymodules.so")
-                if exists(libpymodules_fn):
-                    shprint(sh.mv, libpymodules_fn, 'private/')
-                shprint(sh.cp,
-                        join('python-install', 'include',
-                             'python2.7', 'pyconfig.h'),
-                        join('private', 'include', 'python2.7/'))
-
-                info('Removing some unwanted files')
-                shprint(sh.rm, '-f', join('private', 'lib', 'libpython2.7.so'))
-                shprint(sh.rm, '-rf', join('private', 'lib', 'pkgconfig'))
-
-                libdir = join(self.dist_dir, 'private', 'lib', 'python2.7')
-                site_packages_dir = join(libdir, 'site-packages')
-                with current_directory(libdir):
-                    removes = []
-                    for dirname, root, filenames in walk("."):
-                        for filename in filenames:
-                            for suffix in EXCLUDE_EXTS:
-                                if filename.endswith(suffix):
-                                    removes.append(filename)
-                    shprint(sh.rm, '-f', *removes)
-
-                    info('Deleting some other stuff not used on android')
-                    # To quote the original distribute.sh, 'well...'
-                    shprint(sh.rm, '-rf', 'lib2to3')
-                    shprint(sh.rm, '-rf', 'idlelib')
-                    for filename in glob.glob('config/libpython*.a'):
-                        shprint(sh.rm, '-f', filename)
-                    shprint(sh.rm, '-rf', 'config/python.o')
-
-            elif self.ctx.python_recipe.name == 'python3':
-                self.ctx.python_recipe.create_python_bundle(
-                    join(self.dist_dir, python_bundle_dir), arch)
-                
-            elif self.ctx.python_recipe.from_crystax:
-                self.ctx.python_recipe.create_python_bundle(
-                    join(self.dist_dir, python_bundle_dir), arch)
+            self.ctx.python_recipe.create_python_bundle(
+                join(self.dist_dir, python_bundle_dir), arch)
                 # TODO: Also set site_packages_dir again so fry_eggs can work
 
             if 'sqlite3' not in self.ctx.recipe_build_order:
