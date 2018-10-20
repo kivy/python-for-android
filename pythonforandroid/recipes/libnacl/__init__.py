8ca841f2cc30e13cbefcc10e0b2ae669c8aed23f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-10-20 11:07:32 +0200

    Fix libnacl recipe missing libsodium

--- pythonforandroid/recipes/libnacl/__init__.py
+++ pythonforandroid/recipes/libnacl/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.toolchain import PythonRecipe
 class LibNaClRecipe(PythonRecipe):
     version = '1.4.4'
     url = 'https://github.com/saltstack/libnacl/archive/v{version}.tar.gz'
-    depends = ['hostpython2', 'setuptools']
+    depends = ['hostpython2', 'setuptools', 'libsodium']
     site_packages_name = 'libnacl'
     call_hostpython_via_targetpython = False
 
