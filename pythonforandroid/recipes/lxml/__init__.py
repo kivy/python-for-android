d36cd9d24da394a48ae213eba0ee2b73ffbf431f
Jérôme Poisson <goffi@goffi.org>
2018-10-23 20:31:05 +0200

    [lxml] recipe fix
    This patch fixes the recipes from PR
    https://github.com/kivy/python-for-android/pull/1166.
    
    levenshtein recipe has been removed has it's not used anywhere and its
    causing compilation troubles.

--- pythonforandroid/recipes/lxml/__init__.py
+++ pythonforandroid/recipes/lxml/__init__.py
@@ -1,36 +1,45 @@
-from pythonforandroid.toolchain import Recipe, shprint, shutil, current_directory
-from pythonforandroid.toolchain import CompiledComponentsPythonRecipe
-from pythonforandroid.util import current_directory, ensure_dir
-from pythonforandroid.logger import debug, shprint, info
+from pythonforandroid.toolchain import Recipe, shutil
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 from os.path import exists, join, dirname
-import sh
-import glob
+
 
 class LXMLRecipe(CompiledComponentsPythonRecipe):
-    version = '3.6.0'
-    url = 'https://pypi.python.org/packages/source/l/lxml/lxml-{version}.tar.gz'
-    depends = ['python2', 'libxml2', 'libxslt']
-    name = 'lxml'
+    version = "3.6.0"
+    url = "https://pypi.python.org/packages/source/l/lxml/lxml-{version}.tar.gz"
+    depends = ["python2", "libxml2", "libxslt"]
+    name = "lxml"
 
-    call_hostpython_via_targetpython = False # Due to setuptools
+    call_hostpython_via_targetpython = False  # Due to setuptools
 
     def should_build(self, arch):
         super(LXMLRecipe, self).should_build(arch)
         return True
-        return not exists(join(self.ctx.get_libs_dir(arch.arch), 'etree.so'))
+        return not exists(join(self.ctx.get_libs_dir(arch.arch), "etree.so"))
 
     def build_arch(self, arch):
         super(LXMLRecipe, self).build_arch(arch)
-        shutil.copyfile('%s/build/lib.linux-x86_64-2.7/lxml/etree.so' % self.get_build_dir(arch.arch), join(self.ctx.get_libs_dir(arch.arch), 'etree.so'))
-        shutil.copyfile('%s/build/lib.linux-x86_64-2.7/lxml/objectify.so' % self.get_build_dir(arch.arch), join(self.ctx.get_libs_dir(arch.arch), 'objectify.so'))
+        shutil.copyfile(
+            "%s/build/lib.linux-x86_64-2.7/lxml/etree.so" % self.get_build_dir(arch.arch),
+            join(self.ctx.get_libs_dir(arch.arch), "etree.so"),
+        )
+        shutil.copyfile(
+            "%s/build/lib.linux-x86_64-2.7/lxml/objectify.so"
+            % self.get_build_dir(arch.arch),
+            join(self.ctx.get_libs_dir(arch.arch), "objectify.so"),
+        )
 
     def get_recipe_env(self, arch):
         env = super(LXMLRecipe, self).get_recipe_env(arch)
-        libxslt_recipe = Recipe.get_recipe('libxslt', self.ctx)
-        libxml2_recipe = Recipe.get_recipe('libxml2', self.ctx)
+        libxslt_recipe = Recipe.get_recipe("libxslt", self.ctx).get_build_dir(arch.arch)
+        libxml2_recipe = Recipe.get_recipe("libxml2", self.ctx).get_build_dir(arch.arch)
         targetpython = "%s/include/python2.7/" % dirname(dirname(self.ctx.hostpython))
-        env['CC'] += " -I%s/include -I%s -I%s" % (libxml2_recipe, libxslt_recipe, targetpython)
-        env['LDSHARED'] = '%s -nostartfiles -shared -fPIC -lpython2.7' % env['CC']
+        env["CC"] += " -I%s/include -I%s -I%s" % (
+            libxml2_recipe,
+            libxslt_recipe,
+            targetpython,
+        )
+        env["LDSHARED"] = "%s -nostartfiles -shared -fPIC -lpython2.7" % env["CC"]
         return env
+
 
 recipe = LXMLRecipe()
