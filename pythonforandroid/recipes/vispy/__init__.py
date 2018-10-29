d563fcf30630f07be0845271d6faf97e6cdb911d
Andre Miras <andre.miras@gmail.com>
2018-10-29 20:36:08 +0100

    Conditional builds falls back on Python2 if needed
    Falls back to python2 when tested recipes are python3crystax compatible.
    This is for instance the case on #1428 where the recipes depend only on
    python2, hence couldn't be automatically tested.
    
    Also updated `vispy` recipe to handle the version string properly,
    pin it and clean comments.
    This recipe is a demo case scenario for the conditional build falling
    back on python2 since `vispy` is not yet compatible with python3crystax.
    
    Last, removes the debug flag from the test apps used by the CI as it
    produces too many logs and halts Travis.

--- pythonforandroid/recipes/vispy/__init__.py
+++ pythonforandroid/recipes/vispy/__init__.py
@@ -1,18 +1,10 @@
-
 from pythonforandroid.recipe import PythonRecipe
 
 
 class VispyRecipe(PythonRecipe):
-    # version = 'v0.4.0'
-    version = 'master'
-    url = 'https://github.com/vispy/vispy/archive/{version}.tar.gz'
-    # version = 'campagnola-scenegraph-update'
-    # url = 'https://github.com/campagnola/vispy/archive/scenegraph-update.zip'
-    # version = '???'
-    # url = 'https://github.com/inclement/vispy/archive/Eric89GXL-arcball.zip'
-
+    version = '0.4.0'
+    url = 'https://github.com/vispy/vispy/archive/v{version}.tar.gz'
     depends = ['python2', 'numpy', 'pysdl2']
-
     patches = ['disable_freetype.patch',
                'disable_font_triage.patch',
                'use_es2.patch',
