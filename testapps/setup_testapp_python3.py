f74ce04f9148d6fe8faae1d680fb295469d8970b
Alexander Taylor <alexanderjohntaylor@gmail.com>
2018-11-08 22:54:07 +0000

    Updated CI scripts to run a python3 testapp

--- testapps/setup_testapp_python3.py
+++ testapps/setup_testapp_python3.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 options = {'apk': {'requirements': 'sdl2,pyjnius,kivy,python3',
-                   'android-api': 26,
+                   'android-api': 27,
                    'ndk-api': 19,
                    'dist-name': 'bdisttest_python3_googlendk',
                    'ndk-version': '10.3.2',
