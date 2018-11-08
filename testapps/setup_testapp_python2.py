e48d82e8db61681e27ef27e38334ab0fbd5411ce
Alexander Taylor <alexanderjohntaylor@gmail.com>
2018-11-08 21:52:09 +0000

    Updated travis.yml to target python3crystax testapp

--- testapps/setup_testapp_python2.py
+++ testapps/setup_testapp_python2.py
@@ -2,8 +2,7 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-options = {'apk': {'debug': None,
-                   'requirements': 'sdl2,pyjnius,kivy,python2',
+options = {'apk': {'requirements': 'sdl2,pyjnius,kivy,python2',
                    'android-api': 19,
                    'ndk-api': 19,
                    'ndk-dir': '/home/asandy/android/crystax-ndk-10.3.2',
