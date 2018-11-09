33671a0bbdbd372de03f71bd5905109112f41cc1
Robert Niederreiter <office@squarewave.at>
2018-11-09 15:13:20 +0100

    Add patch for disabling large file support when compiling python2

--- pythonforandroid/recipes/python2/__init__.py
+++ pythonforandroid/recipes/python2/__init__.py
@@ -32,6 +32,7 @@ class Python2Recipe(TargetPythonRecipe):
                'patches/ctypes-find-library-updated.patch',
                'patches/disable-ctermid.patch',
                'patches/disable-getpwall.patch',
+               'patches/disable-lfs.patch',
                ('patches/fix-configure-darwin.patch', is_darwin),
                ('patches/fix-distutils-darwin.patch', is_darwin),
                ('patches/fix-ftime-removal.patch', is_api_gt(19)),
