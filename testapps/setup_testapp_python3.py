82ad9e5d9da9742b64fb25a73295ba8b8f809e02
Alexander Taylor <alexanderjohntaylor@gmail.com>
2018-11-10 00:43:33 +0000

    Updated python3 testapp ndk-api to 21

--- testapps/setup_testapp_python3.py
+++ testapps/setup_testapp_python3.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 options = {'apk': {'requirements': 'sdl2,pyjnius,kivy,python3',
                    'android-api': 27,
-                   'ndk-api': 19,
+                   'ndk-api': 21,
                    'dist-name': 'bdisttest_python3_googlendk',
                    'ndk-version': '10.3.2',
                    'arch': 'armeabi-v7a',
