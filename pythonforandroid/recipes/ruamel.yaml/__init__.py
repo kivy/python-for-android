d8d01d89710cd1d752809b8cd91d934092e99adf
Andre Miras <AndreMiras@users.noreply.github.com>
2018-11-11 18:35:47 +0100

    Update to last version, fixes import and deps

--- pythonforandroid/recipes/ruamel.yaml/__init__.py
+++ pythonforandroid/recipes/ruamel.yaml/__init__.py
@@ -1,14 +1,13 @@
-from pythonforandroid.toolchain import PythonRecipe
+from pythonforandroid.recipe import PythonRecipe
 
 
 class RuamelYamlRecipe(PythonRecipe):
-    version = '0.14.5'
-    url = 'https://pypi.python.org/packages/5c/13/c120a06b3add0f9763ca9190e5f6edb9faf9d34b158dd3cff7cc9097be03/ruamel.yaml-{version}.tar.gz'
-
-    depends = [ ('python2', 'python3crystax') ]
+    version = '0.15.77'
+    url = 'https://pypi.python.org/packages/source/r/ruamel.yaml/ruamel.yaml-{version}.tar.gz'
+    depends = [('python2', 'python3crystax'), 'setuptools']
     site_packages_name = 'ruamel'
     call_hostpython_via_targetpython = False
-
     patches = ['disable-pip-req.patch']
+
 
 recipe = RuamelYamlRecipe()
