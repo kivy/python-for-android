10e53bc2ace554b29e9e64e25b1206507e9c36d8
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-11-23 08:40:34 -0500

    Update __init__.py

--- pythonforandroid/recipes/python3/__init__.py
+++ pythonforandroid/recipes/python3/__init__.py
@@ -72,12 +72,12 @@ class Python3Recipe(TargetPythonRecipe):
             platform_dir = join(self.ctx.ndk_dir, 'platforms', platform_name, arch.platform_dir)
             toolchain = '{android_host}-4.9'.format(android_host=arch.toolchain_prefix)
             toolchain = join(self.ctx.ndk_dir, 'toolchains', toolchain, 'prebuilt', 'linux-x86_64')
-            
+
             target_data = arch.command_prefix.split('-')
             if target_data[0] == 'arm':
                 target_data[0] = 'armv7a'
             target = '-'.join([target_data[0], 'none', target_data[1], target_data[2]])
-    
+
             CC = '{clang} -target {target} -gcc-toolchain {toolchain}'.format(
                 clang=join(self.ctx.ndk_dir, 'toolchains', 'llvm', 'prebuilt', 'linux-x86_64', 'bin', 'clang'),
                 target=target,
