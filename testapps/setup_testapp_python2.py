e11c4d1fb7394a2c5ab2174194f7de41095a129c
opacam <canellestudi@gmail.com>
2018-12-04 16:07:20 +0100

    Fix numpy's recipe and adds python3's compatibility.
    Also:
      - add numpy to CI tests for python3: this module is some kind of special and probably a dependency for a lot of python packages so better make sure that we not break anything...as we already do with python2
      - Add numpy to python2 and python3 test apps, cause already have some buttons that allows to test it from the running app.

--- testapps/setup_testapp_python2.py
+++ testapps/setup_testapp_python2.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-options = {'apk': {'requirements': 'sdl2,pyjnius,kivy,python2',
+options = {'apk': {'requirements': 'sdl2,numpy,pyjnius,kivy,python2',
                    'android-api': 27,
                    'ndk-api': 21,
                    'ndk-dir': '/home/asandy/android/crystax-ndk-10.3.2',
