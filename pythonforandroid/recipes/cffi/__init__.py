9485875a73703defd17c31cae8bd9ba0dc920d96
opacam <canellestudi@gmail.com>
2018-12-10 09:26:04 +0100

    Fix openssl dependant recipe: cryptography+cffi
    Since the recent openssl upgrade this recipe stopped to work. Here we adapt the recipe to the new build circumstances and we also fix the cffi recipe because it is a direct dependency of cryptography and had the wrong flags for the new python2 build system.

--- pythonforandroid/recipes/cffi/__init__.py
+++ pythonforandroid/recipes/cffi/__init__.py
@@ -27,11 +27,10 @@ class CffiRecipe(CompiledComponentsPythonRecipe):
 
     def get_recipe_env(self, arch=None):
         env = super(CffiRecipe, self).get_recipe_env(arch)
-        # sets linker to use the correct gcc (cross compiler)
-        env['LDSHARED'] = env['CC'] + ' -pthread -shared -Wl,-O1 -Wl,-Bsymbolic-functions'
         libffi = self.get_recipe('libffi', self.ctx)
         includes = libffi.get_include_dirs(arch)
         env['CFLAGS'] = ' -I'.join([env.get('CFLAGS', '')] + includes)
+        env['CFLAGS'] += ' -I{}'.format(self.ctx.python_recipe.include_root(arch.arch))
         env['LDFLAGS'] = (env.get('CFLAGS', '') + ' -L' +
                           self.ctx.get_libs_dir(arch.arch))
         env['LDFLAGS'] += ' -L{}'.format(os.path.join(self.ctx.bootstrap.build_dir, 'libs', arch.arch))
@@ -44,15 +43,10 @@ class CffiRecipe(CompiledComponentsPythonRecipe):
             self.ctx.get_site_packages_dir(),
             env['BUILDLIB_PATH'],
         ])
-        if self.ctx.ndk == 'crystax':
-            # only keeps major.minor (discards patch)
-            python_version = self.ctx.python_recipe.version[0:3]
-            ndk_dir_python = os.path.join(self.ctx.ndk_dir, 'sources/python/', python_version)
-            env['LDFLAGS'] += ' -L{}'.format(os.path.join(ndk_dir_python, 'libs', arch.arch))
-            env['LDFLAGS'] += ' -lpython{}m'.format(python_version)
-            # until `pythonforandroid/archs.py` gets merged upstream:
-            # https://github.com/kivy/python-for-android/pull/1250/files#diff-569e13021e33ced8b54385f55b49cbe6
-            env['CFLAGS'] += ' -I{}/include/python/'.format(ndk_dir_python)
+        env['LDFLAGS'] += ' -L{}'.format(self.ctx.python_recipe.link_root(arch.arch))
+        env['LDFLAGS'] += ' -lpython{}'.format(self.ctx.python_recipe.major_minor_version_string)
+        if 'python3' in self.ctx.python_recipe.name:
+            env['LDFLAGS'] += 'm'
         return env
 
 
