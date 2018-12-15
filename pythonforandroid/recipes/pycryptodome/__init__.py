6e2da38e47d05560984c99a1b7225259c5f1f37b
opacam <canellestudi@gmail.com>
2018-12-15 10:13:51 +0100

    Grant python3 compatibility for PyCryptoDome recipe

--- pythonforandroid/recipes/pycryptodome/__init__.py
+++ pythonforandroid/recipes/pycryptodome/__init__.py
@@ -4,7 +4,7 @@ from pythonforandroid.recipe import PythonRecipe
 class PycryptodomeRecipe(PythonRecipe):
     version = '3.4.6'
     url = 'https://github.com/Legrandin/pycryptodome/archive/v{version}.tar.gz'
-    depends = [('python2', 'python3crystax'), 'setuptools', 'cffi']
+    depends = ['setuptools', 'cffi']
 
     def get_recipe_env(self, arch=None, with_flags_in_cc=True):
         env = super(PycryptodomeRecipe, self).get_recipe_env(arch, with_flags_in_cc)
