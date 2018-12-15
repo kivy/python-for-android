28c9b4732124a400ee5c29b3afcdfcff10196ff0
opacam <canellestudi@gmail.com>
2018-12-15 10:18:50 +0100

    Fix hardcoded/unneeded flags for pysha3 recipe and grant python3 compatibility

--- pythonforandroid/recipes/pysha3/__init__.py
+++ pythonforandroid/recipes/pysha3/__init__.py
@@ -6,14 +6,16 @@ from pythonforandroid.recipe import PythonRecipe
 class Pysha3Recipe(PythonRecipe):
     version = '1.0.2'
     url = 'https://github.com/tiran/pysha3/archive/{version}.tar.gz'
-    depends = [('python2', 'python3crystax'), 'setuptools']
+    depends = ['setuptools']
+    call_hostpython_via_targetpython = False
 
     def get_recipe_env(self, arch=None, with_flags_in_cc=True):
         env = super(Pysha3Recipe, self).get_recipe_env(arch, with_flags_in_cc)
-        # sets linker to use the correct gcc (cross compiler)
-        env['LDSHARED'] = env['CC'] + ' -pthread -shared -Wl,-O1 -Wl,-Bsymbolic-functions'
         # CFLAGS may only be used to specify C compiler flags, for macro definitions use CPPFLAGS
-        env['CPPFLAGS'] = env['CFLAGS'] + ' -I{}/sources/python/3.5/include/python/'.format(self.ctx.ndk_dir)
+        env['CPPFLAGS'] = env['CFLAGS']
+        if self.ctx.ndk == 'crystax':
+            env['CPPFLAGS'] += ' -I{}/sources/python/{}/include/python/'.format(
+                self.ctx.ndk_dir, self.ctx.python_recipe.version[0:3])
         env['CFLAGS'] = ''
         # LDFLAGS may only be used to specify linker flags, for libraries use LIBS
         env['LDFLAGS'] = env['LDFLAGS'].replace('-lm', '').replace('-lcrystax', '')
