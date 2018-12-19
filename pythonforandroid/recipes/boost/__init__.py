f551a85f37b6f1af144d5d495810509ca06582cf
opacam <canellestudi@gmail.com>
2018-12-19 13:28:55 +0100

    Fix libtorrent+boost (update to latest versions and add python3 compatibility)
    Since the openssl and ndk update the boost and libtorrent recipes weren't working. The old version for boost libraries does not support openssl 1.1, so we must update it. The old libtorrent recipe is strongly dependant on boost so we also update it. Also, before this commit, the compiler used to make those builds were gcc, now we use clang (the default compiler for the latest android ndk).
    
    Note: Those recipes were tested for both python versions with a slight modification of the test apps and importing the libtorrent at runtime without problems, plus the generated libraries are linked with our versions of openssl and python (using the readelf command)

--- pythonforandroid/recipes/boost/__init__.py
+++ pythonforandroid/recipes/boost/__init__.py
@@ -1,5 +1,6 @@
 from pythonforandroid.toolchain import Recipe, shprint, shutil, current_directory
 from os.path import join, exists
+from os import environ
 import sh
 
 """
@@ -9,11 +10,36 @@ including python bindings
 
 
 class BoostRecipe(Recipe):
-    version = '1.60.0'
-    # Don't forget to change the URL when changing the version
-    url = 'http://downloads.sourceforge.net/project/boost/boost/{version}/boost_1_60_0.tar.bz2'
-    depends = ['python2']
-    patches = ['disable-so-version.patch', 'use-android-libs.patch']
+    # Todo: make recipe compatible with all p4a architectures
+    '''
+    .. note:: This recipe can be built only against API 21+ and arch armeabi-v7a
+
+    .. versionchanged:: 0.6.0
+         Rewrote recipe to support clang's build. The following changes has
+         been made:
+
+            - Bumped version number to 1.68.0
+            - Better version handling for url
+            - Added python 3 compatibility
+            - Default compiler for ndk's toolchain set to clang
+            - Python version will be detected via user-config.jam
+            - Changed stl's lib from ``gnustl_shared`` to ``c++_shared``
+    '''
+    version = '1.68.0'
+    url = 'http://downloads.sourceforge.net/project/boost/' \
+          'boost/{version}/boost_{version_underscore}.tar.bz2'
+    depends = [('python2', 'python3')]
+    patches = ['disable-so-version.patch',
+               'use-android-libs.patch',
+               'fix-android-issues.patch']
+
+    @property
+    def versioned_url(self):
+        if self.url is None:
+            return None
+        return self.url.format(
+            version=self.version,
+            version_underscore=self.version.replace('.', '_'))
 
     def should_build(self, arch):
         return not exists(join(self.get_build_dir(arch.arch), 'b2'))
@@ -28,9 +54,11 @@ class BoostRecipe(Recipe):
                 shprint(bash, join(self.ctx.ndk_dir, 'build/tools/make-standalone-toolchain.sh'),
                         '--arch=' + env['ARCH'],
                         '--platform=android-' + str(self.ctx.android_api),
-                        '--toolchain=' + env['CROSSHOST'] + '-' + env['TOOLCHAIN_VERSION'],
+                        '--toolchain=' + env['CROSSHOST'] + '-' + self.ctx.toolchain_version + ':-llvm',
+                        '--use-llvm',
+                        '--stl=libc++',
                         '--install-dir=' + env['CROSSHOME']
-                )
+                        )
             # Set custom configuration
             shutil.copyfile(join(self.get_recipe_dir(), 'user-config.jam'),
                             join(env['BOOST_BUILD_PATH'], 'user-config.jam'))
@@ -38,31 +66,38 @@ class BoostRecipe(Recipe):
     def build_arch(self, arch):
         super(BoostRecipe, self).build_arch(arch)
         env = self.get_recipe_env(arch)
+        env['PYTHON_HOST'] = self.ctx.hostpython
         with current_directory(self.get_build_dir(arch.arch)):
             # Compile Boost.Build engine with this custom toolchain
             bash = sh.Command('bash')
-            shprint(bash, 'bootstrap.sh',
-                    '--with-python=' + join(env['PYTHON_ROOT'], 'bin/python.host'),
-                    '--with-python-version=2.7',
-                    '--with-python-root=' + env['PYTHON_ROOT']
-            )  # Do not pass env
+            shprint(bash, 'bootstrap.sh')  # Do not pass env
         # Install app stl
-        shutil.copyfile(join(env['CROSSHOME'], env['CROSSHOST'], 'lib/libgnustl_shared.so'),
-                        join(self.ctx.get_libs_dir(arch.arch), 'libgnustl_shared.so'))
+        shutil.copyfile(
+            join(self.ctx.ndk_dir, 'sources/cxx-stl/llvm-libc++/libs/'
+                                   'armeabi-v7a/libc++_shared.so'),
+            join(self.ctx.get_libs_dir(arch.arch), 'libc++_shared.so'))
 
     def select_build_arch(self, arch):
         return arch.arch.replace('eabi-v7a', '').replace('eabi', '')
 
     def get_recipe_env(self, arch):
-        env = super(BoostRecipe, self).get_recipe_env(arch)
+        # We don't use the normal env because we
+        # are building with a standalone toolchain
+        env = environ.copy()
+
         env['BOOST_BUILD_PATH'] = self.get_build_dir(arch.arch)  # find user-config.jam
         env['BOOST_ROOT'] = env['BOOST_BUILD_PATH']  # find boost source
-        env['PYTHON_ROOT'] = self.ctx.get_python_install_dir()
+
+        env['PYTHON_ROOT'] = self.ctx.python_recipe.link_root(arch.arch)
+        env['PYTHON_INCLUDE'] = self.ctx.python_recipe.include_root(arch.arch)
+        env['PYTHON_MAJOR_MINOR'] = self.ctx.python_recipe.version[:3]
+        env['PYTHON_LINK_VERSION'] = self.ctx.python_recipe.major_minor_version_string
+        if 'python3' in self.ctx.python_recipe.name:
+            env['PYTHON_LINK_VERSION'] += 'm'
+
         env['ARCH'] = self.select_build_arch(arch)
-        env['ANDROIDAPI'] = str(self.ctx.android_api)
         env['CROSSHOST'] = env['ARCH'] + '-linux-androideabi'
         env['CROSSHOME'] = join(env['BOOST_ROOT'], 'standalone-' + env['ARCH'] + '-toolchain')
-        env['TOOLCHAIN_PREFIX'] = join(env['CROSSHOME'], 'bin', env['CROSSHOST'])
         return env
 
 
