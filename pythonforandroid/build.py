c32741e916b87bfc591b221cb2e5223cdccc9708
opacam <canellestudi@gmail.com>
2018-12-27 18:50:22 +0100

    Python2legacy: fix get_site_packages_dir for python2legacy

--- pythonforandroid/build.py
+++ pythonforandroid/build.py
@@ -536,6 +536,9 @@ class Context(object):
         '''Returns the location of site-packages in the python-install build
         dir.
         '''
+        if self.python_recipe.name == 'python2legacy':
+            return join(self.get_python_install_dir(),
+                        'lib', 'python2.7', 'site-packages')
         return self.get_python_install_dir()
 
     def get_libs_dir(self, arch):
