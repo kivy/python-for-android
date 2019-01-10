ee9c61c4f307d3489379d50004e3b546e965aabe
opacam <canellestudi@gmail.com>
2019-01-10 21:57:13 +0100

    Fix dependency graph error

--- pythonforandroid/recipes/ipaddress/__init__.py
+++ pythonforandroid/recipes/ipaddress/__init__.py
@@ -5,6 +5,7 @@ class IpaddressRecipe(PythonRecipe):
     name = 'ipaddress'
     version = '1.0.22'
     url = 'https://github.com/phihag/ipaddress/archive/v{version}.tar.gz'
+    depends = ['setuptools']
 
 
 recipe = IpaddressRecipe()
