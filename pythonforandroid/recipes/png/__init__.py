4b0fafdae23eee55df167fec9a61f9e803915a83
opacam <canellestudi@gmail.com>
2019-01-10 02:19:42 +0100

    Update png version

--- pythonforandroid/recipes/png/__init__.py
+++ pythonforandroid/recipes/png/__init__.py
@@ -3,8 +3,8 @@ from pythonforandroid.recipe import NDKRecipe
 
 class PngRecipe(NDKRecipe):
     name = 'png'
-    version = '1.6.15'
-    url = 'https://github.com/julienr/libpng-android/archive/{version}.zip'
+    version = '1.6.29'
+    url = 'https://github.com/julienr/libpng-android/archive/master.zip'
 
     generated_libraries = ['libpng.a']
 
