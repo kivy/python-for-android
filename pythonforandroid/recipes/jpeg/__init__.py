2f5e45a40d530af07146b7968778e1b7928bf8ad
opacam <canellestudi@gmail.com>
2019-01-10 02:21:06 +0100

    Update jpeg and move to mainline
    Also add a patch that may allow us to build the libraries in his shared form...in case we need them

--- pythonforandroid/recipes/jpeg/__init__.py
+++ pythonforandroid/recipes/jpeg/__init__.py
@@ -1,35 +1,78 @@
-from pythonforandroid.recipe import NDKRecipe
+from pythonforandroid.recipe import Recipe
 from pythonforandroid.logger import shprint
 from pythonforandroid.util import current_directory
 from os.path import join, exists
+from os import environ, uname
+from glob import glob
 import sh
 
 
-class JpegRecipe(NDKRecipe):
+class JpegRecipe(Recipe):
+    '''
+    .. versionchanged:: 0.6.0
+        rewrote recipe to be build with clang and updated libraries to latest
+        version of the official git repo.
+    '''
     name = 'jpeg'
-    version = 'linaro-android'
-    url = 'git+https://git.linaro.org/people/tomgall/libjpeg-turbo/libjpeg-turbo'
+    version = '2.0.1'
+    url = 'https://github.com/libjpeg-turbo/libjpeg-turbo/archive/{version}.tar.gz'  # noqa
+    # we will require this below patch to build the shared library
+    # patches = ['remove-version.patch']
 
-    patches = ['build-static.patch']
-
-    generated_libraries = ['libjpeg.a']
-
-    def prebuild_arch(self, arch):
-        super(JpegRecipe, self).prebuild_arch(arch)
-
-        build_dir = self.get_build_dir(arch.arch)
-        app_mk = join(build_dir, 'Application.mk')
-        if not exists(app_mk):
-            shprint(sh.cp, join(self.get_recipe_dir(), 'Application.mk'), app_mk)
-        jni_ln = join(build_dir, 'jni')
-        if not exists(jni_ln):
-            shprint(sh.ln, '-s', build_dir, jni_ln)
+    def should_build(self, arch):
+        return not exists(join(self.get_build_dir(arch.arch),
+                               'libturbojpeg.a'))
 
     def build_arch(self, arch):
         super(JpegRecipe, self).build_arch(arch)
-        with current_directory(self.get_lib_dir(arch)):
-            shprint(sh.mv, 'libjpeg.a', 'libjpeg-orig.a')
-            shprint(sh.ar, '-rcT', 'libjpeg.a', 'libjpeg-orig.a', 'libsimd.a')
+        build_dir = self.get_build_dir(arch.arch)
+
+        # TODO: Fix simd/neon
+        with current_directory(build_dir):
+            env = self.get_recipe_env(arch)
+            toolchain_file = join(self.ctx.ndk_dir,
+                                  'build/cmake/android.toolchain.cmake')
+
+            shprint(sh.rm, '-f', 'CMakeCache.txt', 'CMakeFiles/')
+            shprint(sh.cmake, '-G', 'Unix Makefiles',
+                    '-DCMAKE_SYSTEM_NAME=Android',
+                    '-DCMAKE_SYSTEM_PROCESSOR={cpu}'.format(cpu='arm'),
+                    '-DCMAKE_POSITION_INDEPENDENT_CODE=1',
+                    '-DCMAKE_ANDROID_ARCH_ABI={arch}'.format(arch=arch.arch),
+                    '-DCMAKE_ANDROID_NDK=' + self.ctx.ndk_dir,
+                    '-DCMAKE_C_COMPILER={toolchain}/bin/clang'.format(
+                        toolchain=env['TOOLCHAIN']),
+                    '-DCMAKE_CXX_COMPILER={toolchain}/bin/clang++'.format(
+                        toolchain=env['TOOLCHAIN']),
+                    '-DCMAKE_BUILD_TYPE=Release',
+                    '-DCMAKE_INSTALL_PREFIX=./install',
+                    '-DCMAKE_TOOLCHAIN_FILE=' + toolchain_file,
+
+                    '-DANDROID_ABI={arch}'.format(arch=arch.arch),
+                    '-DANDROID_ARM_NEON=ON',
+                    '-DENABLE_NEON=ON',
+                    # '-DREQUIRE_SIMD=1',
+
+                    # Force disable shared, with the static ones is enough
+                    '-DENABLE_SHARED=0',
+                    '-DENABLE_STATIC=1',
+                    _env=env)
+            shprint(sh.make, _env=env)
+
+            # copy static libs to libs collection
+            for lib in glob(join(build_dir, '*.a')):
+                shprint(sh.cp, '-L', lib, self.ctx.libs_dir)
+
+    def get_recipe_env(self, arch=None, with_flags_in_cc=False, clang=True):
+        env = environ.copy()
+
+        build_platform = '{system}-{machine}'.format(
+            system=uname()[0], machine=uname()[-1]).lower()
+        env['TOOLCHAIN'] = join(self.ctx.ndk_dir, 'toolchains/llvm/'
+                                'prebuilt/{build_platform}'.format(
+                                    build_platform=build_platform))
+
+        return env
 
 
 recipe = JpegRecipe()
