f7d987ad6c06831097118549708818cfaaa510b2
opacam <canellestudi@gmail.com>
2019-01-11 16:45:01 +0100

    Rework libxslt recipe and update version
    Actions taken:
      - replace double quotes to single quotes because I think that it's more readable
      - fixed imports
      - fix hardcoded entries

--- pythonforandroid/recipes/libxslt/__init__.py
+++ pythonforandroid/recipes/libxslt/__init__.py
@@ -1,61 +1,73 @@
-from pythonforandroid.toolchain import Recipe, shprint, shutil, current_directory
+from pythonforandroid.recipe import Recipe
+from pythonforandroid.toolchain import shprint, shutil, current_directory
 from os.path import exists, join
 import sh
 
 
 class LibxsltRecipe(Recipe):
-    version = "1.1.28"
-    url = "http://xmlsoft.org/sources/libxslt-{version}.tar.gz"
-    depends = ["libxml2"]
-    patches = ["fix-dlopen.patch"]
+    version = '1.1.32'
+    url = 'http://xmlsoft.org/sources/libxslt-{version}.tar.gz'
+    depends = ['libxml2']
+    patches = ['fix-dlopen.patch']
 
     call_hostpython_via_targetpython = False
 
     def should_build(self, arch):
-        super(LibxsltRecipe, self).should_build(arch)
-        return not exists(join(self.ctx.get_libs_dir(arch.arch), "libxslt.a"))
+        return not exists(
+            join(self.get_build_dir(arch.arch),
+                 'libxslt', '.libs', 'libxslt.a'))
 
     def build_arch(self, arch):
         super(LibxsltRecipe, self).build_arch(arch)
         env = self.get_recipe_env(arch)
-        with current_directory(self.get_build_dir(arch.arch)):
+        build_dir = self.get_build_dir(arch.arch)
+        with current_directory(build_dir):
             # If the build is done with /bin/sh things blow up,
             # try really hard to use bash
-            env["CC"] += " -I%s" % self.get_build_dir(arch.arch)
-            libxml = Recipe.get_recipe(
-                'libxml2', self.ctx).get_build_dir(arch.arch)
-            shprint(
-                sh.Command("./configure"),
-                "--build=i686-pc-linux-gnu",
-                "--host=arm-linux-eabi",
-                "--without-plugins",
-                "--without-debug",
-                "--without-python",
-                "--without-crypto",
-                "--with-libxml-src=%s" % libxml,
-                _env=env,
-            )
+            libxml2_recipe = Recipe.get_recipe('libxml2', self.ctx)
+            libxml2_build_dir = libxml2_recipe.get_build_dir(arch.arch)
+            build_arch = shprint(sh.gcc, '-dumpmachine').stdout.decode(
+                'utf-8').split('\n')[0]
+
+            if not exists('configure'):
+                shprint(sh.Command('./autogen.sh'), _env=env)
+            shprint(sh.Command('autoreconf'), '-vif', _env=env)
+            shprint(sh.Command('./configure'),
+                    '--build=' + build_arch,
+                    '--host=' + arch.command_prefix,
+                    '--target=' + arch.command_prefix,
+                    '--without-plugins',
+                    '--without-debug',
+                    '--without-python',
+                    '--without-crypto',
+                    '--with-libxml-src=' + libxml2_build_dir,
+                    '--disable-shared',
+                    _env=env)
             shprint(sh.make, "V=1", _env=env)
-            shutil.copyfile(
-                "libxslt/.libs/libxslt.a",
-                join(self.ctx.get_libs_dir(arch.arch), "libxslt.a"),
-            )
-            shutil.copyfile(
-                "libexslt/.libs/libexslt.a",
-                join(self.ctx.get_libs_dir(arch.arch), "libexslt.a"),
-            )
+
+            shutil.copyfile('libxslt/.libs/libxslt.a',
+                            join(self.ctx.libs_dir, 'libxslt.a'))
+            shutil.copyfile('libexslt/.libs/libexslt.a',
+                            join(self.ctx.libs_dir, 'libexslt.a'))
 
     def get_recipe_env(self, arch):
         env = super(LibxsltRecipe, self).get_recipe_env(arch)
-        env["CONFIG_SHELL"] = "/bin/bash"
-        env["SHELL"] = "/bin/bash"
-        env[
-            "CC"
-        ] = "arm-linux-androideabi-gcc -DANDROID -mandroid -fomit-frame-pointer --sysroot={}".format(
-            self.ctx.ndk_platform
-        )
+        env['CONFIG_SHELL'] = '/bin/bash'
+        env['SHELL'] = '/bin/bash'
 
-        env["LDSHARED"] = "%s -nostartfiles -shared -fPIC" % env["CC"]
+        libxml2_recipe = Recipe.get_recipe('libxml2', self.ctx)
+        libxml2_build_dir = libxml2_recipe.get_build_dir(arch.arch)
+        libxml2_libs_dir = join(libxml2_build_dir, '.libs')
+
+        env['CFLAGS'] = ' '.join([
+            env['CFLAGS'],
+            '-I' + libxml2_build_dir,
+            '-I' + join(libxml2_build_dir, 'include', 'libxml'),
+            '-I' + self.get_build_dir(arch.arch),
+        ])
+        env['LDFLAGS'] += ' -L' + libxml2_libs_dir
+        env['LIBS'] = '-lxml2 -lz -lm'
+
         return env
 
 
