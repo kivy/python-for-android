5169b5e97323b3148605ba2172071b307a8885e8
opacam <canellestudi@gmail.com>
2019-01-15 12:21:27 +0100

    Fix hardcoded url
    ¡¡¡Thanks @AndreMiras!!!

--- pythonforandroid/recipes/netifaces/__init__.py
+++ pythonforandroid/recipes/netifaces/__init__.py
@@ -5,7 +5,7 @@ class NetifacesRecipe(CompiledComponentsPythonRecipe):
 
     version = '0.10.7'
 
-    url = 'https://files.pythonhosted.org/packages/81/39/4e9a026265ba944ddf1fea176dbb29e0fe50c43717ba4fcf3646d099fe38/netifaces-{version}.tar.gz'
+    url = 'https://files.pythonhosted.org/packages/source/n/netifaces/netifaces-{version}.tar.gz'
 
     depends = ['setuptools']
 
