973624373db94da96a6c339b9e2b6eb665c4a968
Andre Miras <andre.miras@gmail.com>
2019-02-01 18:33:03 +0100

    Configure en_US.UTF-8 locale on Docker, closes #1654
    Also unit tests how `shprint()` handles unicode command output.

---
+++ tests/test_logger.py
@@ -0,0 +1,18 @@
+import unittest
+from mock import MagicMock
+from pythonforandroid import logger
+
+
+class TestShprint(unittest.TestCase):
+
+    def test_unicode_encode(self):
+        """
+        Makes sure `shprint()` can handle unicode command output.
+        Running the test with PYTHONIOENCODING=ASCII env would fail, refs:
+        https://github.com/kivy/python-for-android/issues/1654
+        """
+        expected_command_output = ["foo\xa0bar"]
+        command = MagicMock()
+        command.return_value = expected_command_output
+        output = logger.shprint(command, 'a1', k1='k1')
+        self.assertEqual(output, expected_command_output)
