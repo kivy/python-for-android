999df75d2fce65d910c07d23b63b8a9dfe49b1c2
opacam <canellestudi@gmail.com>
2019-02-08 12:24:37 +0100

    Defaults to post kivy==1.10.1, commit for `fixes SDL2 image loading (jpg)`

--- pythonforandroid/recipes/kivy/__init__.py
+++ pythonforandroid/recipes/kivy/__init__.py
@@ -6,7 +6,8 @@ import glob
 
 
 class KivyRecipe(CythonRecipe):
-    version = 'master'
+    # post kivy==1.10.1, `fixes SDL2 image loading (jpg)`
+    version = 'a95d67f'
     url = 'https://github.com/kivy/kivy/archive/{version}.zip'
     name = 'kivy'
 
