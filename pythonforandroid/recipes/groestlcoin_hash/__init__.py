523d0261944f1e5ae7cc3adb6966dea3e5958d9d
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-03-03 22:37:13 +0100

    fix groestlcoin_hash recipe (#1738)
    * fix groestlcoin_hash recipe

--- pythonforandroid/recipes/groestlcoin_hash/__init__.py
+++ pythonforandroid/recipes/groestlcoin_hash/__init__.py
@@ -5,7 +5,6 @@ class GroestlcoinHashRecipe(CythonRecipe):
     version = '1.0.1'
     url = 'https://github.com/Groestlcoin/groestlcoin-hash-python/archive/{version}.tar.gz'
     depends = []
-    call_hostpython_via_targetpython = True
     cythonize = False
 
 
