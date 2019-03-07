ac72720243b16e255c149c57a1d4b63f9b07beb2
Mirko Galimberti <me@mirkogalimberti.com>
2019-03-07 19:26:50 +0100

    Updated sdl2_mixer version to 2.0.4

--- pythonforandroid/recipes/sdl2_mixer/__init__.py
+++ pythonforandroid/recipes/sdl2_mixer/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import BootstrapNDKRecipe
 
 
 class LibSDL2Mixer(BootstrapNDKRecipe):
-    version = '2.0.1'
+    version = '2.0.4'
     url = 'https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-{version}.tar.gz'
     dir_name = 'SDL2_mixer'
 
