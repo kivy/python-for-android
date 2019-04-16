2d6fb119f2f4ca0754fcb5504b34586475b3ce25
sduenasg <sara85gon@gmail.com>
2019-04-16 20:07:41 +0200

    Fix sqlalchemy recipe build errors

--- pythonforandroid/recipes/sqlalchemy/__init__.py
+++ pythonforandroid/recipes/sqlalchemy/__init__.py
@@ -5,6 +5,7 @@ class SQLAlchemyRecipe(CompiledComponentsPythonRecipe):
     name = 'sqlalchemy'
     version = '1.0.9'
     url = 'https://pypi.python.org/packages/source/S/SQLAlchemy/SQLAlchemy-{version}.tar.gz'
+    call_hostpython_via_targetpython = False
 
     depends = ['setuptools']
 
