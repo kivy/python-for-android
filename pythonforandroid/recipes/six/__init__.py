25f05facd5c916a276aeb9ece440eb9934c12f43
Alexander Taylor <alexanderjohntaylor@gmail.com>
2019-05-21 21:06:43 +0100

    Updated six version

--- pythonforandroid/recipes/six/__init__.py
+++ pythonforandroid/recipes/six/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 
 class SixRecipe(PythonRecipe):
-    version = '1.9.0'
+    version = '1.10.0'
     url = 'https://pypi.python.org/packages/source/s/six/six-{version}.tar.gz'
     depends = [('python2', 'python2legacy', 'python3', 'python3crystax')]
 
