fb720224d45c48699dd1655cf8927675afed5308
Alexander Taylor <alexanderjohntaylor@gmail.com>
2019-05-28 21:46:49 +0100

    Added libpng to automatically loaded libraries

--- pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
+++ pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
@@ -37,6 +37,7 @@ public class PythonUtil {
         addLibraryIfExists(libsList, "ffi", libsDir);
         addLibraryIfExists(libsList, "ssl.*", libsDir);
         addLibraryIfExists(libsList, "crypto.*", libsDir);
+        addLibraryIfExists(libsList, "png16", libsDir);
         libsList.add("python2.7");
         libsList.add("python3.5m");
         libsList.add("python3.6m");
