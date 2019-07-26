8829783c35f2bebd08418aa54e096a5ce172daa9
opacam <canellestudi@gmail.com>
2019-07-26 12:34:11 +0200

    [recipe-lib] Make libiconv a library recipe and ...
    also make the imports from the right module

--- pythonforandroid/recipes/libiconv/__init__.py
+++ pythonforandroid/recipes/libiconv/__init__.py
@@ -1,5 +1,5 @@
-import os
-from pythonforandroid.toolchain import shprint, current_directory
+from pythonforandroid.logger import shprint
+from pythonforandroid.util import current_directory
 from pythonforandroid.recipe import Recipe
 from multiprocessing import cpu_count
 import sh
@@ -11,14 +11,11 @@ class LibIconvRecipe(Recipe):
 
     url = 'https://ftp.gnu.org/pub/gnu/libiconv/libiconv-{version}.tar.gz'
 
+    built_libraries = {'libiconv.so': 'lib/.libs'}
+
     patches = ['libiconv-1.15-no-gets.patch']
 
-    def should_build(self, arch):
-        return not os.path.exists(
-                os.path.join(self.ctx.get_libs_dir(arch.arch), 'libiconv.so'))
-
     def build_arch(self, arch):
-        super(LibIconvRecipe, self).build_arch(arch)
         env = self.get_recipe_env(arch)
         with current_directory(self.get_build_dir(arch.arch)):
             shprint(
@@ -27,8 +24,6 @@ class LibIconvRecipe(Recipe):
                 '--prefix=' + self.ctx.get_python_install_dir(),
                 _env=env)
             shprint(sh.make, '-j' + str(cpu_count()), _env=env)
-            libs = ['lib/.libs/libiconv.so']
-            self.install_libs(arch, *libs)
 
 
 recipe = LibIconvRecipe()
