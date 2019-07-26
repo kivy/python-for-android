158b1c040b26a4abb8c100c3921adfba0e915a18
opacam <canellestudi@gmail.com>
2019-07-26 11:53:15 +0200

    [recipe-lib] Make libcurl a library recipe and ...
    also make the imports from the right module

--- pythonforandroid/recipes/libcurl/__init__.py
+++ pythonforandroid/recipes/libcurl/__init__.py
@@ -1,20 +1,18 @@
 import sh
-from pythonforandroid.toolchain import Recipe, shprint, shutil, current_directory
-from os.path import exists, join
+from pythonforandroid.recipe import Recipe
+from pythonforandroid.util import current_directory
+from pythonforandroid.logger import shprint
+from os.path import join
 from multiprocessing import cpu_count
 
 
 class LibcurlRecipe(Recipe):
     version = '7.55.1'
     url = 'https://curl.haxx.se/download/curl-7.55.1.tar.gz'
+    built_libraries = {'libcurl.so': 'dist/lib'}
     depends = ['openssl']
 
-    def should_build(self, arch):
-        super(LibcurlRecipe, self).should_build(arch)
-        return not exists(join(self.ctx.get_libs_dir(arch.arch), 'libcurl.so'))
-
     def build_arch(self, arch):
-        super(LibcurlRecipe, self).build_arch(arch)
         env = self.get_recipe_env(arch)
 
         r = self.get_recipe('openssl', self.ctx)
@@ -31,10 +29,6 @@ class LibcurlRecipe(Recipe):
                 _env=env)
             shprint(sh.make, '-j', str(cpu_count()), _env=env)
             shprint(sh.make, 'install', _env=env)
-            shutil.copyfile('{}/lib/libcurl.so'.format(dst_dir),
-                            join(
-                                self.ctx.get_libs_dir(arch.arch),
-                                'libcurl.so'))
 
 
 recipe = LibcurlRecipe()
