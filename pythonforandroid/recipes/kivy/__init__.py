ea38331d25452a5eafe003bd0105f75098a5f266
Alexander Taylor <alexander@inclem.net>
2019-07-28 17:10:59 +0100

    Added setuptools to Kivy recipe requirements

--- pythonforandroid/recipes/kivy/__init__.py
+++ pythonforandroid/recipes/kivy/__init__.py
@@ -10,7 +10,7 @@ class KivyRecipe(CythonRecipe):
     url = 'https://github.com/kivy/kivy/archive/{version}.zip'
     name = 'kivy'
 
-    depends = ['sdl2', 'pyjnius']
+    depends = ['sdl2', 'pyjnius', 'setuptools']
 
     def cythonize_build(self, env, build_dir='.'):
         super(KivyRecipe, self).cythonize_build(env, build_dir=build_dir)
