86902009b3576620dbc81321173024e91a8e53df
opacam <canellestudi@gmail.com>
2019-07-30 09:09:18 +0200

    [recipe-stl] Make leveldb a library recipe and...
    make it work with the reworked snappy recipe

--- pythonforandroid/recipes/leveldb/__init__.py
+++ pythonforandroid/recipes/leveldb/__init__.py
@@ -1,47 +1,46 @@
-from pythonforandroid.toolchain import Recipe, shprint, shutil, current_directory
+from pythonforandroid.logger import shprint
+from pythonforandroid.util import current_directory
+from pythonforandroid.recipe import Recipe
+from multiprocessing import cpu_count
 from os.path import join
 import sh
 
 
 class LevelDBRecipe(Recipe):
-    version = '1.18'
-    url = 'https://github.com/google/leveldb/archive/v{version}.tar.gz'
-    opt_depends = ['snappy']
-    patches = ['disable-so-version.patch', 'find-snappy.patch']
-
-    def should_build(self, arch):
-        return not self.has_libs(arch, 'libleveldb.so', 'libgnustl_shared.so')
+    version = '1.22'
+    url = 'https://github.com/google/leveldb/archive/{version}.tar.gz'
+    depends = ['snappy']
+    built_libraries = {'libleveldb.so': '.'}
+    need_stl_shared = True
 
     def build_arch(self, arch):
-        super(LevelDBRecipe, self).build_arch(arch)
         env = self.get_recipe_env(arch)
-        with current_directory(self.get_build_dir(arch.arch)):
-            if 'snappy' in recipe.ctx.recipe_build_order:
-                # Copy source from snappy recipe
-                sh.cp('-rf', self.get_recipe('snappy', self.ctx).get_build_dir(arch.arch), 'snappy')
-            # Build
-            shprint(sh.make, _env=env)
-            # Copy the shared library
-            shutil.copyfile('libleveldb.so', join(self.ctx.get_libs_dir(arch.arch), 'libleveldb.so'))
-        # Copy stl
-        shutil.copyfile(self.ctx.ndk_dir + '/sources/cxx-stl/gnu-libstdc++/' + self.ctx.toolchain_version + '/libs/' + arch.arch + '/libgnustl_shared.so',
-                        join(self.ctx.get_libs_dir(arch.arch), 'libgnustl_shared.so'))
+        source_dir = self.get_build_dir(arch.arch)
+        with current_directory(source_dir):
+            snappy_recipe = self.get_recipe('snappy', self.ctx)
+            snappy_build = snappy_recipe.get_build_dir(arch.arch)
 
-    def get_recipe_env(self, arch):
-        env = super(LevelDBRecipe, self).get_recipe_env(arch)
-        env['TARGET_OS'] = 'OS_ANDROID_CROSSCOMPILE'
-        if 'snappy' in recipe.ctx.recipe_build_order:
-            env['CFLAGS'] += ' -DSNAPPY' + \
-                             ' -I./snappy'
-        env['CFLAGS'] += ' -I' + self.ctx.ndk_dir + '/platforms/android-' + str(self.ctx.android_api) + '/arch-' + arch.arch.replace('eabi', '') + '/usr/include' + \
-                         ' -I' + self.ctx.ndk_dir + '/sources/cxx-stl/gnu-libstdc++/' + self.ctx.toolchain_version + '/include' + \
-                         ' -I' + self.ctx.ndk_dir + '/sources/cxx-stl/gnu-libstdc++/' + self.ctx.toolchain_version + '/libs/' + arch.arch + '/include'
-        env['CXXFLAGS'] = env['CFLAGS']
-        env['CXXFLAGS'] += ' -frtti'
-        env['CXXFLAGS'] += ' -fexceptions'
-        env['LDFLAGS'] += ' -L' + self.ctx.ndk_dir + '/sources/cxx-stl/gnu-libstdc++/' + self.ctx.toolchain_version + '/libs/' + arch.arch + \
-                          ' -lgnustl_shared'
-        return env
+            shprint(sh.cmake, source_dir,
+                    '-DANDROID_ABI={}'.format(arch.arch),
+                    '-DANDROID_NATIVE_API_LEVEL={}'.format(self.ctx.ndk_api),
+                    '-DANDROID_STL=' + self.stl_lib_name,
+
+                    '-DCMAKE_TOOLCHAIN_FILE={}'.format(
+                        join(self.ctx.ndk_dir, 'build', 'cmake',
+                             'android.toolchain.cmake')),
+                    '-DCMAKE_BUILD_TYPE=Release',
+
+                    '-DBUILD_SHARED_LIBS=1',
+
+                    '-DHAVE_SNAPPY=1',
+                    '-DCMAKE_CXX_FLAGS=-I{path}'.format(path=snappy_build),
+                    '-DCMAKE_SHARED_LINKER_FLAGS=-L{path} -lsnappy'.format(
+                        path=snappy_build),
+                    '-DCMAKE_EXE_LINKER_FLAGS=-L{path} -lsnappy'.format(
+                        path=snappy_build),
+
+                    _env=env)
+            shprint(sh.make, '-j' + str(cpu_count()), _env=env)
 
 
 recipe = LevelDBRecipe()
