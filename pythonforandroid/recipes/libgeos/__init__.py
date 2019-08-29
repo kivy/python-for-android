9e39fbea705cc7c1263665139c5776e71045323a
Pol Canelles <canellestudi@gmail.com>
2019-08-29 22:51:35 +0200

    [WIP][LIBS - PART IV] Rework of shapely and libgeos (#1967)
    * [recipe-stl] Make libgeos a library recipe
    
    * [recipe-stl] Fix shapely for reworked libgeos

--- pythonforandroid/recipes/libgeos/__init__.py
+++ pythonforandroid/recipes/libgeos/__init__.py
@@ -1,44 +1,52 @@
-from pythonforandroid.toolchain import Recipe, shprint, shutil, current_directory
-from os.path import exists, join
-import sh
+from pythonforandroid.util import current_directory, ensure_dir
+from pythonforandroid.toolchain import shprint
+from pythonforandroid.recipe import Recipe
 from multiprocessing import cpu_count
+from os.path import join
+import sh
 
 
 class LibgeosRecipe(Recipe):
-    version = '3.5'
-    # url = 'http://download.osgeo.org/geos/geos-{version}.tar.bz2'
-    url = 'https://github.com/libgeos/libgeos/archive/svn-{version}.zip'
+    version = '3.7.1'
+    url = 'https://github.com/libgeos/libgeos/archive/{version}.zip'
     depends = []
-
-    def should_build(self, arch):
-        super(LibgeosRecipe, self).should_build(arch)
-        return not exists(join(self.ctx.get_libs_dir(arch.arch), 'libgeos_c.so'))
+    built_libraries = {
+        'libgeos.so': 'install_target/lib',
+        'libgeos_c.so': 'install_target/lib'
+    }
+    need_stl_shared = True
 
     def build_arch(self, arch):
-        super(LibgeosRecipe, self).build_arch(arch)
-        env = self.get_recipe_env(arch)
+        source_dir = self.get_build_dir(arch.arch)
+        build_target = join(source_dir, 'build_target')
+        install_target = join(source_dir, 'install_target')
 
-        with current_directory(self.get_build_dir(arch.arch)):
-            dst_dir = join(self.get_build_dir(arch.arch), 'dist')
-            bash = sh.Command('bash')
-            print("If this fails make sure you have autoconf and libtool installed")
-            shprint(bash, 'autogen.sh')  # Requires autoconf and libtool
-            shprint(bash, 'configure', '--host=arm-linux-androideabi', '--enable-shared', '--prefix={}'.format(dst_dir), _env=env)
-            shprint(sh.make, '-j', str(cpu_count()), _env=env)
+        ensure_dir(build_target)
+        with current_directory(build_target):
+            env = self.get_recipe_env(arch)
+            shprint(sh.cmake, source_dir,
+                    '-DANDROID_ABI={}'.format(arch.arch),
+                    '-DANDROID_NATIVE_API_LEVEL={}'.format(self.ctx.ndk_api),
+                    '-DANDROID_STL=' + self.stl_lib_name,
+
+                    '-DCMAKE_TOOLCHAIN_FILE={}'.format(
+                        join(self.ctx.ndk_dir, 'build', 'cmake',
+                             'android.toolchain.cmake')),
+                    '-DCMAKE_INSTALL_PREFIX={}'.format(install_target),
+                    '-DCMAKE_BUILD_TYPE=Release',
+
+                    '-DGEOS_ENABLE_TESTS=OFF',
+
+                    '-DBUILD_SHARED_LIBS=1',
+
+                    _env=env)
+            shprint(sh.make, '-j' + str(cpu_count()), _env=env)
+
+            # We make the install because this way we will have all the
+            # includes in one place (mostly we are interested in `geos_c.h`,
+            # which is not in the include folder, so this way we make easier to
+            # link with this library...case of shapely's recipe)
             shprint(sh.make, 'install', _env=env)
-            shutil.copyfile('{}/lib/libgeos_c.so'.format(dst_dir), join(self.ctx.get_libs_dir(arch.arch), 'libgeos_c.so'))
-
-    def get_recipe_env(self, arch):
-        env = super(LibgeosRecipe, self).get_recipe_env(arch)
-        env['CXXFLAGS'] += ' -I{}/sources/cxx-stl/gnu-libstdc++/4.8/include'.format(self.ctx.ndk_dir)
-        env['CXXFLAGS'] += ' -I{}/sources/cxx-stl/gnu-libstdc++/4.8/libs/{}/include'.format(
-            self.ctx.ndk_dir, arch)
-        env['CXXFLAGS'] += ' -L{}/sources/cxx-stl/gnu-libstdc++/4.8/libs/{}'.format(
-            self.ctx.ndk_dir, arch)
-        env['CXXFLAGS'] += ' -lgnustl_shared'
-        env['LDFLAGS'] += ' -L{}/sources/cxx-stl/gnu-libstdc++/4.8/libs/{}'.format(
-            self.ctx.ndk_dir, arch)
-        return env
 
 
 recipe = LibgeosRecipe()
