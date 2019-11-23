e2851d9b48ffbaeb83b59311a86aba165b013ee2
Mirko <me@mirkogalimberti.com>
2019-11-23 20:59:50 +0100

    Fixes ffmpeg build on ndk 19 (#1997)
    * ffmpeg builds w/ libx264 and openssl via ndk 19 fixes #1996
    * target a specific hash
    * update travis_wait to 30

--- pythonforandroid/recipes/ffpyplayer_codecs/__init__.py
+++ pythonforandroid/recipes/ffpyplayer_codecs/__init__.py
@@ -2,7 +2,8 @@ from pythonforandroid.toolchain import Recipe
 
 
 class FFPyPlayerCodecsRecipe(Recipe):
-    depends = ['libshine', 'libx264']
+    depends = ['libx264']
+    # disabled libshine due a missing symbol error (see ffmpeg recipe)
 
     def build_arch(self, arch):
         pass
