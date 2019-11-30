a880d5a8458cefd17c884e02d7226e0e810415f2
opacam <canellestudi@gmail.com>
2019-11-30 11:43:09 +0100

    [dist] Prettify `dist_info.json` file
    So it's easy for us to read it.
    
    Note: Also formatted lines to be PEP8 friendly (below 79 characters)

--- pythonforandroid/distribution.py
+++ pythonforandroid/distribution.py
@@ -245,15 +245,22 @@ class Distribution(object):
         with current_directory(dirn):
             info('Saving distribution info')
             with open('dist_info.json', 'w') as fileh:
-                json.dump({'dist_name': self.name,
-                           'bootstrap': self.ctx.bootstrap.name,
-                           'archs': [arch.arch for arch in self.ctx.archs],
-                           'ndk_api': self.ctx.ndk_api,
-                           'use_setup_py': self.ctx.use_setup_py,
-                           'recipes': self.ctx.recipe_build_order + self.ctx.python_modules,
-                           'hostpython': self.ctx.hostpython,
-                           'python_version': self.ctx.python_recipe.major_minor_version_string},
-                          fileh)
+                json.dump(
+                    {'dist_name': self.name,
+                     'bootstrap': self.ctx.bootstrap.name,
+                     'archs': [arch.arch for arch in self.ctx.archs],
+                     'ndk_api': self.ctx.ndk_api,
+                     'use_setup_py': self.ctx.use_setup_py,
+                     'recipes':
+                         self.ctx.recipe_build_order + self.ctx.python_modules,
+                     'hostpython': self.ctx.hostpython,
+                     'python_version':
+                         self.ctx.python_recipe.major_minor_version_string,
+                     },
+                    fileh,
+                    indent=4,
+                    sort_keys=True,
+                )
 
 
 def pretty_log_dists(dists, log_func=info):
