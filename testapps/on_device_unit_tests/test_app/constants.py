52162ef726773420e478814985b1fa7b4e41c504
opacam <canellestudi@gmail.com>
2019-12-15 14:42:51 +0100

    :package: Unify most of the test apps into one app
    So depending on the supplied recipes, a different test app will be ran.
    
    The supported test apps are:
      - kivy
      - flask
      - no-gui

---
+++ testapps/on_device_unit_tests/test_app/constants.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from os import environ
+
+RUNNING_ON_ANDROID = "ANDROID_APP_PATH" in environ
+
+FONT_SIZE_TITLE = 32 if RUNNING_ON_ANDROID else 60
+FONT_SIZE_SUBTITLE = 16 if RUNNING_ON_ANDROID else 32
+FONT_SIZE_TEXT = 8 if RUNNING_ON_ANDROID else 16
