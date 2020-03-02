fab33f53df77d067942ba1de4a6038b338a1277e
opacam <canellestudi@gmail.com>
2020-03-02 13:17:00 +0100

    [testapps] Remove `Pillow` and `harfbuzz`

--- testapps/on_device_unit_tests/setup.py
+++ testapps/on_device_unit_tests/setup.py
@@ -39,7 +39,7 @@ options = {
     'apk':
         {
             'requirements':
-                'sqlite3,libffi,openssl,pyjnius,kivy,python3,requests,harfbuzz,Pillow,matplotlib,kiwisolver',
+                'sqlite3,libffi,openssl,pyjnius,kivy,python3,requests,matplotlib,kiwisolver',
             'android-api': 27,
             'ndk-api': 21,
             'dist-name': 'bdist_unit_tests_app',
