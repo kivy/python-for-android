251f5f2bd4ad21b5612ffcac85e188bb3b033de9
opacam <canellestudi@gmail.com>
2020-03-25 13:05:49 +0100

    :arrow_up: Update `pandas` to latest version `v1.0.3`

--- pythonforandroid/recipes/pandas/__init__.py
+++ pythonforandroid/recipes/pandas/__init__.py
@@ -4,7 +4,7 @@ from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
 
 
 class PandasRecipe(CppCompiledComponentsPythonRecipe):
-    version = '1.0.2'
+    version = '1.0.3'
     url = 'https://github.com/pandas-dev/pandas/releases/download/v{version}/pandas-{version}.tar.gz'  # noqa
 
     depends = ['cython', 'numpy', 'pytz', 'libbz2', 'liblzma']
