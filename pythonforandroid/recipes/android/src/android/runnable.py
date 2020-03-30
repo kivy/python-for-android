7c11148b5502d20a8b4a21a68e556a4cce7056c9
oukiar <oukiar@gmail.com>
2020-03-30 10:03:09 -0600

    fix for the problem with decorator run_on_ui_thread and the local ref… (#1830)
    * fix for the problem with decorator run_on_ui_thread and the local reference table overflow
    
    * Added comment about why we cache function references
    
    Co-authored-by: Alexander Taylor <alexanderjohntaylor@gmail.com>

--- pythonforandroid/recipes/android/src/android/runnable.py
+++ pythonforandroid/recipes/android/src/android/runnable.py
@@ -7,9 +7,12 @@ Runnable
 from jnius import PythonJavaClass, java_method, autoclass
 from android.config import JAVA_NAMESPACE
 
-# reference to the activity
+# Reference to the activity
 _PythonActivity = autoclass(JAVA_NAMESPACE + '.PythonActivity')
 
+# Cache of functions table. In older Android versions the number of JNI references
+# is limited, so by caching them we avoid running out.
+__functionstable__ = {}
 
 class Runnable(PythonJavaClass):
     '''Wrapper around Java Runnable class. This class can be used to schedule a
@@ -27,6 +30,7 @@ class Runnable(PythonJavaClass):
         self.args = args
         self.kwargs = kwargs
         Runnable.__runnables__.append(self)
+        
         _PythonActivity.mActivity.runOnUiThread(self)
 
     @java_method('()V')
@@ -38,12 +42,24 @@ class Runnable(PythonJavaClass):
             traceback.print_exc()
 
         Runnable.__runnables__.remove(self)
+        
+        
 
 
 def run_on_ui_thread(f):
     '''Decorator to create automatically a :class:`Runnable` object with the
     function. The function will be delayed and call into the Activity thread.
     '''
+    
+    if f not in __functionstable__:
+        
+        rfunction = Runnable(f) #store the runnable function
+        
+        __functionstable__[f] = {"rfunction":rfunction}
+        
+    rfunction = __functionstable__[f]["rfunction"]
+    
     def f2(*args, **kwargs):
-        Runnable(f)(*args, **kwargs)
+        rfunction(*args, **kwargs)
+            
     return f2
