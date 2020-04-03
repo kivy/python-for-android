187f859b19f8606d73dbd2e0dccfe7f0c2b0723d
SomberNight <somber.night@protonmail.com>
2020-04-04 00:00:25 +0200

    recipe: update 'cryptography' and rm unnecessary dependencies
    - 'idna' no longer needed since version 2.5
    - 'asn1crypto' no longer needed since version 2.8
    - 'enum34' is just a backport of python 3.4+ functionality
    - 'ipaddress' is just a backport of python 3.3+ functionality

--- pythonforandroid/recipes/cryptography/__init__.py
+++ pythonforandroid/recipes/cryptography/__init__.py
@@ -3,10 +3,9 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe, Recipe
 
 class CryptographyRecipe(CompiledComponentsPythonRecipe):
     name = 'cryptography'
-    version = '2.6.1'
+    version = '2.8'
     url = 'https://github.com/pyca/cryptography/archive/{version}.tar.gz'
-    depends = ['openssl', 'idna', 'asn1crypto', 'six', 'setuptools',
-               'enum34', 'ipaddress', 'cffi']
+    depends = ['openssl', 'six', 'setuptools', 'cffi']
     call_hostpython_via_targetpython = False
 
     def get_recipe_env(self, arch):
