5344c56440e9e398cf5ba789d2b2fc17da0fd5e6
opacam <canellestudi@gmail.com>
2020-04-08 11:44:15 +0200

    :lock: Bump twisted version to `20.3.0`
    Because of a detected vulnerability with prior versions of twisted:
    
    **See also**: https://github.com/advisories/GHSA-h96w-mmrf-2h6v

--- pythonforandroid/recipes/twisted/__init__.py
+++ pythonforandroid/recipes/twisted/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import CythonRecipe
 
 
 class TwistedRecipe(CythonRecipe):
-    version = '19.7.0'
+    version = '20.3.0'
     url = 'https://github.com/twisted/twisted/archive/twisted-{version}.tar.gz'
 
     depends = ['setuptools', 'zope_interface', 'incremental', 'constantly']
