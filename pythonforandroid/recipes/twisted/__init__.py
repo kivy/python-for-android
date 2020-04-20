af8e1b740aa4488ffe8c86aecee55b440a6223df
pavelsof <pavelsof@gmail.com>
2020-04-20 23:08:00 +0200

    Better recipe for Twisted: removed the unit tests.

--- pythonforandroid/recipes/twisted/__init__.py
+++ pythonforandroid/recipes/twisted/__init__.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 from pythonforandroid.recipe import CythonRecipe
 
 
@@ -6,7 +9,7 @@ class TwistedRecipe(CythonRecipe):
     url = 'https://github.com/twisted/twisted/archive/twisted-{version}.tar.gz'
 
     depends = ['setuptools', 'zope_interface', 'incremental', 'constantly']
-    patches = ['incremental.patch']
+    patches = ['incremental.patch', 'remove_tests.patch']
 
     call_hostpython_via_targetpython = False
     install_in_hostpython = False
@@ -14,7 +17,13 @@ class TwistedRecipe(CythonRecipe):
     def prebuild_arch(self, arch):
         super().prebuild_arch(arch)
         # TODO Need to whitelist tty.pyo and termios.so here
-        print('Should remove twisted tests etc. here, but skipping for now')
+
+        # remove the unit test dirs
+        source_dir = os.path.join(self.get_build_dir(arch.arch), 'src/twisted')
+        for item in os.walk(source_dir):
+            if os.path.basename(item[0]) == 'test':
+                full_path = os.path.join(source_dir, item[0])
+                shutil.rmtree(full_path, ignore_errors=True)
 
     def get_recipe_env(self, arch):
         env = super().get_recipe_env(arch)
