77df9eed793b0082087fda9f20c9c156c4f6bc83
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-04-20 21:05:11 -0400

    Update build.py

--- pythonforandroid/build.py
+++ pythonforandroid/build.py
@@ -765,7 +765,6 @@ def run_pymodules_install(ctx, modules, project_dir=None,
         info('Will process project install, if it fails then the '
              'project may not be compatible for Android install.')
     venv = sh.Command('python3')
-    
     with current_directory(join(ctx.build_dir)):
         try:
             shprint(venv,
