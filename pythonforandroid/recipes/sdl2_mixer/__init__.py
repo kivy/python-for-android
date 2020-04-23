db0a6218935239910232a32f2ac563088a21c8b6
Rigel <zedr@zedr.com>
2020-04-23 22:53:11 +0200

    Enable modplug support for the SDL Mixer device on Android

--- pythonforandroid/recipes/sdl2_mixer/__init__.py
+++ pythonforandroid/recipes/sdl2_mixer/__init__.py
@@ -6,7 +6,7 @@ class LibSDL2Mixer(BootstrapNDKRecipe):
     url = 'https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-{version}.tar.gz'
     dir_name = 'SDL2_mixer'
 
-    patches = ['toggle_modplug_mikmod_smpeg_ogg.patch']
+    patches = ['toggle_smpeg_ogg.patch']
 
 
 recipe = LibSDL2Mixer()
