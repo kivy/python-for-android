87184302cc42ec8a476d039b46fa57416a643ff9
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-04-24 21:24:37 -0400

    Update build.py

--- pythonforandroid/build.py
+++ pythonforandroid/build.py
@@ -765,14 +765,25 @@ def run_pymodules_install(ctx, modules, project_dir=None,
         info('Will process project install, if it fails then the '
              'project may not be compatible for Android install.')
     venv = sh.Command('python3')
+   
     with current_directory(join(ctx.build_dir)):
-        shprint(venv,
-                '-m venv'.format(
-                    ctx.python_recipe.major_minor_version_string.
-                    partition(".")[0]
-                    ),
-                'venv'
-               )
+        try:
+            shprint(venv,
+                    '-m venv'.format(
+                        ctx.python_recipe.major_minor_version_string.
+                        partition(".")[0]
+                        ),
+                    'venv'
+                   )
+        except sh.ErrorReturnCode_1:
+            shprint(venv,
+                    '-m virtualenv'.format(
+                        ctx.python_recipe.major_minor_version_string.
+                        partition(".")[0]
+                        ),
+                    'venv'
+                   )
+            
         # Prepare base environment and upgrade pip:
         base_env = copy.copy(os.environ)
         base_env["PYTHONPATH"] = ctx.get_site_packages_dir()
