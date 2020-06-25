813c1444dc2022e79447188c44444a59fe092baa
Rene Leveille <rene.leveille@polymtl.ca>
2020-06-25 15:48:36 -0400

    Fix libpq env for cross compile

--- pythonforandroid/recipes/libpq/__init__.py
+++ pythonforandroid/recipes/libpq/__init__.py
@@ -8,6 +8,12 @@ class LibpqRecipe(Recipe):
     url = "http://ftp.postgresql.org/pub/source/v{version}/postgresql-{version}.tar.bz2"
     depends = []
 
+    def get_recipe_env(self, arch):
+        env = super().get_recipe_env(arch)
+        env["USE_DEV_URANDOM"] = "1"
+
+        return env
+
     def should_build(self, arch):
         return not os.path.isfile("{}/libpq.a".format(self.ctx.get_libs_dir(arch.arch)))
 
