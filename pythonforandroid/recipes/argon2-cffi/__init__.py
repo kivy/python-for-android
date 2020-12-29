d3a27101efe1a3114b4bc15a3272fa32771b9b85
Arjun Somvanshi <59286213+Arjun-Somvanshi@users.noreply.github.com>
2020-12-29 12:00:47 +0530

    Update __init__.py

--- pythonforandroid/recipes/argon2-cffi/__init__.py
+++ pythonforandroid/recipes/argon2-cffi/__init__.py
@@ -4,7 +4,7 @@ from pythonforandroid.recipe import PythonRecipe
 class PycryptodomeRecipe(PythonRecipe):
     version = '20.1.0'
     url = 'https://github.com/hynek/argon2-cffi/archive/master.zip'
-    depends = ['setuptools', 'cffi']
+    depends = ['cffi']
 
 
 recipe = PycryptodomeRecipe()
