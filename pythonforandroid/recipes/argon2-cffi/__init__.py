06720ee462605a0c62148336246de9be29357729
Arjun Somvanshi <arjunsomvanshi@gmail.com>
2021-01-03 19:51:38 +0530

    Using CompiledComponentsPythonRecipe

--- pythonforandroid/recipes/argon2-cffi/__init__.py
+++ pythonforandroid/recipes/argon2-cffi/__init__.py
@@ -1,10 +1,11 @@
-from pythonforandroid.recipe import PythonRecipe
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 
-class PycryptodomeRecipe(PythonRecipe):
+class Argon2Recipe(CompiledComponentsPythonRecipe):
     version = '20.1.0'
     url = 'https://github.com/hynek/argon2-cffi/archive/master.zip'
     depends = ['setuptools', 'cffi']
+    call_hostpython_via_targetpython = False
 
 
-recipe = PycryptodomeRecipe()
+recipe = Argon2Recipe()
