8fbb4cd1ceec53bc90410fd4c15e70ae8a5d4e2a
Arjun Somvanshi <59286213+Arjun-Somvanshi@users.noreply.github.com>
2021-01-03 20:33:53 +0530

    adding the extra lines

--- pythonforandroid/recipes/argon2-cffi/__init__.py
+++ pythonforandroid/recipes/argon2-cffi/__init__.py
@@ -1,5 +1,6 @@
 from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
+
 class Argon2Recipe(CompiledComponentsPythonRecipe):
     version = '20.1.0'
     url = 'https://github.com/hynek/argon2-cffi/archive/master.zip'
