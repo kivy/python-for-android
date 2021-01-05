5b37ba728f44431752a4a9f20cce63bede82129a
Arjun Somvanshi <59286213+Arjun-Somvanshi@users.noreply.github.com>
2021-01-05 11:32:17 +0530

    Recipe for argon2-cffi

--- pythonforandroid/recipes/argon2-cffi/__init__.py
+++ pythonforandroid/recipes/argon2-cffi/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 class Argon2Recipe(CompiledComponentsPythonRecipe):
     version = '20.1.0'
-    url = 'git+https://github.com/hynek/argon2-cffi'
+    url = 'https://github.com/hynek/argon2-cffi/archive/{version}.tar.gz'
     depends = ['setuptools', 'cffi']
     call_hostpython_via_targetpython = False
     build_cmd = 'build'
