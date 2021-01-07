1518d9c8f699c5493784f5be9ac1879988ac112c
Eero af Heurlin <eero.afheurlin@iki.fi>
2021-01-07 22:57:38 +0200

    PyZQM recipe needs setuptools, list it explicitly in deps (#2404)
    * try installing setuptools first
    * use specific pyzmq  version tag

--- pythonforandroid/recipes/pyzmq/__init__.py
+++ pythonforandroid/recipes/pyzmq/__init__.py
@@ -10,10 +10,10 @@ import glob
 
 class PyZMQRecipe(CythonRecipe):
     name = 'pyzmq'
-    version = 'master'
-    url = 'https://github.com/zeromq/pyzmq/archive/{version}.zip'
+    version = '20.0.0'
+    url = 'https://github.com/zeromq/pyzmq/archive/v{version}.zip'
     site_packages_name = 'zmq'
-    depends = ['libzmq']
+    depends = ['setuptools', 'libzmq']
     cython_args = ['-Izmq/utils',
                    '-Izmq/backend/cython',
                    '-Izmq/devices']
