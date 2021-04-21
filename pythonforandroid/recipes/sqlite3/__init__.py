bcfec0102bf4f113a45691dab191d464d4632962
Felix C. Stegerman <flx@obfusk.net>
2021-04-21 18:28:18 +0200

    update sqlite3

--- pythonforandroid/recipes/sqlite3/__init__.py
+++ pythonforandroid/recipes/sqlite3/__init__.py
@@ -5,9 +5,9 @@ import sh
 
 
 class Sqlite3Recipe(NDKRecipe):
-    version = '3.34.1'
+    version = '3.35.5'
     # Don't forget to change the URL when changing the version
-    url = 'https://www.sqlite.org/2021/sqlite-amalgamation-3340100.zip'
+    url = 'https://www.sqlite.org/2021/sqlite-amalgamation-3350500.zip'
     generated_libraries = ['sqlite3']
 
     def should_build(self, arch):
