cb244b58ebea618af4a7fc371585daaccb078a73
Robert Niederreiter <office@squarewave.at>
2021-06-10 13:09:14 +0200

    worker basics build successfully

--- pythonforandroid/bootstraps/service_library/build/templates/Worker.tmpl.java
+++ pythonforandroid/bootstraps/service_library/build/templates/Worker.tmpl.java
@@ -1,15 +1,22 @@
+import android.content.Context;
 import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.work.ListenableWorker.Result;
 import androidx.work.Worker;
+import androidx.work.WorkerParameters;
 
 
 public class {{ name|capitalize }}Worker extends Worker {
 
+    public {{ name|capitalize }}Worker (
+        @NonNull Context context,
+        @NonNull WorkerParameters params) {
+        super(context, params);
+    }
+
     @Override
     public Result doWork() {
-        log.d("Python Worker", "I am the god of hell fire. And it bring you FIRE");
-        log.d("Python Worker", "I am the god of hell fire. And it bring you FIRE");
-        log.d("Python Worker", "I am the god of hell fire. And it bring you FIRE");
+        Log.d("Python Worker", "I am the god of hell fire. And it bring you FIRE");
 
         // Indicate whether the work finished successfully with the Result
         return Result.success();
