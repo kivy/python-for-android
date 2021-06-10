6a81bfbcb1d25bcb1233088353ecf0981cba9c0e
Robert Niederreiter <office@squarewave.at>
2021-06-10 15:19:44 +0200

    worker extends ListenableWorker. Add basic threading for worker

--- pythonforandroid/bootstraps/service_library/build/templates/Worker.tmpl.java
+++ pythonforandroid/bootstraps/service_library/build/templates/Worker.tmpl.java
@@ -3,12 +3,18 @@ package {{ args.package }};
 import android.content.Context;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import androidx.concurrent.futures.CallbackToFutureAdapter.Completer;
+import androidx.concurrent.futures.CallbackToFutureAdapter;
 import androidx.work.ListenableWorker.Result;
+import androidx.work.ListenableWorker;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+import com.google.common.util.concurrent.ListenableFuture;
 
 
-public class {{ name|capitalize }}Worker extends Worker {
+public class {{ name|capitalize }}Worker extends ListenableWorker implements Runnable {
+    Thread pythonThread = null;
+    Completer workCompleter = null;
 
     public {{ name|capitalize }}Worker (
         @NonNull Context context,
@@ -16,11 +22,30 @@ public class {{ name|capitalize }}Worker extends Worker {
         super(context, params);
     }
 
+    @NonNull
     @Override
-    public Result doWork() {
+    public ListenableFuture<Result> startWork() {
+        return CallbackToFutureAdapter.getFuture(completer -> {
+            workCompleter = completer;
+
+            pythonThread = new Thread(this);
+            pythonThread.start();
+
+            String msg = "{{ name|capitalize }}Worker started";
+            Log.d("Python Worker", msg);
+            return msg;
+        });
+    }
+
+    @Override
+    public void run() {
         Log.d("Python Worker", "I am the god of hell fire. And it bring you FIRE");
 
-        // Indicate whether the work finished successfully with the Result
-        return Result.success();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {}
+
+        workCompleter.set(Result.success());
+        Log.d("Python Worker", "{{ name|capitalize }}Worker Thread terminating");
     }
 }
