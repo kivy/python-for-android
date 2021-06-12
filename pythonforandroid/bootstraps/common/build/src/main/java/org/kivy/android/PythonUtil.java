dc2806ee31b2316e51dfc059f5061a820543ace7
Robert Niederreiter <office@squarewave.at>
2021-06-12 19:49:51 +0200

    Move PythonActivityUtil.unpackData to PythonUtil.unpackData (#2462)
    * Move PythonActivityUtil.unpackData to PythonUtil.unpackData
    * Move toastError and some fixes
    * declare activity final in toastError
    * Remove PythonActivityUtil

--- pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
+++ pythonforandroid/bootstraps/common/build/src/main/java/org/kivy/android/PythonUtil.java
@@ -1,11 +1,20 @@
 package org.kivy.android;
 
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.File;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.util.Log;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+import org.renpy.android.AssetExtract;
 
 public class PythonUtil {
 	private static final String TAG = "pythonutil";
@@ -76,5 +85,111 @@ public class PythonUtil {
         }
 
         Log.v(TAG, "Loaded everything!");
+    }
+
+    public static String getAppRoot(Context ctx) {
+        String appRoot = ctx.getFilesDir().getAbsolutePath() + "/app";
+        return appRoot;
+    }
+
+    public static String getResourceString(Context ctx, String name) {
+        // Taken from org.renpy.android.ResourceManager
+        Resources res = ctx.getResources();
+        int id = res.getIdentifier(name, "string", ctx.getPackageName());
+        return res.getString(id);
+    }
+
+    /**
+     * Show an error using a toast. (Only makes sense from non-UI threads.)
+     */
+    protected static void toastError(final Activity activity, final String msg) {
+        activity.runOnUiThread(new Runnable () {
+            public void run() {
+                Toast.makeText(activity, msg, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Wait to show the error.
+        synchronized (activity) {
+            try {
+                activity.wait(1000);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    protected static void recursiveDelete(File f) {
+        if (f.isDirectory()) {
+            for (File r : f.listFiles()) {
+                recursiveDelete(r);
+            }
+        }
+        f.delete();
+    }
+
+    public static void unpackData(
+        Context ctx,
+        final String resource,
+        File target,
+        boolean cleanup_on_version_update) {
+
+        Log.v(TAG, "Unpacking " + resource + " " + target.getName());
+
+        // The version of data in memory and on disk.
+        String dataVersion = getResourceString(ctx, resource + "_version");
+        String diskVersion = null;
+
+        Log.v(TAG, "Data version is " + dataVersion);
+
+        // If no version, no unpacking is necessary.
+        if (dataVersion == null) {
+            return;
+        }
+
+        // Check the current disk version, if any.
+        String filesDir = target.getAbsolutePath();
+        String diskVersionFn = filesDir + "/" + resource + ".version";
+
+        try {
+            byte buf[] = new byte[64];
+            InputStream is = new FileInputStream(diskVersionFn);
+            int len = is.read(buf);
+            diskVersion = new String(buf, 0, len);
+            is.close();
+        } catch (Exception e) {
+            diskVersion = "";
+        }
+
+        // If the disk data is out of date, extract it and write the version file.
+        if (! dataVersion.equals(diskVersion)) {
+            Log.v(TAG, "Extracting " + resource + " assets.");
+
+            if (cleanup_on_version_update) {
+                recursiveDelete(target);
+            }
+            target.mkdirs();
+
+            AssetExtract ae = new AssetExtract(ctx);
+            if (!ae.extractTar(resource + ".mp3", target.getAbsolutePath())) {
+                String msg = "Could not extract " + resource + " data.";
+                if (ctx instanceof Activity) {
+                    toastError((Activity)ctx, msg);
+                } else {
+                    Log.v(TAG, msg);
+                }
+            }
+
+            try {
+                // Write .nomedia.
+                new File(target, ".nomedia").createNewFile();
+
+                // Write version file.
+                FileOutputStream os = new FileOutputStream(diskVersionFn);
+                os.write(dataVersion.getBytes());
+                os.close();
+            } catch (Exception e) {
+                Log.w("python", e);
+            }
+        }
     }
 }
