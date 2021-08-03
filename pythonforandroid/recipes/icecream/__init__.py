8fb67fcd3db3e0e51afd6991d49719534ea5dd50
Arjun-Somvanshi <arjunsomvanshi@gmail.com>
2021-08-03 20:45:27 +0530

    updated __init__.py

--- pythonforandroid/recipes/icecream/__init__.py
+++ pythonforandroid/recipes/icecream/__init__.py
@@ -5,6 +5,7 @@ class IcecreamRecipe(PythonRecipe):
     version = '2.1.1'
     url = 'https://github.com/gruns/icecream/archive/refs/tags/v{version}.tar.gz'
     depends = ['setuptools']
+    call_hostpython_via_targetpython = False
 
 
 recipe = IcecreamRecipe()
