93a417abcd0a50975be7872c25100e6f87ed52c3
RobertFlatt <34464649+RobertFlatt@users.noreply.github.com>
2021-11-27 15:06:09 -1000

    test

--- pythonforandroid/toolchain.py
+++ pythonforandroid/toolchain.py
@@ -996,7 +996,9 @@ class ToolchainCL:
             else:
                 asset_src = asset_dest = asset
             # take abspath now, because build.py will be run in bootstrap dir
-            print('=========fix',asset_src,os.path.abspath(asset_src))
+            info('===========================fix')
+            info(asset_src)
+            info(os.path.abspath(asset_src))
             unknown_args += ["--asset", os.path.abspath(asset_src)+":"+asset_dest]
         for i, arg in enumerate(unknown_args):
             argx = arg.split('=')
