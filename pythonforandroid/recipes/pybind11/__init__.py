5d36c40365feb44f86ea01dbcd91b8ef71cb580f
RobertF <34464649+RobertFlatt@users.noreply.github.com>
2022-03-02 14:13:44 -1000

    add pybind11

---
+++ pythonforandroid/recipes/pybind11/__init__.py
@@ -0,0 +1,13 @@
+from pythonforandroid.recipe import Recipe
+from os.path import join
+
+class Pybind11Recipe(Recipe):
+
+    version = 'v2.9.0'
+    url = 'https://github.com/pybind/pybind11/archive/refs/tags/{version}.zip'
+
+    def get_include_dir(self, arch):
+        return join(self.get_build_dir(arch.arch), 'include')
+    
+recipe = Pybind11Recipe()
+
