6fc71c30239da76d282653beade411026053518f
RobertF <34464649+RobertFlatt@users.noreply.github.com>
2022-03-03 07:13:58 -1000

    move 'v' in version

--- pythonforandroid/recipes/pybind11/__init__.py
+++ pythonforandroid/recipes/pybind11/__init__.py
@@ -4,8 +4,8 @@ from os.path import join
 
 class Pybind11Recipe(Recipe):
 
-    version = 'v2.9.0'
-    url = 'https://github.com/pybind/pybind11/archive/refs/tags/{version}.zip'
+    version = '2.9.0'
+    url = 'https://github.com/pybind/pybind11/archive/refs/tags/v{version}.zip'
 
     def get_include_dir(self, arch):
         return join(self.get_build_dir(arch.arch), 'include')
