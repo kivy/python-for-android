9caa21f2f3fdb4cdcc6c12086eaae6ba4ae98c9c
domedave <67050282+domedave@users.noreply.github.com>
2022-03-09 10:51:14 +0700

    Update __init__.py
    fixed on "E305 expected 2 blank lines after class or function definition, found 1"

--- pythonforandroid/recipes/freetype-py/__init__.py
+++ pythonforandroid/recipes/freetype-py/__init__.py
@@ -2,6 +2,8 @@ from pythonforandroid.recipe import PythonRecipe
 
 
 class FreetypePyRecipe(PythonRecipe):
+    
+    
     version = '2.2.0'
     url = 'https://github.com/rougier/freetype-py/archive/refs/tags/v{version}.tar.gz'
 
