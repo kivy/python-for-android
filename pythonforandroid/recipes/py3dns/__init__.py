625ecbd9573cf9f4395f12f34c08314bd3cc017c
Neizvestnyj <40869738+Neizvestnyj@users.noreply.github.com>
2022-05-01 11:57:11 +0300

    Update py3dns recipe

--- pythonforandroid/recipes/py3dns/__init__.py
+++ pythonforandroid/recipes/py3dns/__init__.py
@@ -4,7 +4,7 @@ from pythonforandroid.recipe import PythonRecipe
 class Py3DNSRecipe(PythonRecipe):
     site_packages_name = 'DNS'
     version = '3.2.1'
-    url = 'https://github.com/Neizvestnyj/py3dns/archive/{version}.zip'
+    url = 'https://launchpad.net/py3dns/trunk/{version}/+download/py3dns-{version}.tar.gz'
     depends = ['setuptools']
     patches = ['patches/android.patch']
     call_hostpython_via_targetpython = False
