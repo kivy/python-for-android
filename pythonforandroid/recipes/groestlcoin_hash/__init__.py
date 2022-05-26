61cc596be24ae35223d72def4e587ed05d60c888
gruve-p <gruve-p@users.noreply.github.com>
2022-05-26 19:34:18 +0200

    Bump groestlcoin_hash to 1.0.3 (#2607)

--- pythonforandroid/recipes/groestlcoin_hash/__init__.py
+++ pythonforandroid/recipes/groestlcoin_hash/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import CythonRecipe
 
 
 class GroestlcoinHashRecipe(CythonRecipe):
-    version = '1.0.1'
+    version = '1.0.3'
     url = 'https://github.com/Groestlcoin/groestlcoin-hash-python/archive/{version}.tar.gz'
     depends = ['setuptools']
     cythonize = False
