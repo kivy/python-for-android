3c55699f1e6a10f0bad7ca9e71f6ad3db5782608
Mikhail Zakharov <mzakharo@gmail.com>
2022-06-05 20:58:51 -0400

    use LEGACY_NDK option to build lapack/scipy with a separate NDK, supporting gcc/gfortran

--- pythonforandroid/recipes/lapack/__init__.py
+++ pythonforandroid/recipes/lapack/__init__.py
@@ -9,6 +9,10 @@ from pythonforandroid.util import current_directory, ensure_dir, BuildInterrupti
 from multiprocessing import cpu_count
 from os.path import join
 import sh
+from os import environ
+from pythonforandroid.util import build_platform
+
+arch_to_sysroot = {'armeabi': 'arm', 'armeabi-v7a': 'arm', 'arm64-v8a': 'arm64'}
 
 
 class LapackRecipe(Recipe):
@@ -18,12 +22,20 @@ class LapackRecipe(Recipe):
     url = 'https://github.com/Reference-LAPACK/lapack/archive/{version}.tar.gz'
     libdir = 'build/install/lib'
     built_libraries = {'libblas.so': libdir, 'liblapack.so': libdir, 'libcblas.so': libdir}
-    need_stl_shared = True
 
     def get_recipe_env(self, arch):
         env = super().get_recipe_env(arch)
-        sysroot = f"{self.ctx.ndk_dir}/platforms/{env['NDK_API']}/{arch.platform_dir}"
-        FC = f"{env['TOOLCHAIN_PREFIX']}-gfortran"  # FIXME
+
+        ndk_dir = environ.get("LEGACY_NDK")
+        if ndk_dir is None:
+            raise BuildInterruptingException("Please set the environment variable 'LEGACY_NDK' to point to a NDK location with gcc/gfortran support (last tested NDK version was 'r19c')")
+
+        GCC_VER = '4.9'
+        HOST = build_platform
+
+        sysroot_suffix = arch_to_sysroot.get(arch.arch, arch.arch)
+        sysroot = f"{ndk_dir}/platforms/{env['NDK_API']}/arch-{sysroot_suffix}"
+        FC = f"{ndk_dir}/toolchains/{arch.command_prefix}-{GCC_VER}/prebuilt/{HOST}/bin/{arch.command_prefix}-gfortran"
         env['FC'] = f'{FC} --sysroot={sysroot}'
         if sh.which(FC) is None:
             raise BuildInterruptingException(f"{FC} not found. See https://github.com/mzakharo/android-gfortran")
@@ -37,12 +49,13 @@ class LapackRecipe(Recipe):
         ensure_dir(build_target)
         with current_directory(build_target):
             env = self.get_recipe_env(arch)
+            ndk_dir = environ["LEGACY_NDK"]
             shprint(sh.rm, '-rf', 'CMakeFiles/', 'CMakeCache.txt', _env=env)
             shprint(sh.cmake, source_dir,
                     '-DCMAKE_SYSTEM_NAME=Android',
                     '-DCMAKE_POSITION_INDEPENDENT_CODE=1',
                     '-DCMAKE_ANDROID_ARCH_ABI={arch}'.format(arch=arch.arch),
-                    '-DCMAKE_ANDROID_NDK=' + self.ctx.ndk_dir,
+                    '-DCMAKE_ANDROID_NDK=' + ndk_dir,
                     '-DCMAKE_BUILD_TYPE=Release',
                     '-DCMAKE_INSTALL_PREFIX={}'.format(install_target),
                     '-DANDROID_ABI={arch}'.format(arch=arch.arch),
