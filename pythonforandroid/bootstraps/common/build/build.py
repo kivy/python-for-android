35de838fddc47974724672ae91c777b64707f734
Daniel García Moreno <dani@danigm.net>
2022-08-02 08:19:11 +0200

    Use existing ensure_dir function to create anydpi_dir

--- pythonforandroid/bootstraps/common/build/build.py
+++ pythonforandroid/bootstraps/common/build/build.py
@@ -349,10 +349,9 @@ main.py that loads it.''')
     if args.icon_fg and args.icon_bg:
         shutil.copy(args.icon_fg, join(res_dir, 'mipmap/icon_foreground.png'))
         shutil.copy(args.icon_bg, join(res_dir, 'mipmap/icon_background.png'))
-        mipmap_anydpi = join(res_dir, 'mipmap-anydpi-v26')
-        if not exists(mipmap_anydpi):
-            os.mkdir(mipmap_anydpi)
-        with open(join(mipmap_anydpi, 'icon.xml'), "w") as fd:
+        anydpi_dir = join(res_dir, 'mipmap-anydpi-v26')
+        ensure_dir(anydpi_dir)
+        with open(join(anydpi_dir, 'icon.xml'), "w") as fd:
             fd.write("""<?xml version="1.0" encoding="utf-8"?>
 <adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android">
     <background android:drawable="@mipmap/icon_background"/>
