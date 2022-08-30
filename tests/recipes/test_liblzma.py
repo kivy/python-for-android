c98daf48f0a5bfd7f0ea100c242e42c7cd9c1d12
Mirko Galimberti <me@mirkogalimberti.com>
2022-08-30 20:39:54 +0200

    Use p4a_install instead of install, as a file named INSTALL is already present.

--- tests/recipes/test_liblzma.py
+++ tests/recipes/test_liblzma.py
@@ -15,7 +15,7 @@ class TestLibLzmaRecipe(BaseTestForMakeRecipe, unittest.TestCase):
         recipe_build_dir = self.recipe.get_build_dir(self.arch.arch)
         self.assertEqual(
             self.recipe.get_library_includes(self.arch),
-            f" -I{join(recipe_build_dir, 'install/include')}",
+            f" -I{join(recipe_build_dir, 'p4a_install/include')}",
         )
 
     def test_get_library_ldflags(self):
@@ -25,7 +25,7 @@ class TestLibLzmaRecipe(BaseTestForMakeRecipe, unittest.TestCase):
         recipe_build_dir = self.recipe.get_build_dir(self.arch.arch)
         self.assertEqual(
             self.recipe.get_library_ldflags(self.arch),
-            f" -L{join(recipe_build_dir, 'install/lib')}",
+            f" -L{join(recipe_build_dir, 'p4a_install/lib')}",
         )
 
     def test_link_libs_flags(self):
@@ -33,3 +33,19 @@ class TestLibLzmaRecipe(BaseTestForMakeRecipe, unittest.TestCase):
         Test :meth:`~pythonforandroid.recipes.liblzma.get_library_libs_flag`.
         """
         self.assertEqual(self.recipe.get_library_libs_flag(), " -llzma")
+
+    def test_install_dir_not_named_install(self):
+        """
+        Tests that the install directory is not named ``install``.
+
+        liblzma already have a file named ``INSTALL`` in its source directory.
+        On case-insensitive filesystems, using a folder named ``install`` will
+        cause a conflict. (See issue: #2343).
+
+        WARNING: This test is quite flaky, but should be enough to
+        ensure that someone in the future will not accidentally rename
+        the install directory without seeing this test to fail.
+        """
+        liblzma_install_dir = self.recipe.built_libraries["liblzma.so"]
+
+        self.assertNotIn("install", liblzma_install_dir.split("/"))
