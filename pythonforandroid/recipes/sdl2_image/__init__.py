16a81b7df331834091e2cb4b1527a8e7907f11a0
Mirko Galimberti <me@mirkogalimberti.com>
2022-09-07 16:06:00 +0200

    Update SDL2, SDL2_ttf, SDL2_mixer, SDL2_image to latest releases

--- pythonforandroid/recipes/sdl2_image/__init__.py
+++ pythonforandroid/recipes/sdl2_image/__init__.py
@@ -1,14 +1,25 @@
+import os
+import sh
+from pythonforandroid.logger import shprint
 from pythonforandroid.recipe import BootstrapNDKRecipe
+from pythonforandroid.util import current_directory
 
 
 class LibSDL2Image(BootstrapNDKRecipe):
-    version = '2.0.4'
-    url = 'https://www.libsdl.org/projects/SDL_image/release/SDL2_image-{version}.tar.gz'
+    version = '2.6.2'
+    url = 'https://github.com/libsdl-org/SDL_image/releases/download/release-{version}/SDL2_image-{version}.tar.gz'
     dir_name = 'SDL2_image'
 
-    patches = ['toggle_jpg_png_webp.patch',
-               'extra_cflags.patch',
-               ]
+    patches = ['enable-webp.patch']
+
+    def prebuild_arch(self, arch):
+        # We do not have a folder for each arch on BootstrapNDKRecipe, so we
+        # need to skip the external deps download if we already have done it.
+        external_deps_dir = os.path.join(self.get_build_dir(arch.arch), "external")
+        if not os.path.exists(os.path.join(external_deps_dir, "libwebp")):
+            with current_directory(external_deps_dir):
+                shprint(sh.Command("./download.sh"))
+        super().prebuild_arch(arch)
 
 
 recipe = LibSDL2Image()
--- pythonforandroid/recipes/sdl2_ttf/__init__.py
+++ pythonforandroid/recipes/sdl2_ttf/__init__.py
@@ -2,8 +2,8 @@ from pythonforandroid.recipe import BootstrapNDKRecipe
 
 
 class LibSDL2TTF(BootstrapNDKRecipe):
-    version = '2.0.15'
-    url = 'https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-{version}.tar.gz'
+    version = '2.20.1'
+    url = 'https://github.com/libsdl-org/SDL_ttf/releases/download/release-{version}/SDL2_ttf-{version}.tar.gz'
     dir_name = 'SDL2_ttf'
 
 
