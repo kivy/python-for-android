b07895500a83d0bb90be11c55516efb33aca8a59
Xavier Fiechter <31884704+xavierfiechter@users.noreply.github.com>
2022-10-19 10:04:50 +0200

    Make CI compile aiohttp again.
    References: 
    https://stackoverflow.com/a/64755338/20124004,
    https://stackoverflow.com/a/64755052/20124004, 
    https://github.com/kivy/python-for-android/pull/2518

--- pythonforandroid/recipes/aiohttp/__init__.py
+++ pythonforandroid/recipes/aiohttp/__init__.py
@@ -1,16 +1,17 @@
 """Build AIOHTTP"""
 from typing import List
-from pythonforandroid.recipe import CythonRecipe  # type: ignore
+from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
 
-
-class AIOHTTPRecipe(CythonRecipe):  # type: ignore # pylint: disable=R0903
-    """Build AIOHTTP"""
-
-    version = "v3.6.2"
-    url = "https://github.com/aio-libs/aiohttp/archive/{version}.zip"
+class AIOHTTPRecipe(CppCompiledComponentsPythonRecipe):  # type: ignore # pylint: disable=R0903
+    url = "https://files.pythonhosted.org/packages/ff/4f/62d9859b7d4e6dc32feda67815c5f5ab4421e6909e48cbc970b6a40d60b7/aiohttp-3.8.3.tar.gz"
     name = "aiohttp"
-
     depends: List[str] = ["setuptools"]
+    call_hostpython_via_targetpython = False
+    install_in_hostpython = True
 
+    def get_recipe_env(self, arch):
+        env = super().get_recipe_env(arch)
+        env['LDFLAGS'] += ' -lc++_shared'
+        return env
 
 recipe = AIOHTTPRecipe()
