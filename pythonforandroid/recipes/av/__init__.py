c9d469620a3cd7565eb332c600de10e9dc05e2fe
Dexer <73297572+DexerBR@users.noreply.github.com>
2023-02-13 17:45:43 -0300

    flake8 fix

--- pythonforandroid/recipes/av/__init__.py
+++ pythonforandroid/recipes/av/__init__.py
@@ -4,19 +4,22 @@ from pythonforandroid.recipe import CythonRecipe
 
 class PyAVRecipe(CythonRecipe):
 
-    name = 'av'
-    version = 'v10.0.0'
-    url = 'https://github.com/PyAV-Org/PyAV/archive/v10.0.0.zip'
+    name = "av"
+    version = "v10.0.0"
+    url = "https://github.com/PyAV-Org/PyAV/archive/v10.0.0.zip"
 
-    depends = ['python3', 'cython', 'ffmpeg', 'av_codecs']
-    opt_depends = ['openssl']
+    depends = ["python3", "cython", "ffmpeg", "av_codecs"]
+    opt_depends = ["openssl"]
 
     def get_recipe_env(self, arch, with_flags_in_cc=True):
         env = super().get_recipe_env(arch)
 
-        build_dir = Recipe.get_recipe('ffmpeg', self.ctx).get_build_dir(arch.arch)
+        build_dir = Recipe.get_recipe("ffmpeg", self.ctx).get_build_dir(
+            arch.arch
+        )
         self.setup_extra_args = ["--ffmpeg-dir={}".format(build_dir)]
 
         return env
+
 
 recipe = PyAVRecipe()
--- pythonforandroid/recipes/av_codecs/__init__.py
+++ pythonforandroid/recipes/av_codecs/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.toolchain import Recipe
 
 
 class PyAVCodecsRecipe(Recipe):
-    depends = ['libx264', 'libshine', 'libvpx']
+    depends = ["libx264", "libshine", "libvpx"]
 
     def build_arch(self, arch):
         pass
