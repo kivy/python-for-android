70debe5aaa1e090c18f4c366c32a63069d9ee724
Steve Kowalik <steven@wedontsleep.org>
2023-05-31 05:57:03 +1000

    Use build rather than pep517 for building (#2784)
    pep517 has been renamed to pyproject-hooks, and as a consequence all of
    the deprecated functionality has been removed. build now provides the
    functionality required, and since we are only interested in the
    metadata, we can leverage a helper function for that. I've also removed
    all of the subprocess machinery for calling the wrapping function, since
    it appears to not be as noisy as pep517.

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ data_files = []
 install_reqs = [
     'appdirs', 'colorama>=0.3.3', 'jinja2',
     'sh>=1.10, <2.0; sys_platform!="nt"',
-    'pep517', 'toml', 'packaging',
+    'build', 'toml', 'packaging',
 ]
-# (pep517 and toml are used by pythonpackage.py)
+# (build and toml are used by pythonpackage.py)
 
 
 # By specifying every file manually, package_data will be able to
