afa5f3cdfe3b1c34cbfbe4e89401adc3e7e3ca14
T-Dynamos <anshdadwal298@gmail.com>
2023-06-25 08:25:57 +0530

    zbar: fix build

--- pythonforandroid/recipes/zbar/__init__.py
+++ pythonforandroid/recipes/zbar/__init__.py
@@ -4,20 +4,13 @@ from pythonforandroid.recipe import PythonRecipe
 
 class ZBarRecipe(PythonRecipe):
 
-    version = '0.10'
+    version = '0.23.92'
 
-    # For some reason the version 0.10 on PyPI is not the same as the ones
-    # in sourceforge and GitHub. The one in PyPI has a setup.py.
-    # url = 'https://github.com/ZBar/ZBar/archive/{version}.zip'
-    url = 'https://pypi.python.org/packages/e0/5c/' + \
-        'bd2a96a9f2adacffceb4482cdd56831735ab5a67ea6a60c0a8757c17b62e' + \
-        '/zbar-{version}.tar.gz'
+    url = 'https://github.com/mchehab/zbar/archive/{version}.tar.gz'
 
     call_hostpython_via_targetpython = False
 
     depends = ['setuptools', 'libzbar']
-
-    patches = ["zbar-0.10-python-crash.patch"]
 
     def get_recipe_env(self, arch=None, with_flags_in_cc=True):
         env = super().get_recipe_env(arch, with_flags_in_cc)
