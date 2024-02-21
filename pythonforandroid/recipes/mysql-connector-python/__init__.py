0e506c0cb24fc24ae5900b8ad5dcb556fcc550e7
Kypros Andreou <100076168+kyprosantreou@users.noreply.github.com>
2024-02-21 13:25:01 +0200

    mysql-connector-python fix

--- pythonforandroid/recipes/mysql-connector-python/__init__.py
+++ pythonforandroid/recipes/mysql-connector-python/__init__.py
@@ -3,11 +3,13 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 class MySQLConnectorPythonRecipe(CompiledComponentsPythonRecipe):
 
+
     name = 'mysql-connector-python'
     version = '8.3.0'
     url = f'https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-j-{version}.tar.gz'
     call_hostpython_via_targetpython = False
 
     depends = ['python3', 'setuptools']
+
 
 recipe = MySQLConnectorPythonRecipe()
