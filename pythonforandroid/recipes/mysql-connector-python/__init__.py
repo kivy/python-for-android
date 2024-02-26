ae6e07770d2d674c575d76d7cb9af097923530ce
Kypros Andreou <100076168+kyprosantreou@users.noreply.github.com>
2024-02-26 19:02:20 +0200

    Update pythonforandroid/recipes/mysql-connector-python/__init__.py
    Co-authored-by: Andre Miras <AndreMiras@users.noreply.github.com>

--- pythonforandroid/recipes/mysql-connector-python/__init__.py
+++ pythonforandroid/recipes/mysql-connector-python/__init__.py
@@ -7,7 +7,8 @@ class MySQLConnectorPythonRecipe(CompiledComponentsPythonRecipe):
     version = '8.3.0'
     url = (
         f'https://dev.mysql.com/get/Downloads/'
-        f'Connector-J/mysql-connector-j-{version}.tar.gz'
+        f'Connector-J/mysql-connector-python-{version}.tar.gz'
+
     )
     call_hostpython_via_targetpython = False
 
