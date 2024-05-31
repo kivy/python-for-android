b9341ef9f171bd11bc2ca62c849290f960293250
Kenechukwu Akubue <kengoon19@gmail.com>
2024-05-31 23:21:14 +0100

    Migrate to PyProjectRecipe

--- pythonforandroid/recipes/grpcio/__init__.py
+++ pythonforandroid/recipes/grpcio/__init__.py
@@ -1,13 +1,7 @@
-import glob
-from logging import info
-
-import sh
-from pythonforandroid.logger import shprint
-from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
-from pythonforandroid.util import current_directory
+from pythonforandroid.recipe import PyProjectRecipe, Recipe
 
 
-class GrpcioRecipe(CppCompiledComponentsPythonRecipe):
+class GrpcioRecipe(PyProjectRecipe):
     version = '1.64.0'
     url = 'https://files.pythonhosted.org/packages/source/g/grpcio/grpcio-{version}.tar.gz'
     depends = ["setuptools", "librt", "libpthread"]
@@ -15,32 +9,20 @@ class GrpcioRecipe(CppCompiledComponentsPythonRecipe):
 
     def get_recipe_env(self, arch, **kwargs):
         env = super().get_recipe_env(arch, **kwargs)
-        env['NDKPLATFORM'] = "NOTNONE"
-        env['GRPC_PYTHON_BUILD_WITH_CYTHON'] = '1'
+        env["NDKPLATFORM"] = "NOTNONE"
+        env["GRPC_PYTHON_BUILD_SYSTEM_OPENSSL"] = "1"
+        env["GRPC_PYTHON_BUILD_SYSTEM_ZLIB"] = "1"
+        env["ZLIB_INCLUDE"] = self.ctx.ndk.sysroot_include_dir
+        # replace -I with a space
+        openssl_recipe = Recipe.get_recipe('openssl', self.ctx)
+        env["SSL_INCLUDE"] = openssl_recipe.include_flags(arch).strip().replace("-I", "")
         env["CFLAGS"] += " -U__ANDROID_API__"
         env["CFLAGS"] += " -D__ANDROID_API__={}".format(self.ctx.ndk_api)
-
         # turn off c++11 warning error of "invalid suffix on literal"
         env["CFLAGS"] += " -Wno-reserved-user-defined-literal"
-        env['PLATFORM'] = 'android'
+        env["PLATFORM"] = "android"
         env["LDFLAGS"] += " -llog -landroid"
         return env
-
-    def build_compiled_components(self, arch):
-        info('Building compiled components in {}'.format(self.name))
-
-        env = self.get_recipe_env(arch)
-        hostpython = sh.Command(self.hostpython_location)
-        with current_directory(self.get_build_dir(arch.arch)):
-            if self.install_in_hostpython:
-                shprint(hostpython, 'setup.py', 'clean', '--all', _env=env)
-            shprint(hostpython, 'setup.py', self.build_cmd, '-v',
-                    _env=env, *self.setup_extra_args)
-
-            # grpcio creates a build directory and names it `pyb`
-            build_dir = glob.glob('pyb/lib.*')[0]
-            shprint(sh.find, build_dir, '-name', '"*.o"', '-exec',
-                    env['STRIP'], '{}', ';', _env=env)
 
 
 recipe = GrpcioRecipe()
