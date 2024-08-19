18b1bd675d6eb83a5e0133fea7fa5d1e9ce03216
RobertF <34464649+RobertFlatt@users.noreply.github.com>
2024-08-18 15:18:53 -1000

    Update __init__.py

--- pythonforandroid/recipes/tiktoken/__init__.py
+++ pythonforandroid/recipes/tiktoken/__init__.py
@@ -5,8 +5,7 @@ class TiktokenRecipe(RustCompiledComponentsRecipe):
     name = 'tiktoken'
     version = '0.7.0'
     url = 'https://github.com/openai/tiktoken/archive/refs/tags/{version}.tar.gz'
-    depends = ['regex','requests']
+    depends = ['regex', 'requests']
 
 
 recipe = TiktokenRecipe()
-
