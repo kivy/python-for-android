8bfc4b300c359ec5cf6110967ef19272a0a5b5e3
Jithesh Kuyyalil <jithesh82@gmail.com>
2024-08-22 21:02:13 -0400

    Update __init__.py
    commented out patches, commented out os import, updated version to 0.4.9

--- pythonforandroid/recipes/aubio/__init__.py
+++ pythonforandroid/recipes/aubio/__init__.py
@@ -1,12 +1,12 @@
 from pythonforandroid.recipe import PyProjectRecipe
-from os.path import join
+#from os.path import join
 
 
 class AubioRecipe(PyProjectRecipe):
-    version = "0.4.7"
+    version = "0.4.9"
     url = "https://aubio.org/pub/aubio-{version}.tar.bz2"
     depends = ["numpy", "setuptools"]  # Make sure 'samplerate' is included as a dependency
-    patches = [join("patches", "build_ext.patch")]
+    #patches = [join("patches", "build_ext.patch")]
 
 
 recipe = AubioRecipe()
