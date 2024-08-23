b2bcc6670433385f20e5e548b36a34d4599b6683
Jithesh Kuyyalil <jithesh82@gmail.com>
2024-08-22 21:10:37 -0400

    Update __init__.py
    flaked

--- pythonforandroid/recipes/aubio/__init__.py
+++ pythonforandroid/recipes/aubio/__init__.py
@@ -4,7 +4,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 class AubioRecipe(PyProjectRecipe):
     version = "0.4.9"
     url = "https://aubio.org/pub/aubio-{version}.tar.bz2"
-    depends = ["numpy", "setuptools"]  
-    
+    depends = ["numpy", "setuptools"]
+
 
 recipe = AubioRecipe()
