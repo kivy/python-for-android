a5d5ef7a99ac85559d3f381a16d1d97d23202c57
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2024-11-19 14:41:28 +0530

    Update __init__.py

--- pythonforandroid/recipes/bitarray/__init__.py
+++ pythonforandroid/recipes/bitarray/__init__.py
@@ -4,7 +4,7 @@ from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
 class BitarrayRecipe(CppCompiledComponentsPythonRecipe):
     stl_lib_name = "c++_shared"
     version = "3.0.0"
-    url = f"https://github.com/ilanschnell/bitarray/archive/refs/tags/{version}.tar.gz"
+    url = "https://github.com/ilanschnell/bitarray/archive/refs/tags/{version}.tar.gz"
     depends = ["setuptools"]
 
 
