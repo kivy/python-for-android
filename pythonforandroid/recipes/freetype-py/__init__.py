6d1b9d38260a56e58558b81038fea820e2997a96
Zachary Spector <public@zacharyspector.com>
2025-10-22 14:25:19 +1300

    Remove freetype's distutils "fallback"
    It was actually using distutils every time, apparently?

--- pythonforandroid/recipes/freetype-py/__init__.py
+++ pythonforandroid/recipes/freetype-py/__init__.py
@@ -5,7 +5,6 @@ class FreetypePyRecipe(PythonRecipe):
     version = '2.2.0'
     url = 'https://github.com/rougier/freetype-py/archive/refs/tags/v{version}.tar.gz'
     depends = ['freetype']
-    patches = ['fall-back-to-distutils.patch']
     site_packages_name = 'freetype'
 
 
