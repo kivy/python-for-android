8102b3348f1779df7eaddef2d729a02884159f16
Zachary Spector <public@zacharyspector.com>
2025-10-23 12:05:05 +1300

    Turn `FreetypePyRecipe` into a `PyProjectRecipe`
    I don't think this will require the version hack

--- pythonforandroid/recipes/freetype-py/__init__.py
+++ pythonforandroid/recipes/freetype-py/__init__.py
@@ -1,16 +1,11 @@
-from pythonforandroid.recipe import PythonRecipe
+from pythonforandroid.recipe import PyProjectRecipe
 
 
-class FreetypePyRecipe(PythonRecipe):
+class FreetypePyRecipe(PyProjectRecipe):
     version = '2.5.1'
     url = 'https://github.com/rougier/freetype-py/archive/refs/tags/v{version}.tar.gz'
     depends = ['freetype']
     site_packages_name = 'freetype'
-
-    def get_recipe_env(self, arch=None, with_flags_in_cc=True):
-        env = super().get_recipe_env(arch, with_flags_in_cc)
-        env["SETUPTOOLS_SCM_PRETEND_VERSION_FOR_freetype"] = self.version
-        return env
 
 
 recipe = FreetypePyRecipe()
