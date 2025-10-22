9df2658c445897687269fc528950c42ac0cd29fb
Zachary Spector <public@zacharyspector.com>
2025-10-22 15:35:31 +1300

    Use the latest *released* freetype-py version

--- pythonforandroid/recipes/freetype-py/__init__.py
+++ pythonforandroid/recipes/freetype-py/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 
 class FreetypePyRecipe(PythonRecipe):
-    version = '2.14.1'
+    version = '2.5.1'
     url = 'https://github.com/rougier/freetype-py/archive/refs/tags/v{version}.tar.gz'
     depends = ['freetype']
     site_packages_name = 'freetype'
