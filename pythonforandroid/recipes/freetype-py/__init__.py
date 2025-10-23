1972bc177846a6ce26799a0698d0a995fe40f2e9
Zachary Spector <public@zacharyspector.com>
2025-10-23 14:23:37 +1300

    Switch to pythonhosted FreetypePy
    `setuptools-scm` can't deal with the github hosted one, it seems.

--- pythonforandroid/recipes/freetype-py/__init__.py
+++ pythonforandroid/recipes/freetype-py/__init__.py
@@ -3,14 +3,11 @@ from pythonforandroid.recipe import PyProjectRecipe
 
 class FreetypePyRecipe(PyProjectRecipe):
     version = '2.5.1'
-    url = 'https://github.com/rougier/freetype-py/archive/refs/tags/v{version}.tar.gz'
+    url = ('https://files.pythonhosted.org/packages/d0/9c/'
+           '61ba17f846b922c2d6d101cc886b0e8fb597c109cedfcb39b8c5d2304b54/'
+           'freetype-py-{version}.zip')
     depends = ['freetype']
     site_packages_name = 'freetype'
-
-    def get_recipe_env(self, arch=None, **kwargs):
-        env = super().get_recipe_env(arch, **kwargs)
-        env["SETUPTOOLS_SCM_PRETEND_VERSION_FOR_freetype"] = self.version
-        return env
 
 
 recipe = FreetypePyRecipe()
