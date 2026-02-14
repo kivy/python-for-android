28d482155de72d98827406b11bf27a18bd75fd25
اشکان ولی‌زاده <valizadeh.ashkan@gmail.com>
2026-02-14 18:41:09 +0330

    change the jave service code so it passes spotless checks

--- pythonforandroid/bootstraps/qt/build/src/main/java/org/kivy/android/PythonService.java
+++ pythonforandroid/bootstraps/qt/build/src/main/java/org/kivy/android/PythonService.java
@@ -1,24 +1,27 @@
 package org.kivy.android;
 
-import android.os.Build;
-import java.lang.reflect.Method;
-import java.lang.reflect.InvocationTargetException;
-import android.app.Service;
-import android.os.IBinder;
-import android.os.Bundle;
-import android.content.Intent;
-import android.content.Context;
-import android.util.Log;
 import android.app.Notification;
-import android.app.PendingIntent;
-import android.os.Process;
-import java.io.File;
-import android.app.NotificationManager;
 import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Process;
+import android.util.Log;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.qtproject.qt.android.bindings.QtService;
 
 public class PythonService extends QtService implements Runnable {
+
     private static final String TAG = "PythonQtService";
 
     // Thread for Python code
@@ -31,7 +34,7 @@ public class PythonService extends QtService implements Runnable {
     private String pythonHome;
     private String pythonPath;
     private String serviceEntrypoint;
-    // Argument to pass to Python code,
+    // Argument to pass to Python code
     private String pythonServiceArgument;
 
     public static PythonService mService = null;
@@ -42,7 +45,7 @@ public class PythonService extends QtService implements Runnable {
     public void setEnvironmentVariable(String key, String value) {
         /**
          * Sets an environment variable based on key/value.
-         **/
+         */
         try {
             android.system.Os.setenv(key, value, true);
         } catch (Exception e) {
@@ -74,8 +77,8 @@ public class PythonService extends QtService implements Runnable {
             Log.v(TAG, "service exists, do not start again");
             return startType();
         }
-        
-	    //intent is null if OS restarts a STICKY service
+
+        // intent is null if OS restarts a STICKY service
         if (intent == null) {
             Context context = getApplicationContext();
             intent = getThisDefaultIntent(context, "");
@@ -89,10 +92,10 @@ public class PythonService extends QtService implements Runnable {
         pythonName = extras.getString("pythonName");
         pythonHome = extras.getString("pythonHome");
         pythonPath = extras.getString("pythonPath");
-        boolean serviceStartAsForeground = (
-            extras.getString("serviceStartAsForeground").equals("true")
-        );
+        boolean serviceStartAsForeground =
+                extras.getString("serviceStartAsForeground").equals("true");
         pythonServiceArgument = extras.getString("pythonServiceArgument");
+
         pythonThread = new Thread(this);
         pythonThread.start();
 
@@ -116,52 +119,69 @@ public class PythonService extends QtService implements Runnable {
         String smallIconName = extras.getString("smallIconName");
         String contentTitle = extras.getString("contentTitle");
         String contentText = extras.getString("contentText");
+
         Notification notification;
         Context context = getApplicationContext();
         Intent contextIntent = new Intent(context, PythonActivity.class);
-        PendingIntent pIntent = PendingIntent.getActivity(context, 0, contextIntent,
-            PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pIntent =
+                PendingIntent.getActivity(
+                        context,
+                        0,
+                        contextIntent,
+                        PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
 
         // Unspecified icon uses default.
         int smallIconId = context.getApplicationInfo().icon;
-        
+
         if (smallIconName != null) {
-            if (!smallIconName.equals("")){
-                int resId = getResources().getIdentifier(smallIconName, "mipmap",
-                                    getPackageName());
-                if (resId ==0) {
-                resId = getResources().getIdentifier(smallIconName, "drawable",
-                                    getPackageName());
+            if (!smallIconName.isEmpty()) {
+                int resId =
+                        getResources()
+                                .getIdentifier(smallIconName, "mipmap", getPackageName());
+                if (resId == 0) {
+                    resId =
+                            getResources()
+                                    .getIdentifier(smallIconName, "drawable", getPackageName());
                 }
-                if (resId !=0) {
-                smallIconId = resId;
+                if (resId != 0) {
+                    smallIconId = resId;
                 }
             }
         }
 
-	    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
             // This constructor is deprecated
-            notification = new Notification(
-                smallIconId, serviceTitle, System.currentTimeMillis());
+            notification = new Notification(smallIconId, serviceTitle, System.currentTimeMillis());
             try {
                 // prevent using NotificationCompat, this saves 100kb on apk
-                Method func = notification.getClass().getMethod(
-                    "setLatestEventInfo", Context.class, CharSequence.class,
-                    CharSequence.class, PendingIntent.class);
+                Method func =
+                        notification
+                                .getClass()
+                                .getMethod(
+                                        "setLatestEventInfo",
+                                        Context.class,
+                                        CharSequence.class,
+                                        CharSequence.class,
+                                        PendingIntent.class);
                 func.invoke(notification, context, contentTitle, contentText, pIntent);
-            } catch (NoSuchMethodException | IllegalAccessException |
-                     IllegalArgumentException | InvocationTargetException e) {
+            } catch (NoSuchMethodException
+                    | IllegalAccessException
+                    | IllegalArgumentException
+                    | InvocationTargetException e) {
+                // ignored
             }
         } else {
             // for android 8+ we need to create our own channel
-            // https://stackoverflow.com/questions/47531742/startforeground-fail-after-upgrade-to-android-8-1
             String NOTIFICATION_CHANNEL_ID = "org.kivy.p4a" + getServiceId();
             String channelName = "Background Service" + getServiceId();
-            NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+            NotificationChannel chan =
+                    new NotificationChannel(
+                            NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
 
             chan.setLightColor(Color.BLUE);
             chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationManager manager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             manager.createNotificationChannel(chan);
 
             Notification.Builder builder = new Notification.Builder(context, NOTIFICATION_CHANNEL_ID);
@@ -187,24 +207,25 @@ public class PythonService extends QtService implements Runnable {
     }
 
     /**
-     * Stops the task gracefully when killed.
-     * Calling stopSelf() will trigger a onDestroy() call from the system.
+     * Stops the task gracefully when killed. Calling stopSelf() will trigger a onDestroy() call
+     * from the system.
      */
     @Override
     public void onTaskRemoved(Intent rootIntent) {
         super.onTaskRemoved(rootIntent);
-        //sticky service runtime/restart is managed by the OS. leave it running when app is closed
+        // sticky service runtime/restart is managed by the OS. leave it running when app is closed
         if (startType() != START_STICKY) {
             stopSelf();
         }
     }
 
     @Override
-    public void run(){
-        String app_root =  getFilesDir().getAbsolutePath() + "/app";
+    public void run() {
+        String app_root = getFilesDir().getAbsolutePath() + "/app";
         File app_root_file = new File(app_root);
-        PythonUtil.loadLibraries(app_root_file,
-            new File(getApplicationInfo().nativeLibraryDir));
+
+        PythonUtil.loadLibraries(
+                app_root_file, new File(getApplicationInfo().nativeLibraryDir));
         this.mService = this;
 
         Log.v(TAG, "Setting env vars for start.c and Python to use");
@@ -224,8 +245,11 @@ public class PythonService extends QtService implements Runnable {
 
     // Native part
     public static native void nativeStart(
-            String androidPrivate, String androidArgument,
-            String serviceEntrypoint, String pythonName,
-            String pythonHome, String pythonPath,
+            String androidPrivate,
+            String androidArgument,
+            String serviceEntrypoint,
+            String pythonName,
+            String pythonHome,
+            String pythonPath,
             String pythonServiceArgument);
 }
