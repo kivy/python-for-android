bf4737a9b36f6eeeef5437dc31a130e730483428
Leonid <87101242+GvozdevLeonid@users.noreply.github.com>
2026-02-21 21:00:49 +0100

    Fixed Python version support.

--- pythonforandroid/recipes/python3/__init__.py
+++ pythonforandroid/recipes/python3/__init__.py
@@ -55,7 +55,6 @@ class Python3Recipe(TargetPythonRecipe):
     '''
 
     version = '3.14.2'
-    _p_version = Version(version)
     url = 'https://github.com/python/cpython/archive/refs/tags/v{version}.tar.gz'
     name = 'python3'
 
@@ -63,28 +62,6 @@ class Python3Recipe(TargetPythonRecipe):
         'patches/pyconfig_detection.patch',
         'patches/reproducible-buildinfo.diff',
     ]
-
-    if _p_version.major == 3 and _p_version.minor == 7:
-        patches += [
-            'patches/py3.7.1_fix-ctypes-util-find-library.patch',
-            'patches/py3.7.1_fix-zlib-version.patch',
-        ]
-
-    if 8 <= _p_version.minor <= 10:
-        patches.append('patches/py3.8.1.patch')
-
-    if _p_version.minor >= 11:
-        patches.append('patches/cpython-311-ctypes-find-library.patch')
-
-    if _p_version.minor >= 14:
-        patches.append('patches/3.14_armv7l_fix.patch')
-        patches.append('patches/3.14_fix_remote_debug.patch')
-
-    if shutil.which('lld') is not None:
-        if _p_version.minor == 7:
-            patches.append("patches/py3.7.1_fix_cortex_a8.patch")
-        elif _p_version.minor >= 8:
-            patches.append("patches/py3.8.1_fix_cortex_a8.patch")
 
     depends = ['hostpython3', 'sqlite3', 'openssl', 'libffi']
     # those optional depends allow us to build python compression modules:
@@ -210,6 +187,32 @@ class Python3Recipe(TargetPythonRecipe):
             flags=flags
         )
 
+    def apply_patches(self, arch, build_dir=None):
+
+        _p_version = Version(self.version)
+        if _p_version.major == 3 and _p_version.minor == 7:
+            self.patches += [
+                'patches/py3.7.1_fix-ctypes-util-find-library.patch',
+                'patches/py3.7.1_fix-zlib-version.patch',
+            ]
+    
+        if 8 <= _p_version.minor <= 10:
+            self.patches.append('patches/py3.8.1.patch')
+    
+        if _p_version.minor >= 11:
+            self.patches.append('patches/cpython-311-ctypes-find-library.patch')
+    
+        if _p_version.minor >= 14:
+            self.patches.append('patches/3.14_armv7l_fix.patch')
+            self.patches.append('patches/3.14_fix_remote_debug.patch')
+    
+        if shutil.which('lld') is not None:
+            if _p_version.minor == 7:
+                self.patches.append("patches/py3.7.1_fix_cortex_a8.patch")
+            elif _p_version.minor >= 8:
+                self.patches.append("patches/py3.8.1_fix_cortex_a8.patch")
+        super().__init__(arch, build_dir)
+
     def include_root(self, arch_name):
         return join(self.get_build_dir(arch_name), 'Include')
 
@@ -317,7 +320,13 @@ class Python3Recipe(TargetPythonRecipe):
         env['ZLIB_VERSION'] = line.replace('#define ZLIB_VERSION ', '')
         add_flags(' -I' + zlib_includes, ' -L' + zlib_lib_path, ' -lz')
 
-        if self._p_version.minor >= 13 and self.disable_gil:
+        _p_version = Version(self.version)
+        if _p_version.minor >= 11:
+            configure_args.extend([
+                '--with-build-python={python_host_bin}',
+            ])
+
+        if _p_version.minor >= 13 and self.disable_gil:
             self.configure_args.append("--disable-gil")
 
         return env
