ba1aac911de42459e29bdc9ffbd933a48b272345
RobertF <34464649+RobertFlatt@users.noreply.github.com>
2026-06-08 06:31:56 +0000

    Update __init__.py
    Update version to 3.5.1

--- pythonforandroid/recipes/greenlet/__init__.py
+++ pythonforandroid/recipes/greenlet/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PyProjectRecipe
 
 
 class GreenletRecipe(PyProjectRecipe):
-    version = '3.1.1'
+    version = '3.5.1'
     url = 'https://pypi.python.org/packages/source/g/greenlet/greenlet-{version}.tar.gz'
     depends = ['setuptools']
     call_hostpython_via_targetpython = False
