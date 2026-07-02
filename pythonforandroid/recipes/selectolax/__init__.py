08a2ed28d25bb6ee6a71d6d602ba5166b708e171
Johannes Habel <EchterAlsFake@proton.me>
2026-07-02 04:56:45 +0200

    - pinned cython version 3.2.8

--- pythonforandroid/recipes/selectolax/__init__.py
+++ pythonforandroid/recipes/selectolax/__init__.py
@@ -5,7 +5,7 @@ class SelectolaxRecipe(PyProjectRecipe):
     version = '0.4.10'
     url = 'https://pypi.python.org/packages/source/s/selectolax/selectolax-{version}.tar.gz'
     depends = ['setuptools']
-    hostpython_prerequisites = ['cython']
+    hostpython_prerequisites = ['cython==3.2.8']
 
 
 recipe = SelectolaxRecipe()
