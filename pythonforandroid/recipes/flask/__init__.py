c0435a4462d7e23142e91c3a72557103dca83d26
Ansh Dadwal <anshdadwal298@gmail.com>
2026-08-21 12:28:51 +0530

    move markupsafe to depends

--- pythonforandroid/recipes/flask/__init__.py
+++ pythonforandroid/recipes/flask/__init__.py
@@ -4,7 +4,8 @@ from pythonforandroid.recipe import PyProjectRecipe
 class FlaskRecipe(PyProjectRecipe):
     version = '3.1.1'
     url = 'https://github.com/pallets/flask/archive/{version}.zip'
-    python_depends = ['jinja2', 'werkzeug', 'markupsafe', 'itsdangerous', 'click', 'blinker']
+    depends = ["markupsafe"]
+    python_depends = ['jinja2', 'werkzeug', 'itsdangerous', 'click', 'blinker']
 
 
 recipe = FlaskRecipe()
